9c12103e0c5c652b9fb4ea6989fce4047529a1f8
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-02-02 15:42:58 +0100

    (2D) LPP.cpp : update() by hand.

--- 2D/LPP.cpp
+++ 2D/LPP.cpp
@@ -20,29 +20,33 @@ int main (int argc, char **argv) {
 
   for (int x=0; x<n; ++x)
     if (drand48() < r) {
-      img.putpoint(x,0,1);
+      img.putpoint(x,0,1,0);
       field[x]=1;
     } else {
       field[x]=0;
     }
 
+  img.update();
+
   for (int y=1; y<n; ++y) {
     for (int x=0; x<n; ++x)
       if  ( (drand48() < l) ||
             ((x==y)&&(drand48() < d)) ) {
-        img.putpoint(x,y,1);
+        img.putpoint(x,y,1,0);
         field[x]++;
         int xx=(x+1)%n;
         while (field[xx]==0) {
-          img.putpoint(xx,y,1);
+          img.putpoint(xx,y,1,0);
           xx = (xx+1)%n;
         }
         field[xx]--;
-        img.putpoint(xx,y,1);
+        img.putpoint(xx,y,1,0);
       }
     for (int x=0; x<n; ++x)
       if (field[x])
-        img.putpoint(x,y,1);
+        img.putpoint(x,y,1,0);
+  
+    if (!(y%100)) img.update();
   }
 
   cout << img;
