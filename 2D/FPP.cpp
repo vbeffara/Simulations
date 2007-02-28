7d41398af9e14e9bd57d8189b64fa8de858bb8f5
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-02-28 17:06:36 +0100

    (2D) FPP : added the two-step Eden model.

--- 2D/FPP.cpp
+++ 2D/FPP.cpp
@@ -17,13 +17,26 @@ double cost (void) {
   return PR.exponential();
 }
 
+void spread (CoarseImage &img, double t, int x, int y) {
+  if (!(img(x+1,y)))
+    queue << Point(x+1,y,t+cost());
+  if (!(img(x-1,y)))
+    queue << Point(x-1,y,t+cost());
+  if (!(img(x,y+1)))
+    queue << Point(x,y+1,t+cost());
+  if (!(img(x,y-1)))
+    queue << Point(x,y-1,t+cost());
+}
+
 int main (int argc, char **argv) {
   char title[100];
   double curtime;
 
-  CL_Parser CLP (argc,argv,"n=200,i");
+  CL_Parser CLP (argc,argv,"n=1000,i,2",
+      "-n <size>, -i for invasion percolation, -2 for the two-step Eden model");
   int n = CLP.as_int('n');
   bool invasion = CLP.as_bool('i');
+  bool twostep = CLP.as_bool('2');
 
   snprintf (title,99,"A First-Passage Percolation cluster of radius %d", n);
 
@@ -33,6 +46,7 @@ int main (int argc, char **argv) {
 
   Point pt(n>>1,n>>1,+cost());
   queue << pt;
+  if (twostep) queue << Point((n>>1)+1,n>>1,cost());
 
   curtime = 0;
 
@@ -40,16 +54,22 @@ int main (int argc, char **argv) {
     queue >> pt;
 
     if (!(img(pt.x,pt.y))) {
-      curtime = (invasion==1) ? 0.0 : pt.t;
       img << pt;
-      if (!(img(pt.x+1,pt.y)))
-	queue << Point(pt.x+1,pt.y,curtime+cost());
-      if (!(img(pt.x-1,pt.y)))
-	queue << Point(pt.x-1,pt.y,curtime+cost());
-      if (!(img(pt.x,pt.y+1)))
-	queue << Point(pt.x,pt.y+1,curtime+cost());
-      if (!(img(pt.x,pt.y-1)))
-	queue << Point(pt.x,pt.y-1,curtime+cost());
+
+      curtime = (invasion==1) ? 0.0 : pt.t;
+      int deg=1;
+      if (twostep)
+        deg = img(pt.x-1,pt.y) + img(pt.x+1,pt.y) + img(pt.x,pt.y+1) + img(pt.x,pt.y-1);
+
+      for (int i=0; i<deg; ++i)
+        spread (img,curtime,pt.x,pt.y);
+      
+      if (twostep) {
+        if (img(pt.x-1,pt.y)) spread(img,curtime,pt.x-1,pt.y);
+        if (img(pt.x+1,pt.y)) spread(img,curtime,pt.x+1,pt.y);
+        if (img(pt.x,pt.y-1)) spread(img,curtime,pt.x,pt.y-1);
+        if (img(pt.x,pt.y+1)) spread(img,curtime,pt.x,pt.y+1);
+      }
       
       if ( (pt.x==1) || (pt.y==1) || (pt.x==n-2) || (pt.y==n-2) )
 	break;
