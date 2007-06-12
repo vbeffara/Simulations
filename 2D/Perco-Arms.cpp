e056c0119a143e4a343e6b1fe9c75a3fcc6e4951
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-06-12 17:29:02 +0100

    (2D/Perco-Arms.cpp) Some cleaning up

--- 2D/Perco-Arms.cpp
+++ 2D/Perco-Arms.cpp
@@ -2,7 +2,6 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/edmunds_karp_max_flow.hpp>
 #include <boost/graph/graph_utility.hpp>
-#include <boost/graph/read_dimacs.hpp>
 #include <iostream>
 #include <vb/CL_Parser.h>
 #include <vb/PRNG.h>
@@ -11,33 +10,34 @@
 using namespace boost;
 using namespace std;
 
-typedef adjacency_list_traits < vecS, vecS, directedS > Traits;
+typedef adjacency_list_traits < listS, vecS, directedS > Traits;
+typedef Traits::edge_descriptor edge_descriptor;
+
 typedef adjacency_list < listS, vecS, directedS,
-        property < vertex_name_t, std::string >,
+        no_property,
         property < edge_capacity_t, long,
         property < edge_residual_capacity_t, long,
-        property < edge_reverse_t, Traits::edge_descriptor > > > > Graph;
-typedef graph_traits<Graph>::edge_descriptor edge_descriptor;
+        property < edge_reverse_t, edge_descriptor > > > > Graph;
 
-Graph *g;
-
-property_map < Graph, edge_capacity_t >::type cap;
-property_map < Graph, edge_reverse_t >::type rev;
-
-void add_one (int i, int j) {
+void add_one (Graph &g, int i, int j) {
   edge_descriptor e1,e2;
   bool t;
 
-  tie(e1,t) = add_edge(i,j,*g);
-  tie(e2,t) = add_edge(j,i,*g);
-  cap[e1] = 1; cap[e2] = 1;
-  rev[e1] = e2; rev[e2] = e1;
+  tie(e1,t) = add_edge(i,j,g);
+  tie(e2,t) = add_edge(j,i,g);
+
+  put (edge_capacity, g, e1, 1);
+  put (edge_capacity, g, e2, 1);
+
+  put (edge_reverse, g, e1, e2);
+  put (edge_reverse, g, e2, e1);
 }
 
 int main(int argc, char **argv) {
-  vb::CL_Parser CLP(argc,argv,"n=100,t=1");
+  vb::CL_Parser CLP(argc,argv,"n=100,t=1,p=.5");
   int n = CLP.as_int('n');
   int n_iter = CLP.as_int('t');
+  double p = CLP.as_double('p');
 
   int stats[5];
   for (int i=0;i<5;++i) stats[i]=0;
@@ -45,39 +45,29 @@ int main(int argc, char **argv) {
   vb::PRNG prng;
   vb::ProgressBar PB (n_iter);
 
+  Graph g (n*n+1);
+
   for (int iter=1;iter<=n_iter;++iter) {
-    PB.update (iter);
-
-    g = new Graph (n*n+1);
-    cap = get(edge_capacity, *g);
-    rev = get(edge_reverse, *g);
-
-    char *adj = new char[n*n];
-
-    for (int x=0; x<n; ++x)
-      for (int y=0; y<n; ++y)
-        adj[x+n*y] = (char)prng.uniform(4);
-
     for (int x=0; x<n; ++x) {
       for (int y=0; y<n; ++y) {
         int i=x+n*y;
-        if ((x<n-1)&&(adj[i]&1)) add_one(i,i+1);
-        if ((y<n-1)&&(adj[i]&2)) add_one(i,i+n);
+        if ((x<n-1)&&(prng.bernoulli(p))) add_one(g,i,i+1);
+        if ((y<n-1)&&(prng.bernoulli(p))) add_one(g,i,i+n);
       }
     }
 
     for (int i=0; i<n; ++i) {
-      add_one(i,n*n);
-      add_one(n*i,n*n);
-      add_one((n-1)+n*i,n*n);
-      add_one((n-1)*n+i,n*n);
+      add_one(g,i,n*n);
+      add_one(g,n*i,n*n);
+      add_one(g,(n-1)+n*i,n*n);
+      add_one(g,(n-1)*n+i,n*n);
     }
 
-    long flow = edmunds_karp_max_flow (*g,(n>>1)*(n+1),n*n);
+    long flow = edmunds_karp_max_flow (g,(n>>1)*(n+1),n*n);
     stats[flow]++;
 
-    delete g;
-    delete[] adj;
+    g.clear();
+    PB.update (iter);
   }
 
   PB.die();
