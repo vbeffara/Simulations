8c3bdaf108cd5d1758cc9c8ff13a1a8192fa4d21
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-09-09 12:25:09 +0200

    Better example for the Figure class.

--- tests/figtest.cpp
+++ tests/figtest.cpp
@@ -9,8 +9,13 @@ using namespace vb;
 int main (int argc, char **argv) {
   Figure F;
   
-  F.add(new Segment (complex<double>(0,0), complex<double>(2,1)));
-  F.add(new Circle (complex<double>(1,.5), .1));
+  for (int i=0; i<=10; ++i) {
+    F.add(new Segment (complex<double>(0,0.1*i), complex<double>(1,0.1*i)));
+    F.add(new Segment (complex<double>(0.1*i,0), complex<double>(0.1*i,1)));
+  }
+
+  for (int i=1; i<=5; ++i)
+    F.add(new Circle (complex<double>(.5,.5),.1*i));
 
   F.printASY(cout);
   return 0;
