755ffee754133557caddd7dc5cec19fe39a6f68d
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-09-20 16:30:40 +0200

    Added fix-points localization

--- 2D/geom.cpp
+++ 2D/geom.cpp
@@ -6,7 +6,6 @@
 #define N 10
 #define PI 4*atan(1.0)
 
-
 using namespace std;
 using namespace vb;
 
@@ -39,9 +38,10 @@ class pt {
     void step (const vector<double> &o);
     void reverse ();
 };
-
-pt nopoint (-1,-1,-1,-1);
-
+ostream &operator<< (ostream &os, const pt p) {
+  os << (double)p.i + p.x << " " << (double)p.j + p.y << endl;
+  return os;
+}
 void pt::step (const vector<double> &o) {
   int ij = N*i+j;
   int r=0;
@@ -108,6 +108,13 @@ void pt::step (const vector<double> &o) {
   x -= di; i += di;
   y -= dj; j += dj;
 }
+void pt::reverse () {
+  if (x==0) { x=1; i--; }
+  else if (x==1) { x=0; i++; }
+  else if (y==0) { y=1; j--; }
+  else if (y==1) { y=0; j++; }
+}
+pt nopoint (-1,-1,-1,-1);
 
 pt geodesique (pt p, const vector<double> &o, bool tracing=false) {
   set<pt> S;
@@ -123,14 +130,6 @@ pt geodesique (pt p, const vector<double> &o, bool tracing=false) {
   if ( (p.i<0) || (p.i>=N) || (p.j<0) || (p.j>=N) ) return nopoint;
   else return p;
 }
-
-void pt::reverse () {
-  if (x==0) { x=1; i--; }
-  else if (x==1) { x=0; i++; }
-  else if (y==0) { y=1; j--; }
-  else if (y==1) { y=0; j++; }
-}
-
 void hair (pt p, pt trap, const vector<double> &o) {
   pt p1 = geodesique (pt(p),o,false);
   if (p1.dist2(trap) == 0) {
@@ -139,7 +138,6 @@ void hair (pt p, pt trap, const vector<double> &o) {
     geodesique (p,o,true);
   }
 }
-
 void octopus (int i, int j, vector<double> &o, PRNG &prng) {
   while (true) {
     double o1 = o[(i)*N + (j)] = prng.uniform(2*PI);
@@ -165,6 +163,20 @@ void octopus (int i, int j, vector<double> &o, PRNG &prng) {
     hair (pt(i,j-1,1,x),trap,o);
   }
 }
+set<pt> fixpoints (const vector<double> &o) {
+  set<pt> S;
+  for (int i=0; i<N; ++i) {
+    for (int j=0; j<N; ++j) {
+      for (double x=0; x<=1; x+=.01) {
+        S.insert ( geodesique (pt(i,j,x,0),o,false) );
+        S.insert ( geodesique (pt(i,j,x,1),o,false) );
+        S.insert ( geodesique (pt(i,j,0,x),o,false) );
+        S.insert ( geodesique (pt(i,j,1,x),o,false) );
+      }
+    }
+  }
+  return S;
+}
 
 int main (int argc, char **argv) {
   PRNG prng;
@@ -172,5 +184,8 @@ int main (int argc, char **argv) {
   vector<double> o (N*N);
   for (int i=0; i<N*N; ++i) o[i] = prng.uniform(2*PI);
 
-  octopus (N/2,N/2,o,prng);
+  set<pt> S = fixpoints(o);
+  for (set<pt>::iterator i = S.begin(); i != S.end(); ++i) {
+    if (!(*i == nopoint)) geodesique (pt(*i),o,true);
+  }
 }
