c999730f590b8391f100c29dae1f0398663bba71
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-09-20 16:06:19 +0200

    Moved reverse() inside class pt.

--- 2D/geom.cpp
+++ 2D/geom.cpp
@@ -6,6 +6,7 @@
 #define N 10
 #define PI 4*atan(1.0)
 
+
 using namespace std;
 using namespace vb;
 
@@ -36,7 +37,10 @@ class pt {
     }
 
     void step (const vector<double> &o);
+    void reverse ();
 };
+
+pt nopoint (-1,-1,-1,-1);
 
 void pt::step (const vector<double> &o) {
   int ij = N*i+j;
@@ -114,25 +118,26 @@ pt geodesique (pt p, const vector<double> &o, bool tracing=false) {
     if (S.count(p)) break;
     S.insert(p);
   }
-
   if (tracing) cout << endl;
-  return p;
+
+  if ( (p.i<0) || (p.i>=N) || (p.j<0) || (p.j>=N) ) return nopoint;
+  else return p;
 }
 
-pt reverse (pt p) {
-  if (p.x==0) { p.x=1; p.i--; }
-  else if (p.x==1) { p.x=0; p.i++; }
-  else if (p.y==0) { p.y=1; p.j--; }
-  else if (p.y==1) { p.y=0; p.j++; }
-  return p;
+void pt::reverse () {
+  if (x==0) { x=1; i--; }
+  else if (x==1) { x=0; i++; }
+  else if (y==0) { y=1; j--; }
+  else if (y==1) { y=0; j++; }
 }
 
 void hair (pt p, pt trap, const vector<double> &o) {
-    pt p1 = geodesique (p,o,false);
-    if (p1.dist2(trap) == 0) {
-      geodesique (p,o,true);
-      geodesique (reverse(p),o,true);
-    }
+  pt p1 = geodesique (pt(p),o,false);
+  if (p1.dist2(trap) == 0) {
+    geodesique (pt(p),o,true);
+    p.reverse();
+    geodesique (p,o,true);
+  }
 }
 
 void octopus (int i, int j, vector<double> &o, PRNG &prng) {
