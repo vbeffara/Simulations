c9783fa627791ba4adc0ca69e4b0540e3b4d1d84
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-11-16 17:27:58 +0100

    Use CL_Parser in Perco-grad for rectangular images.

--- 2D/Perco-grad.cpp
+++ 2D/Perco-grad.cpp
@@ -1,6 +1,7 @@
 
 #include <math.h>
 #include <vb.h>
+#include <vb/CL_Parser.h>
 
 using namespace vb;
 
@@ -19,7 +20,7 @@ void cnx (Image *img, long ox, long oy, long in, long out)
   while (imin<=imax) {
     i=fifox[imin];
     j=fifoy[imin];
-    k=i+j*img->width;
+    k=i+j*img->pitch;
     imin++;
     if ((i<img->width-1)&&(t[k+1]!=in)&&(t[k+1]!=out)) {
       fifox[++imax]=i+1;
@@ -31,12 +32,12 @@ void cnx (Image *img, long ox, long oy, long in, long out)
       fifoy[imax]=j;
       img->putpoint (i-1,j,in);
     }
-    if ((j<img->height-1)&&(t[k+img->width]!=in)&&(t[k+img->width]!=out)) {
+    if ((j<img->height-1)&&(t[k+img->pitch]!=in)&&(t[k+img->pitch]!=out)) {
       fifox[++imax]=i;
       fifoy[imax]=j+1;
       img->putpoint (i,j+1,in);
     }
-    if ((j>0)&&(t[k-img->width]!=in)&&(t[k-img->width]!=out)) {
+    if ((j>0)&&(t[k-img->pitch]!=in)&&(t[k-img->pitch]!=out)) {
       fifox[++imax]=i;
       fifoy[imax]=j-1;
       img->putpoint (i,j-1,in);
@@ -46,37 +47,39 @@ void cnx (Image *img, long ox, long oy, long in, long out)
 
 int main(int argc, char ** argv)
 {
-  Image *img;
-  long n,i;
-  char title[80];
-  double p1,p2;
+  CL_Parser clp (argc, argv, "n=500,p=0,q=1,W=0,H=0");
+  int n = clp.as_int('n');
+  int w = clp.as_int('W');
+  int h = clp.as_int('H');
+  double p1 = clp.as_double('p');
+  double p2 = clp.as_double('q');
 
-  /* arguments -> n */
-
-  if (argc != 4) {
-    fprintf(stderr, "Syntaxe : %s <n> <p1> <p2>\n",argv[0]);
+  if ((w*h==0) && (w+h!=0)) {
+    std::cerr << "Please specify both width and height, or neither." << std::endl;
     exit(1);
   }
-  n=atoi(argv[1]);
-  p1=atof(argv[2]);
-  p2=atof(argv[3]);
 
+  if (w*h==0) { w=n; h=n; }
+
+  char title[80];
   sprintf(title,"Gradient percolation cluster");
-  img=new Image(n,n,2,title);
+
+  Image *img = new Image(w,h,2,title);
   t=img->give_me_the_pic();
 
   PRNG prng;
 
   /* Site-percolation --- 1=dedans, 2=dehors */
 
-  for (i=0;i<n*n;i++) {
-    if ( prng.bernoulli( p1 + (p2-p1)*((double)i / (n*n)) )) 
+  long i;
+  for (i=0;i<w*h;i++) {
+    if ( prng.bernoulli( p1 + (p2-p1)*((double)i / (w*h)) )) 
       t[i]=1;
     else
       t[i]=2;
   }
 
-  for (i=n*n-n;i<n*n;i++) {
+  for (i=w*h-w;i<w*h;i++) {
     t[i]=1;
   }
 
@@ -87,7 +90,7 @@ int main(int argc, char ** argv)
 
   fifox = new int[4*img->width*img->height];
   fifoy = new int[4*img->width*img->height];
-  cnx(img,0,n-1,3,2);
+  cnx(img,0,h-1,3,2);
 
   img->update();
 
@@ -98,7 +101,7 @@ int main(int argc, char ** argv)
   /* for (i=0;i<2*n;i++) if (t[img->width*(i+1)-1]!=3)       cnx(img,img->width-1,i,0,3); */
   /* for (i=0;i<2*n;i++) if (t[img->width*(img->height-1)+i]!=3) cnx(img,i,img->height-1,0,3); */
 
-  for (i=0;i<n*n;i++) if (t[i]==2) t[i]=0;
+  for (i=0;i<w*h;i++) if (t[i]==2) t[i]=0;
 
   /* affichage du resultat */
 
