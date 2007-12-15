277cd58c8f7ee59cb8eccfd7d008d2f198e95e6a
Vincent Beffara <vbeffara@ens-lyon.fr>
2007-12-15 23:57:56 +0100

    Modified MDLA.cpp to use CL_Parser and show ghosts.

--- 2D/MDLA.cpp
+++ 2D/MDLA.cpp
@@ -5,9 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <vb.h>
+#include <vb/Image.h>
+#include <vb/PRNG.h>
+#include <vb/CL_Parser.h>
 
 #define EC fprintf(stderr,"<%d>\n",__LINE__);
+
+#define EMPTY  3
+#define AWAY   2
+#define ALIVE  1
+#define DEAD   0
 
 using namespace vb;
 
@@ -18,20 +25,14 @@ int main(int argc, char ** argv)
 {
   Image *img;              /* L image */
   char title[80];          /* Titre ... */
-  int n;                   /* Taille du carré */
   char d;                  /* La chaine */
-  double p;                /* Renforcement a 1+a */
   
   long x,y,nx,ny,done,ndraw,ngrey;
 
-  /* arguments -> taille du terrain, densité */
-
-  if (argc != 3) {
-    fprintf(stderr, "Syntaxe : MDLA <n> <p>\n");
-    exit(1);
-  }
-  n=atoi(argv[1]);
-  p=atof(argv[2]);
+  CL_Parser CLP (argc, argv, "n=250,p=.5,g");
+  int    n = CLP.as_int('n');
+  double p = CLP.as_double('p');
+  bool   g = CLP.as_bool('g');
 
   /* Initialisations */
 
@@ -40,43 +41,54 @@ int main(int argc, char ** argv)
   sprintf(title,"A MDLA in density %.2f",p);
   img = new Image(2*n,2*n,2,title);
 
-  img->onscreen();
+  ngrey = 0;
 
-  ngrey=0;
-  for (x=0;x<2*n;x++) {
-    for (y=0;y<2*n;y++) {
-      if (prng.bernoulli(p)) { 
-	img->putpoint (x,y,1);
-	ngrey++;
+  if (g) {
+    for (x=0;x<2*n;++x)
+      for (y=0;y<2*n;++y)
+        img->putpoint (x,y,AWAY);
+  } else {
+    for (x=0;x<2*n;x++) {
+      for (y=0;y<2*n;y++) {
+        if (prng.bernoulli(p)) { 
+          img->putpoint (x,y,ALIVE);
+          if ((x!=n)||(y!=n)) ngrey++;
+        } else
+          img->putpoint (x,y,EMPTY);
       }
     }
   }
-  img->putpoint (n,n,3);
+  img->putpoint (n,n,DEAD);
+
+  img->onscreen();
 
   /* Simulation */
 
   done=0; ndraw=0;
   
-  while (!done) {
+  while (true) {
     x = prng.rand() % (2*n);
     y = prng.rand() % (2*n);
-    if ((*img)(x,y) == 1) {
-      d = prng.rand() &3;
-      nx = x + dx[(int)d];
-      ny = y + dy[(int)d];
-      if ((nx>=0)&&(ny>=0)&&(nx<2*n)&&(ny<2*n)) {
-	switch ((*img)(nx,ny)) {
-	case 0:
-	  img->putpoint (x,y,0);
-	  img->putpoint (nx,ny,1);
-	  break;
-	case 3:
-	  img->putpoint (x,y,3);
-	  ngrey--;
-	  if ((x==2*n-1)||(y==2*n-1)||(x==0)||(y==0)||(ngrey==0)) done=1;
-	  break;
-	}
-      }
+
+    d = prng.rand() &3;
+    nx = x + dx[(int)d]; if ((nx<0)||(nx>=2*n)) continue;
+    ny = y + dy[(int)d]; if ((ny<0)||(ny>=2*n)) continue;
+
+    if ((*img)(x,y) == (*img)(nx,ny)) continue;
+
+    if ((*img)(x,y) == AWAY) img->putpoint (x,y,(prng.bernoulli(p)?ALIVE:EMPTY));
+    if ((*img)(nx,ny) == AWAY) img->putpoint (nx,ny,(prng.bernoulli(p)?ALIVE:EMPTY));
+
+    if ((*img)(x,y) == (*img)(nx,ny)) continue;
+    if ((*img)(x,y) != ALIVE) { int t=x; x=nx; nx=t;  t=y; y=ny; ny=t; }
+    if ((*img)(x,y) != ALIVE) continue;
+
+    if ((*img)(nx,ny) == EMPTY) {
+      img->putpoint (x,y,EMPTY);
+      img->putpoint (nx,ny,ALIVE);
+    } else if ((*img)(nx,ny) == DEAD) {
+      img->putpoint (x,y,DEAD);
+      --ngrey;
     }
   }
   
