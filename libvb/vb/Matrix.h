0e5f8ce9929c648502e1cf842d6c126910bf52f3
Vincent Beffara <vbeffara@ens-lyon.fr>
2008-06-10 20:34:09 +0200

    Non-trivial function minimization.

--- libvb/vb/Matrix.h
+++ libvb/vb/Matrix.h
@@ -44,15 +44,52 @@ namespace vb {
         return *this;
       }
 
+      Matrix<T> &operator-= (const Matrix<T> &M) {
+        if ((lines != M.lines) || (columns != M.columns))
+          throw std::runtime_error("vb::Matrix : wrong dimension.");
+        for (int i=0; i<lines; ++i)
+          for (int j=0; j<columns; ++j)
+            data[i][j] -= M.data[i][j];
+        return *this;
+      }
+
       Matrix<T> &operator*= (const Matrix<T> &M) {
         (*this) = (*this) * M;
         return (*this);
       }
+
+      Matrix<T> &operator*= (T l) {
+        for (int i=0; i<lines; ++i)
+          for (int j=0; j<columns; ++j)
+            data[i][j] *= l;
+        return (*this);
+      }
+
+      Matrix<T> &operator/= (T l) {
+        for (int i=0; i<lines; ++i)
+          for (int j=0; j<columns; ++j)
+            data[i][j] /= l;
+        return (*this);
+      }
   };
+
+  template <class T> Matrix<T> operator- (const Matrix<T> &M) {
+    Matrix<T> B = M;
+    for (int i=0; i<B.lines; ++i)
+      for (int j=0; j<B.columns; ++j)
+        B.data[i][j] = - B.data[i][j];
+    return B;
+  }
 
   template <class T> Matrix<T> operator+ (const Matrix<T> &A, const Matrix<T> &B) {
     Matrix<T> S = A;
     S += B;
+    return S;
+  }
+
+  template <class T> Matrix<T> operator- (const Matrix<T> &A, const Matrix<T> &B) {
+    Matrix<T> S = A;
+    S -= B;
     return S;
   }
 
@@ -83,6 +120,44 @@ namespace vb {
       if (i < M.lines-1) os << ",";
     }
     return os << "]";
+  }
+
+  template <class T> Matrix<T> operator* (T l, const Matrix<T> &M) {
+    Matrix<T> P = M; P *= l;
+    return P;
+  }
+
+  template <class T> Matrix<T> operator* (const Matrix<T> &M, T l) {
+    return l*M;
+  }
+
+  template <class T> Matrix<T> operator/ (const Matrix<T> &M, T l) {
+    Matrix<T> P = M; P /= l;
+    return P;
+  }
+
+  template <class T> Matrix<T> transpose (const Matrix<T> M) {
+    Matrix<T> P (M.columns, M.lines);
+    for (int i=0; i<P.lines; ++i)
+      for (int j=0; j<P.columns; ++j)
+        P.data[i][j] = M.data[j][i];
+    return P;
+  }
+
+  template <class T> T scalar_product (const Matrix<T> &A, const Matrix<T> &B) {
+    if ((A.lines != B.lines) || (A.columns != B.columns))
+          throw std::runtime_error("vb::Matrix : wrong dimension.");
+
+    T t = A.data[0][0] * B.data[0][0];
+    for (int i=0; i<A.lines; ++i)
+      for (int j=0; j<A.columns; ++j)
+        if ((i!=0) || (j!=0))
+          t += A.data[i][j] * B.data[i][j];
+    return t;
+  }
+
+  template <class T> T norm_squared (const Matrix<T> &M) {
+    return scalar_product(M,M);
   }
 }
 
--- tests/mattest.cpp
+++ tests/mattest.cpp
@@ -6,19 +6,72 @@
 using namespace vb;
 using namespace std;
 
-int main() {
-  Matrix<vb::real> M(3,3);
-  Matrix<vb::real> N(3,3);
+vb::real SQRT2 (sqrt(2.0));
+vb::real SQRT3 (sqrt(3.0));
 
-  for (int i=0; i<3; ++i) {
-    for (int j=0; j<3; ++j) {
-      M.data[i][j] = i+j;
-      N.data[i][j] = i+j;
-    }
+vb::real f (Matrix<vb::real> x) {
+  vb::real x1 = x.data[0][0];
+  vb::real x2 = x.data[1][0];
+
+  return (x1-SQRT2)*(x1-SQRT2) + (x2-SQRT3)*(x2-SQRT3);
+}
+
+Matrix<vb::real> g (Matrix<vb::real> x) {
+  Matrix<vb::real> out(2,1);
+  out.data[0][0] = ((x.data[0][0]-SQRT2)*vb::real(2.0));
+  out.data[1][0] = ((x.data[1][0]-SQRT3)*vb::real(2.0));
+  return out;
+}
+
+#define m1 vb::real(.3)
+#define m2 vb::real(.7)
+
+vb::real line_search (Matrix<vb::real> x, Matrix<vb::real> d) {
+  vb::real t_l = 0.0, t_r = 0.0, t(1.0);
+  vb::real q_0 = f(x), qq_0 = m2 * scalar_product (g(x),d);
+  while (true) {
+    //cout << "  " << t_l << " < " << t << " < " << t_r << " - " << f(x+t*d) << endl;
+    vb::real q = f (x+t*d);
+    vb::real qq = scalar_product (g(x+t*d),d);
+    vb::real y = q_0 + m1 * t * qq_0;
+
+    //cout << "  " << t << " - " << q << endl;
+
+    if ((q<=y) && (qq>=qq_0)) break;
+    if (q>y) t_r=t;
+    else t_l = t;
+
+    if (t_r>t_l) t = (t_r+t_l)/vb::real(2.0);
+    else t = t_l * vb::real(2.0);
   }
+  return t;
+}
 
-  cout << M << endl;
-  cout << N << endl;
-  M *= N;
-  cout << M << endl;
+int main() {
+  Matrix<vb::real> x(2,1);
+  vb::real ff = f(x);
+  Matrix<vb::real> gg = g(x);
+
+  Matrix<vb::real> W(2,2);
+  for (int i=0; i<2; ++i) W.data[i][i] = vb::real(1.0);
+
+  vb::real old_f = ff + vb::real(1.0);
+
+  for (int i=0; i<10; ++i) {
+    cout << x << " -> " << ff << endl;
+    old_f = ff;
+
+    Matrix<vb::real> dd = - W * gg;
+    Matrix<vb::real> ss = dd * line_search(x,dd) ; x += ss;
+    Matrix<vb::real> newgg = g(x);
+    Matrix<vb::real> yy = newgg - gg;
+
+    if (norm_squared(ss) == vb::real(0.0)) break;
+
+    vb::real u = vb::real(1.0) + scalar_product(yy,W*yy) / scalar_product(yy,ss);
+    W += u * ss * transpose(ss) / scalar_product(yy,ss);
+    W -= (ss*transpose(yy)*W + W*yy*transpose(ss)) / scalar_product(yy,ss);
+
+    ff = f(x); gg = g(x);
+  }
 }
