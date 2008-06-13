a9e5ce9c1bc3e7dadde28d480087d1c08bf88821
Vincent Beffara <vbeffara@ens-lyon.fr>
2008-06-13 23:03:37 +0200

    Put function minimization inside the library.

---
+++ libvb/vb/minimize.h
@@ -0,0 +1,129 @@
+
+/// @file minimize.h
+/// Algorithms for function minimization.
+
+#ifndef __VB_MINIMIZE_H
+#define __VB_MINIMIZE_H
+
+#include <vb/Matrix.h>
+
+namespace vb {
+  /** A utility class for function minimization.
+   *
+   * It stores the triple of a point, a value and a gradient.
+   */
+
+  template <class T> class PointValueGradient {
+    public:
+      Vector<T> point;     ///< The position of the point.
+      T value;             ///< The value of a function at the point.
+      Vector<T> gradient;  ///< The gradient of a function at the point.
+
+      /// Constructor from the three constituents.
+
+      PointValueGradient (const Vector<T> &p, const T &v, const Vector<T> &g) : point(p), value(v), gradient(g) { };
+  };
+
+  /** Line-search as a plug-in for a numerical optimization algorithm.
+   *
+   * It implements Wolfe's method. Reference : J.F. Bonnans et al., 
+   * "Numerical Optimization" (2ed, Springer, 2006), p. 43.
+   *
+   * Parameters chosen : m1 = 0.3, m2 = 0.8.
+   *
+   * @todo Improve the choice of the new point using polynomial 
+   * interpolation instead of linear ? Not sure whether it actually 
+   * improves speed of convergence, but needs to be tried.
+   */
+
+  template <class T> PointValueGradient<T> line_search (T f (const Vector<T> &), Vector<T> g (const Vector<T> &), const Vector<T> &x, const Vector<T> &d) {
+    bool reverse = false;
+    Vector<T> dd = d;
+    if (scalar_product(g(x),d)>0) { reverse = true; dd = -dd; }
+
+    T t_l = 0.0, t_r = 0.0, t = 1.0;
+    T q_0 = f(x);
+    T qq_0 = .8 * scalar_product (g(x),dd);
+    T q, qq, y;
+    Vector<T> xx=x;
+    Vector<T> gg;
+    bool refining = false;
+
+    while (true) {
+      xx=x+t*dd;
+      q = f(xx);
+      gg = g(xx);
+      qq = scalar_product (gg,dd);
+      y = q_0 + .3 * t * qq_0;
+
+      if ((q<=y) && (qq>=qq_0)) break;
+      if (q>y) { t_r=t; refining = true; } else t_l = t;
+      if (refining) t = (t_r+t_l)/2.0; else t *= 2.0;
+      if (t-t_l+1.0 == 1.0) break;
+    }
+
+    if (reverse) { t = -t; }
+    return PointValueGradient<T> (xx,q,gg);
+  }
+
+  /** A quasi-Newtonian minimization algorithm.
+   *
+   * It is the algorithm of Broyden, Fletcher, Goldfarb and Shanno (BFGS 
+   * method).  Reference : J.F. Bonnans et al., "Numerical Optimization" 
+   * (2ed, Springer, 2006), p. 54.
+   *
+   * In dimension N it has to maintain an N by N matrix, which limits it 
+   * to a few thousand dimensions.
+   *
+   * @todo Keep track of a context (additional void* pointer in function 
+   * calls) to make it more generic, and in particular applicable to 
+   * circle packings.
+   */
+
+  template <class T> PointValueGradient<T> minimize_bfgs (T f (const Vector<T> &), Vector<T> g (const Vector<T> &), const Vector<T> &x0, const Vector<T> &W0 = Vector<T>(0)) {
+    PointValueGradient<T> cur_pvg (x0,f(x0),g(x0));
+    PointValueGradient<T> old_pvg (x0,cur_pvg.value+1,cur_pvg.gradient);
+
+    Vector<T> dx,dg,Wdg;
+    T dgdx,u;
+
+    Vector<T> diag = W0;
+    if (diag.size() == 0) diag = Vector<T> (x0.size(), T(1.0));
+    Matrix<T> W(x0.size(),x0.size(),diag);
+
+    while (cur_pvg.value < old_pvg.value) {
+      old_pvg = cur_pvg; cur_pvg = line_search(f,g,old_pvg.point, -(W*old_pvg.gradient));
+
+      dx = cur_pvg.point - old_pvg.point;
+      dg = cur_pvg.gradient - old_pvg.gradient;
+      Wdg = W*dg;
+      dgdx = scalar_product(dg,dx);
+      dx /= dgdx;
+      u = dgdx + scalar_product(dg,Wdg);
+
+      W.rank1update(u*dx-Wdg,dx);
+      W.rank1update(dx,-Wdg);
+    }
+
+    return old_pvg;
+  }
+
+  /** Function minimization by a steepest-descent algorithm.
+   *
+   * It has been called a "numerical absurdity", but it still works 
+   * pretty well compared to BFGS especially in very high dimension 
+   * (when storage of the approximate inverse Hessian is problematic).
+   */
+
+  template <class T> PointValueGradient<T> minimize_grad (T f (const Vector<T> &), Vector<T> g (const Vector<T> &), const Vector<T> &x0) {
+    PointValueGradient<T> cur_pvg (x0,f(x0),g(x0));
+    PointValueGradient<T> old_pvg (x0,cur_pvg.value+1,cur_pvg.gradient);
+
+    while (cur_pvg.value < old_pvg.value) {
+      old_pvg = cur_pvg; cur_pvg = line_search (f,g,old_pvg.point,-old_pvg.gradient);
+    }
+    return old_pvg;
+  }
+}
+
+#endif
--- tests/mattest.cpp
+++ tests/mattest.cpp
@@ -1,7 +1,5 @@
 
-#include <vb/Matrix.h>
-//#include <vb/Real.h>
-#include <vb/PRNG.h>
+#include <vb/minimize.h>
 #include <iostream>
 #include <math.h>
 
@@ -11,8 +9,6 @@ using namespace std;
 using namespace vb;
 
 #define DIM 400
-
-//namespace vb { typedef double Real; }
 
 Real f (const Vector<Real> &x) {
   Real o = 0;
@@ -24,121 +20,6 @@ Vector<Real> g (const Vector<Real> &x) {
   Vector<Real> out(DIM);
   for (unsigned int i=0; i<DIM; ++i) out[i] = sin(x[i]/(i+1))/(i+1);
   return out;
-}
-
-#define m1 .3
-#define m2 .8
-
-template <class T> class PointValueGradient {
-  public:
-    Vector<T> point;
-    T value;
-    Vector<T> gradient;
-
-    PointValueGradient (const Vector<T> &p, const T &v, const Vector<T> &g) : point(p), value(v), gradient(g) { };
-};
-
-/** Line-search as a plug-in for a numerical optimization algorithm.
- *
- * It implements Wolfe's method. Reference : J.F. Bonnans et al., 
- * "Numerical Optimization" (2ed, Springer, 2006), p. 43.
- *
- * @todo Improvr the choice of the new point using polynomial 
- * interpolation instead of linear ? Not sure whether it actually 
- * improves speed of convergence, but needs to be tried.
- */
-
-PointValueGradient<Real> line_search (Real f (const Vector<Real> &), Vector<Real> g (const Vector<Real> &), const Vector<Real> &x, const Vector<Real> &d) {
-  bool reverse = false;
-  Vector<Real> dd = d;
-  if (scalar_product(g(x),d)>0) { reverse = true; dd = -dd; }
-
-  Real t_l = 0.0, t_r = 0.0, t = 1.0;
-  Real q_0 = f(x);
-  Real qq_0 = m2 * scalar_product (g(x),dd);
-  Real q, qq, y;
-  Vector<Real> xx=x;
-  Vector<Real> gg;
-  bool refining = false;
-
-  while (true) {
-    xx=x+t*dd;
-    q = f(xx);
-    gg = g(xx);
-    qq = scalar_product (gg,dd);
-    y = q_0 + m1 * t * qq_0;
-
-    if ((q<=y) && (qq>=qq_0)) break;
-    if (q>y) { t_r=t; refining = true; } else t_l = t;
-    if (refining) t = (t_r+t_l)/2.0; else t *= 2.0;
-    if (t-t_l+1.0 == 1.0) break;
-  }
-
-  if (reverse) { t = -t; }
-  return PointValueGradient<Real> (xx,q,gg);
-}
-
-/** A quasi-Newtonian minimization algorithm.
- *
- * It is the algorithm of Broyden, Fletcher, Goldfarb and Shanno (BFGS 
- * method).  Reference : J.F. Bonnans et al., "Numerical Optimization" 
- * (2ed, Springer, 2006), p. 54.
- *
- * In dimension N it has to maintain an N by N matrix, which limits it 
- * to a few thousand dimensions.
- *
- * The iteration step attempts to minimize the number of vb::Real 
- * multiplications while remaining readable. Maybe epsilon more 
- * optimization is doable, but it is quite efficient already.
- *
- * @todo Find something that works in higher dimension, for circle 
- * packings and such: this one is enough for small figures, but not for 
- * scaling limits. Maybe using the fact that W-I has small rank (twice 
- * the number of iterates at most) and hoping that the number of 
- * iterates remains smaller that the dimension, which does seem to be 
- * the case, and templating it.
- *
- * @todo Put it inside libvb (vb/Numerical.h) and actually use it for 
- * circle packings.
- */
-
-PointValueGradient<Real> minimize_bfgs (Real f (const Vector<Real> &), Vector<Real> g (const Vector<Real> &), const Vector<Real> &x0, const Vector<Real> &W0 = Vector<Real>(0)) {
-  PointValueGradient<Real> cur_pvg (x0,f(x0),g(x0));
-  PointValueGradient<Real> old_pvg (x0,cur_pvg.value+1,cur_pvg.gradient);
-
-  Vector<Real> dx,dg,Wdg;
-  Real dgdx,u;
-
-  Vector<Real> diag = W0;
-  if (diag.size() == 0) diag = Vector<Real> (x0.size(), Real(1.0));
-  Matrix<Real> W(DIM,DIM,diag);
-
-  while (cur_pvg.value < old_pvg.value) {
-    old_pvg = cur_pvg; cur_pvg = line_search(f,g,old_pvg.point, -(W*old_pvg.gradient));
-    cerr << cur_pvg.value << endl;
-
-    dx = cur_pvg.point - old_pvg.point;
-    dg = cur_pvg.gradient - old_pvg.gradient;
-    Wdg = W*dg;
-    dgdx = scalar_product(dg,dx);
-    dx /= dgdx;
-    u = dgdx + scalar_product(dg,Wdg);
-
-    W.rank1update(u*dx-Wdg,dx);
-    W.rank1update(dx,-Wdg);
-  }
-
-  return old_pvg;
-}
-
-PointValueGradient<Real> minimize_grad (Real f (const Vector<Real> &), Vector<Real> g (const Vector<Real> &), const Vector<Real> &x0) {
-  PointValueGradient<Real> cur_pvg (x0,f(x0),g(x0));
-  PointValueGradient<Real> old_pvg (x0,cur_pvg.value+1,cur_pvg.gradient);
-
-  while (cur_pvg.value < old_pvg.value) {
-    old_pvg = cur_pvg; cur_pvg = line_search (f,g,old_pvg.point,-old_pvg.gradient);
-  }
-  return old_pvg;
 }
 
 int main () {
