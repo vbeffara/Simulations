be52160665abb737c881a1e26ed06ae3423f3152
Vincent Beffara <vbeffara@ens-lyon.fr>
2009-02-03 18:34:19 +0100

    New percolation RW corrector program.

---
+++ 2D/Corrector.cpp
@@ -0,0 +1,77 @@
+
+#include <vb/Minimizer.h>
+#include <vb/Map.h>
+#include <vb/CL_Parser.h>
+#include <vb/PRNG.h>
+#include <iostream>
+#include <vector>
+
+using namespace vb;
+using namespace std;
+
+int center = 6;
+int infinity = 13;
+
+int main (int argc, char **argv) {
+  CL_Parser CLP (argc, argv, "n=10,p=.9,b");
+  int n = CLP('n');
+  double p = CLP('p');
+  bool batch = CLP('b');
+
+  PRNG prng;
+
+  Map m (n*n);
+  
+  {
+    vector<char> adj(n*n,0);
+
+    for (int i=0; i<n; ++i) {
+      for (int j=0; j<n; ++j) {
+        int ij = i+n*j;
+
+        if (prng.bernoulli(p)) adj[ij] |= 1;
+        if (prng.bernoulli(p)) adj[ij] |= 2;
+        if ((i>0) && (adj[ij-1] & 1)) adj[ij] |= 4;
+        if ((j>0) && (adj[ij-n] & 2)) adj[ij] |= 8;
+      }
+    }
+
+    for (int i=0; i<n; ++i) {
+      adj[i] |= 5;
+      adj[n*i] |= 10;
+      adj[n-1+n*i] |= 10;
+      adj[(n-1)*n+i] |= 5;
+    }
+
+    for (int i=0; i<n; ++i) {
+      adj[i] &= 7;
+      adj[n*i] &= 11;
+      adj[n-1+n*i] &= 14;
+      adj[(n-1)*n+i] &= 13;
+    }
+
+    for (int i=0; i<n; ++i) {
+      for (int j=0; j<n; ++j) {
+        int ij = i+n*j;
+
+        if ((i<n-1) && (adj[ij] & 1)) m << Edge(ij, ij+1);
+        if ((j<n-1) && (adj[ij] & 2)) m << Edge(ij, ij+n);
+        if ((i>0) && (adj[ij] & 4)) m << Edge(ij, ij-1);
+        if ((j>0) && (adj[ij] & 8)) m << Edge(ij, ij-n);
+
+        m.v[ij] -> z = cpx(i,j);
+
+        m.bd[ij] = ((i==0) || (i==n-1) || (j==0) || (j==n-1));
+      }
+    }
+  }
+
+  if (!batch) { m.show(); m.pause(); }
+
+  Real output = m.balance();
+  cerr << n << " " << p << " " << output << endl;
+
+  if (!batch) m.pause();
+
+  return 0;
+}
