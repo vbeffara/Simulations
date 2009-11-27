5054f540c8a17a7c0d9abfa48e3fed4397a3426d
Vincent Beffara <vbeffara@ens-lyon.fr>
2009-11-27 22:35:42 +0100

    Populate vb::TriMatrix a little bit.

--- libvb/vb/TriMatrix.h
+++ libvb/vb/TriMatrix.h
@@ -4,6 +4,8 @@
 #ifndef __VB_TRIMATRIX_H
 #define __VB_TRIMATRIX_H
 
+#include <iostream>
+
 namespace vb {
 
   /** A class for storing unbounded spatial data.
@@ -11,8 +13,20 @@ namespace vb {
 
   template <typename T> class TriMatrix {
     public:
-      TriMatrix (T _empty = T(0)) : empty (_empty) {};
-      ~TriMatrix () {};
+      TriMatrix (T _empty = 0) : empty(_empty) {
+        base = (5*sizeof(TriMatrix<T>)) / sizeof(T);
+        size = base;
+        stride = 2*base+1;
+        std::cerr << "Base tile is of width " << stride << std::endl;
+        mid = (base+1)*stride;
+        sub  = NULL;
+        contents = new T[stride*stride];
+        for (int i=0; i<stride*stride; ++i) contents[i]=empty;
+      };
+
+      ~TriMatrix () {
+        delete[] contents;
+      };
 
       T operator() (int i, int j) const { return empty; }
       void put (int i, int j, T t) { };
@@ -20,6 +34,12 @@ namespace vb {
       /*************************************** Should be private below ! */
 
       T empty;
+      int base;
+      int stride;
+      int mid;
+      int size;
+      TriMatrix<T> *sub;
+      T* contents;
   };
 }
 
