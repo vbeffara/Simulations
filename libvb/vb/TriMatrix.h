a385ab8f199522f5f2e44635eda7888e166bfcba
Vincent Beffara <vbeffara@ens-lyon.fr>
2009-12-03 17:40:24 +0100

    Properize the code for vb::TriMatrix a little.

--- libvb/vb/TriMatrix.h
+++ libvb/vb/TriMatrix.h
@@ -13,25 +13,16 @@ namespace vb {
 
   template <typename T> class TriMatrix {
     public:
-      TriMatrix (T _empty = 0) : empty(_empty) {
-        base = (5*sizeof(TriMatrix<T>)) / sizeof(T);
-        size = base;
-        stride = 2*base+1;
-        std::cerr << "Base tile is of width " << stride << std::endl;
-        mid = (base+1)*stride;
-        sub  = NULL;
-        contents = new T[stride*stride];
-        for (int i=0; i<stride*stride; ++i) contents[i]=empty;
-      };
+      TriMatrix  (T _empty = 0);
+      ~TriMatrix ();
 
-      ~TriMatrix () {
-        delete[] contents;
-      };
+      T    get (int i, int j) const;
+      void put (int i, int j, T t);
 
-      T operator() (int i, int j) const { return empty; }
-      void put (int i, int j, T t) { };
+      T operator() (int i, int j) const { return get(i,j); };
 
-      /*************************************** Should be private below ! */
+    private:
+      TriMatrix (T _empty, int base, T *center);
 
       T empty;
       int base;
@@ -41,6 +32,28 @@ namespace vb {
       TriMatrix<T> *sub;
       T* contents;
   };
+
+  template <typename T> TriMatrix<T>::TriMatrix (T _empty) : empty(_empty) {
+    base = (5*sizeof(TriMatrix<T>)) / sizeof(T);
+    size = base;
+    stride = 2*base+1;
+    std::cerr << "Base tile is of width " << stride << std::endl;
+    mid = (base+1)*stride;
+    sub  = NULL;
+    contents = new T[stride*stride];
+    for (int i=0; i<stride*stride; ++i) contents[i]=empty;
+  }
+
+  template <typename T> TriMatrix<T>::~TriMatrix () {
+    delete[] contents;
+  }
+
+  template <typename T> T TriMatrix<T>::get (int i, int j) const {
+    return empty;
+  }
+
+  template <typename T> void TriMatrix<T>::put (int i, int j, T t) {
+  }
 }
 
 #endif
