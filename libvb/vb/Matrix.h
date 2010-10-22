2f81cf4ad450c96bf8b41fc9eedf2a6da7b9df8d
Vincent Beffara <vbeffara@ens-lyon.fr>
2010-10-22 15:46:25 +0200

    Finally made vb::Matrix an alias to Boost's matrix type.

--- libvb/vb/Matrix.h
+++ libvb/vb/Matrix.h
@@ -9,52 +9,10 @@
 
 namespace vb {
   typedef boost::numeric::ublas::vector<double> Vector;
+  typedef boost::numeric::ublas::matrix<double> Matrix;
 
-  class Matrix : public boost::numeric::ublas::matrix<double> {
-    public:
-      /** Standard constructor of a zero matrix.
-       *
-       * @param h The height of the matrix.
-       * @param w The width of the matrix.
-       */
-
-      Matrix (unsigned int h, unsigned int w);
-
-      /** Constructor from a diagonal vector.
-       *
-       * The length of the vector should be larger than max(w,h), but 
-       * the matrix does not have to be square.
-       *
-       * @param h The height of the matrix.
-       * @param w The width of the matrix.
-       * @param d The diagonal of the matrix.
-       */
-
-      Matrix (unsigned int h, unsigned int w, const Vector &d);
-
-      /** Right-multiply by another matrix.
-       *
-       * @param M The right multiplicator.
-       */
-
-      Matrix & operator*= (const Matrix &M);
-  };
-
-  /** Compute the product of two matrices.
-   *
-   * @param M The left matrix.
-   * @param N The right matrix.
-   */
-
-  Matrix operator* (const Matrix &M, const Matrix &N);
-
-  /** Compute the product of a matrix and a vector.
-   *
-   * @param M The matrix.
-   * @param X The vector.
-   */
-
-  Vector operator* (const Matrix &M, const Vector &X);
+  inline Matrix operator* (const Matrix &M, const Matrix &N) { return prod(M,N); }
+  inline Vector operator* (const Matrix &M, const Vector &X) { return prod(M,X); }
 }
 
 #endif
