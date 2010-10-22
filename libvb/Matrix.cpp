890e4d56b63d1e492fa8afb2aed7c8aeb3da10c4
Vincent Beffara <vbeffara@ens-lyon.fr>
2010-10-22 11:33:20 +0200

    Use boost for matrix additions and subtractions.

--- libvb/Matrix.cpp
+++ libvb/Matrix.cpp
@@ -11,32 +11,6 @@ namespace vb {
     (*this)(i,j) = t;
   }
 
-  MatrixStorage_Plain * MatrixStorage_Plain::add (MatrixStorage_Plain *M) {
-    for (unsigned int i=0; i<height; ++i)
-      for (unsigned int j=0; j<width; ++j)
-        (*this)(i,j) += (*M)(i,j);
-    return this;
-  }
-
-  MatrixStorage_Plain * MatrixStorage_Plain::sub (MatrixStorage_Plain *M) {
-    for (unsigned int i=0; i<height; ++i)
-      for (unsigned int j=0; j<width; ++j)
-        (*this)(i,j) -= (*M)(i,j);
-    return this;
-  }
-
-  MatrixStorage_Plain * MatrixStorage_Plain::mul_right (MatrixStorage_Plain *M) {
-    MatrixStorage_Plain *tmp = new MatrixStorage_Plain (this->height, M->width);
-    for (unsigned int i=0; i<height; ++i) {
-      for (unsigned int j=0; j<M->width; ++j) {
-        (*tmp)(i,j) = (*this)(i,0) * (*M)(0,j);
-        for (unsigned int k=1; k<width; ++k) 
-          (*tmp)(i,j) += (*this)(i,k) * (*M)(k,j);
-      }
-    }
-    return tmp;
-  }
-
   MatrixStorage_Plain * MatrixStorage_Plain::rank1update (const Vector &A, const Vector &B) {
     for (unsigned int i=0; i<this->height; ++i)
       for (unsigned int j=0; j<this->width; ++j)
@@ -51,59 +25,50 @@ namespace vb {
   /******************************************/
 
   Matrix::Matrix (unsigned int h, unsigned int w)
-    : width(w), height(h), data (new MatrixStorage_Plain (h,w)) {}
+    : width(w), height(h), data (h,w) {}
 
   Matrix::Matrix (unsigned int h, unsigned int w, const Vector &d)
-    : width(w), height(h), data (new MatrixStorage_Plain (h,w)) {
-      for (int i=0; i<d.size(); ++i) data -> put (i,i,d[i]);
+    : width(w), height(h), data (h,w) {
+      for (int i=0; i<d.size(); ++i) data.put (i,i,d[i]);
     }
 
-  Matrix::Matrix (const Matrix &M) : width(M.width), height(M.height) {
-    for (int i=0; i<height; ++i)
-      for (int j=0; j<width; ++j)
-        (*data)(i,j) = (*M.data)(i,j);
-  }
+  Matrix::Matrix (const Matrix &M) : width(M.width), height(M.height),
+    data (M.data) { }
 
   Matrix::~Matrix () {
-    delete data;
   }
 
   Matrix & Matrix::operator= (const Matrix &M) {
     if (&M != this) {
       width = M.width; height = M.height;
-      delete data; data = new MatrixStorage_Plain (M.height, M.width);
-      for (int i=0; i<height; ++i)
-        for (int j=0; j<width; ++j)
-          (*data)(i,j) = (*M.data)(i,j);
+      data = M.data;
     }
     return (*this);
   }
 
-  double Matrix::operator() (unsigned int i, unsigned int j) const { return (*data)(i,j); }
+  double Matrix::operator() (unsigned int i, unsigned int j) const { return data(i,j); }
 
   void Matrix::put (unsigned int i, unsigned int j, double t) { 
-    data->put(i,j,t);
+    data.put(i,j,t);
   }
 
   Matrix & Matrix::operator+= (const Matrix &M) {
-    data->add (M.data);
+    data += M.data;
     return (*this);
   }
 
   Matrix & Matrix::operator-= (const Matrix &M) {
-    data->sub(M.data);
+    data -= M.data;
     return (*this);
   }
 
   Matrix & Matrix::operator*= (const Matrix &M) {
-    MatrixStorage_Plain *tmp = data->mul_right (M.data);
-    if (data != tmp) { delete data; data = tmp; }
-    return (*this);
+    (boost::numeric::ublas::matrix<double>)(data) = prod (data, M.data);
+    return *this;
   }
 
   Matrix & Matrix::rank1update (const Vector &A, const Vector &B) {
-    MatrixStorage_Plain *tmp = data->rank1update(A,B);
-    if (data != tmp) { delete data; data = tmp; }
+    data.rank1update(A,B);
     return (*this);
   }
 
@@ -126,7 +91,7 @@ namespace vb {
   }
 
   Vector operator* (const Matrix &M, const Vector &X) {
-    return M.data->map_right(X);
+    return M.data.map_right(X);
   }
 
   std::ostream &operator<< (std::ostream &os, const Matrix &M) {
--- libvb/vb/Matrix.h
+++ libvb/vb/Matrix.h
@@ -25,12 +25,6 @@ namespace vb {
 
       void put (unsigned int i, unsigned int j, double t);
 
-      MatrixStorage_Plain *add (MatrixStorage_Plain *M);
-
-      MatrixStorage_Plain *sub (MatrixStorage_Plain *M);
-
-      MatrixStorage_Plain *mul_right (MatrixStorage_Plain *M);
-
       MatrixStorage_Plain *rank1update (const Vector &A, const Vector &B);
 
       Vector map_right (const Vector &X);
@@ -48,7 +42,7 @@ namespace vb {
     public:
       unsigned int width;   ///< The width of the matrix.
       unsigned int height;  ///< The height of the matrix.
-      MatrixStorage_Plain *data;  ///< The underlying storage of the matrix.
+      MatrixStorage_Plain data;  ///< The underlying storage of the matrix.
 
       /** Standard constructor of a zero matrix.
        *
