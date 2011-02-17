1649196e09b573a62f6dfde47d27b4344bf4d3eb
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-02-17 03:56:47 +0100

    Make a movie from SRW.

--- 2D/SRW.cpp
+++ 2D/SRW.cpp
@@ -8,13 +8,16 @@
 #include <vb/Figure.h>
 
 using namespace vb;
+using namespace std;
 
 int dx[4] = {1,0,-1,0};  /* 0=est, 1=nord ... */
 int dy[4] = {0,1,0,-1};
 
+typedef pair<cpx,cpx> bicpx;
+
 int main(int argc, char ** argv) {
   Figure f;
-  std::vector<cpx> t(1);
+  vector < bicpx > t;
   PRNG prng(0);
   
   int x=0, y=0;
@@ -27,9 +30,13 @@ int main(int argc, char ** argv) {
   for (int a=1 ;; ++a) {
     for (int b=0; b<a; ++b) {
       int d = prng.uniform_int(4);
-      x += dx[d]; if ((x>xmax) || (-x>xmax)) ++xmax;
-      y += dy[d]; if ((y>ymax) || (-y>ymax)) ++ymax;
-      t.push_back(cpx(x,y));
+      int nx = x + dx[d]; if ((nx>xmax) || (-nx>xmax)) ++xmax;
+      int ny = y + dy[d]; if ((ny>ymax) || (-ny>ymax)) ++ymax;
+      pair<cpx,cpx> step (cpx(x,y),cpx(nx,ny));
+      bool there=false;
+      foreach (bicpx s, t) if (s==step) { there=true; break; }
+      if (!there) t.push_back(step);
+      x = nx; y = ny;
     }
     
     f.clean();
@@ -38,8 +45,9 @@ int main(int argc, char ** argv) {
     f.dot(cpx(xmax+1,-ymax-1));
     f.dot(cpx(-xmax-1,ymax+1));
     f.dot(cpx(-xmax-1,-ymax-1));
-    for (unsigned i=0; i<t.size()-1; ++i)
-      f.segment(t[i],t[i+1]);
+    for (unsigned i=0; i<t.size(); ++i)
+      f.segment(t[i].first,t[i].second);
     f.show();
+    f.snapshot();
   }
 }
