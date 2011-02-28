92c55cf96d509a48bc094f37f7e9982a729aeac5
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-02-28 17:25:09 +0100

    Allow several kinds of SRW; center or not.

--- 2D/SRW.cpp
+++ 2D/SRW.cpp
@@ -8,20 +8,24 @@
 #include <vb/Figure.h>
 
 using namespace vb;
-using namespace std;
 
-int dx[4] = {1,0,-1,0};  /* 0=est, 1=nord ... */
-int dy[4] = {0,1,0,-1};
+int dx[8] = {1,1,0,-1,-1,-1,0,1};
+int dy[8] = {0,1,1,1,0,-1,-1,-1};
 
-typedef pair<cpx,cpx> bicpx;
+typedef std::pair<cpx,cpx> bicpx;
 
 int main(int argc, char ** argv) {
+  CL_Parser CLP (argc, argv, "a=85,c,v");
+  int  adj    = CLP('a');
+  bool center = CLP('c');
+  bool video  = CLP('v');
+  
   Figure f;
-  vector < bicpx > t;
+  std::vector < bicpx > t;
   PRNG prng(0);
   
   int x=0, y=0;
-  int xmax=0, ymax=0;
+  int xmax=0, ymax=0, xmin=0, ymin=0;
 
   f.circle(0,.5);  
   f.show();
@@ -29,25 +33,34 @@ int main(int argc, char ** argv) {
 
   for (int a=1 ;; ++a) {
     for (int b=0; b<a; ++b) {
-      int d = prng.uniform_int(4);
-      int nx = x + dx[d]; if ((nx>xmax) || (-nx>xmax)) ++xmax;
-      int ny = y + dy[d]; if ((ny>ymax) || (-ny>ymax)) ++ymax;
-      pair<cpx,cpx> step (cpx(x,y),cpx(nx,ny));
+      int d = 9; while (((1<<d) & adj) == 0) d = prng.uniform_int(8);
+      
+      int nx = x + dx[d]; if (nx>xmax) ++xmax; if (nx<xmin) --xmin;
+      int ny = y + dy[d]; if (ny>ymax) ++ymax; if (ny<ymin) --ymin;
+      
+      std::pair<cpx,cpx> step (cpx(x,y),cpx(nx,ny));
+      
       bool there=false;
       foreach (bicpx s, t) if (s==step) { there=true; break; }
       if (!there) t.push_back(step);
+      
       x = nx; y = ny;
+    }
+
+    if (center) {
+      xmax = max(xmax,-xmin); xmin = -xmax;
+      ymax = max(ymax,-ymin); ymin = -ymax;
     }
     
     f.clean();
     f.circle(cpx(x,y),.5);
     f.dot(cpx(xmax+1,ymax+1));
-    f.dot(cpx(xmax+1,-ymax-1));
-    f.dot(cpx(-xmax-1,ymax+1));
-    f.dot(cpx(-xmax-1,-ymax-1));
+    f.dot(cpx(xmax+1,ymin-1));
+    f.dot(cpx(xmin-1,ymax+1));
+    f.dot(cpx(xmin-1,ymin-1));
     for (unsigned i=0; i<t.size(); ++i)
       f.segment(t[i].first,t[i].second);
     f.show();
-    f.snapshot();
+    if (video) f.snapshot();
   }
 }
