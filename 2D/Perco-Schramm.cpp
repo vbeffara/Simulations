7f38e95b173473a5ac1f5151cd59659145634ff7
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-03-08 22:56:23 +0100

    Shortened perco-schramm a lot.

--- 2D/Perco-Schramm.cpp
+++ 2D/Perco-Schramm.cpp
@@ -11,24 +11,21 @@
 
 using namespace std;
 
-// Poor man's PS emulation, good enough here.
-
 typedef pair<double,double> coo;
 
-coo operator+ (coo a, coo b) {
-  return coo (a.first+b.first, a.second+b.second);
-}
+int numw = 40, numh = 40;
+double omx = sqrt(3.0);
 
-coo operator/ (coo a, double l) {
-  return coo (a.first/l, a.second/l);
-}
+vector<bool> cols;
+int base,dir;
+coo pos;
+
+coo operator+ (coo a, coo b)    { return coo (a.first+b.first , a.second+b.second); }
+coo operator/ (coo a, double l) { return coo (a.first/l       , a.second/l       ); }
 
 ostream &operator<< (ostream &o, coo c) {
-  o << "(" << c.first << "," << c.second << ")";
-  return o;
+  return o << "(" << c.first << "," << c.second << ")";
 }
-
-coo pos;
 
 void lineto (coo xy) {
   cout << "draw(" << pos << "--"<< xy << ");" << endl;
@@ -37,54 +34,22 @@ void lineto (coo xy) {
 
 void rlineto (coo xy) { lineto (pos+xy); }
 
-// Parameters
-
-int    numw        = 100;                                 // Must be even
-double marginhexes = 1.2;
-double picwidth    = 440;
-double eachhex     = picwidth / (numw-(marginhexes*2)-1); // The width of each hexagon
-// double picwidth    = eachhex * (numw-(marginhexes*2)-1); // The width of each hexagon
-double omx         = sqrt(3.0)/2;
-int    numh        = 290/(eachhex*omx) + marginhexes;
-
-// Global variables
-
-vector<bool> cols;
-
-int base,dir;
-
 void hex (coo xy) {
-  pos = xy + coo(-omx,-.5);
-  rlineto (coo(0,1));
-  rlineto (coo(omx,.5));
-  rlineto (coo(omx,-.5));
-  rlineto (coo(0,-1));
-  rlineto (coo(-omx,-.5));
-  rlineto (coo(-omx,.5));
+  pos = xy + coo(-omx,-1);
+  rlineto (coo(0,2)); rlineto (coo(omx,1)); rlineto (coo(omx,-1));
+  rlineto (coo(0,-2)); rlineto (coo(-omx,-1)); rlineto (coo(-omx,1));
 }
 
 coo thepos (int i) {
-  return coo(omx*(((i/numw)%2)+2*(i%numw)) , 1.5*(i/numw));
-}
-
-void drawhexes () {
-  for (int i=0; i<numw*numh; ++i) {
-    hex(thepos(i));
-  }
+  return coo(omx*(((i/numw)%2)+2*(i%numw)) , 3*(i/numw));
 }
 
 void perc () {
-  for (int i=0; i<numw*numh; ++i) {
-    if (cols[i]) hex(thepos(i));
-  }
+  for (int i=0; i<numw*numh; ++i) if (cols[i]) hex(thepos(i));
 }
 
 int rotleft  (int d) { return (d+1)%6; }
 int rotright (int d) { return (d+5)%6; }
-
-bool spillout () {
-  return (((base+1)%numw)<2) || ((base/numw)>(numh-2));
-}
 
 int follow (int dir, int base) {
   int fola[6] = { 1, numw, numw-1, -1, -numw-1, -numw };
@@ -108,7 +73,7 @@ void walk () {
   base = numw/2-1;
   dir = 0;
   bool basebit = cols[base];
-  while (!spillout()) {
+  while (((base+1)%numw >= 2) && (base/numw <= numh-2)) {
     segment(rotleft);
     if (cols[thenext()]==basebit) {
       base = thenext();
@@ -121,12 +86,10 @@ void walk () {
 }
 
 int main (int argc, char ** argv) {
-  cerr << eachhex << endl;
   for (int i=0; i < numw/2; ++i)        cols.push_back(true);
   for (int i=0; i < numw/2; ++i)        cols.push_back(false);
   for (int i=0; i < (numw-1)*numh; ++i) cols.push_back(rand()<rand());
   // perc();
-  // drawhexes();
   walk();
   return 0;
 }
