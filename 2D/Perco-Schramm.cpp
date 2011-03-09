ad164546abf5d60434f1c87afa55e9c3dc9518ac
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-03-09 02:57:06 +0100

    Mostly cosmetic changes.

--- 2D/Perco-Schramm.cpp
+++ 2D/Perco-Schramm.cpp
@@ -7,15 +7,14 @@
 
 #include <vb/Figure.h>
 
-using namespace std;
 using namespace vb;
 
 double omx = sqrt(3.0);
 
-class Perco_Schramm : public Figure {
+class Perco_Schramm : public vb::Figure {
 public:
   int w, h;
-  vector<bool> cols;
+  std::vector<bool> cols;
   cpx pos;
   
   Perco_Schramm (int w_, int h_) : w(w_), h(h_) {
@@ -24,10 +23,7 @@ public:
     for (int i=0; i < (w-1)*h; ++i) cols.push_back (rand()<rand());
   }
   
-  void lineto (cpx xy) {
-    segment (pos,xy);
-    pos = xy;
-  }
+  void lineto (cpx xy) { segment (pos,xy); pos = xy; }
   
   void hex (cpx xy) {
     pos = xy + cpx(-omx,-1);
@@ -35,36 +31,30 @@ public:
     lineto (pos + cpx(0,-2)); lineto (pos + cpx(-omx,-1)); lineto (pos + cpx(-omx,1));
   }
   
-  cpx thepos (int i) {
-    return cpx(omx*(((i/w)%2)+2*(i%w)) , 3*(i/w));
-  }
+  cpx thepos (int i) { return cpx(omx*(((i/w)%2)+2*(i%w)) , 3*(i/w)); }
   
-  void perc () {
-    for (int i=0; i<w*h; ++i) if (cols[i]) hex(thepos(i));
-  }
+  void perc () { for (int i=0; i<w*h; ++i) if (cols[i]) hex(thepos(i)); }
   
-  int follow (int dir, int base) {
-    int fola[6] = { 1, w, w-1, -1, -w-1, -w };
-    int folb[6] = { 1, w+1, w, -1, -w, -w+1 };
+  int follow (int base, int dir) {
+    static int fola[6] = { 1, w, w-1, -1, -w-1, -w };
+    static int folb[6] = { 1, w+1, w, -1, -w, -w+1 };
     return ((base/w)%2 ? folb : fola) [dir] + base;
   }
   
-  int thenext(int dir, int base) { return follow ((dir+1)%6, base); }
+  int thenext (int base, int dir) { return follow (base, (dir+1)%6); }
   
   void seg (int base, int dir, int rot) {
     cpx x1y1 = thepos(base);
-    cpx x2y2 = thepos(follow(dir,base));
-    cpx x3y3 = thepos(follow((dir+rot)%6,base));
-    pos = (x1y1+x2y2) * .5;
-    lineto ((x1y1+x2y2+x3y3) * (1.0/3));
+    cpx x2y2 = thepos(follow(base,dir));
+    cpx x3y3 = thepos(follow(base,(dir+rot)%6));
+    segment ((x1y1+x2y2)*.5, (x1y1+x2y2+x3y3)*(1.0/3));
   }
 
   void walk () {
-    int base = w/2-1;
-    int dir = 0;
+    int base = w/2-1, dir = 0;
     while (((base+1)%w >= 2) && (base/w <= h-2)) {
       seg (base,dir,1);
-      if (cols[thenext(dir,base)]) { base = thenext(dir,base); dir = (dir+5)%6; }
+      if (cols[thenext(base,dir)]) { base = thenext(base,dir); dir = (dir+5)%6; }
       else                         {                           dir = (dir+1)%6; }
       seg (base,dir,5);
     }
@@ -72,9 +62,7 @@ public:
 };
 
 int main (int argc, char ** argv) {
-  Perco_Schramm RS(60,70);
-  RS.walk();
-  RS.show();
-  RS.pause();
+  Perco_Schramm RS (60,70);
+  RS.walk(); RS.show(); RS.pause();
   return 0;
 }
