4dbe6cd310aeab7d827a00565553a4f7df2dd46b
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-05-29 21:18:44 +0200

    Thinner lines in the circle packing test

--- tests/test_lattice.cpp
+++ tests/test_lattice.cpp
@@ -7,12 +7,7 @@ using namespace vb;
 using namespace std;
 
 int main (int argc, char ** argv) {
-
-  cpx tau (.5, sqrt(3.0)/2);
-
-  // Circle-pack the chosen lattice:
-
-  Lattice L = C5();
+  Lattice L = SO();
   L.relax(1e-14);
   L.tau = L.tau_rw();
   L.optimize (cost_cp);
@@ -34,13 +29,13 @@ int main (int argc, char ** argv) {
   for (int i=0; i<R.w; ++i)
     for (int j=0; j<R.h; ++j)
       for (int k=0; k<L.n; ++k)
-        F.add (new Circle (L(i,j,k), L.r[k]));
+        F.add (new Circle (L(i,j,k), L.r[k], Pen(0,.2)));
 
   for (int i=0; i<R.w; ++i)
     for (int j=0; j<R.h; ++j)
       for (int k=0; k<L.n; ++k)
         for (int l=0; l<L.adj[k].size(); ++l)
-          F.add (new Segment (L(i,j,k), L(i,j,k) + L.shift(k,l)));
+          F.add (new Segment (L(i,j,k), L(i,j,k) + L.shift(k,l), Pen(0,.1)));
 
   F.show(); F.pause(); F.output_pdf();
   return 0;
