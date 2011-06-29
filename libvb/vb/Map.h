12a880d11115509334b86830c72d0fb11bf93d51
Vincent Beffara <vbeffara@ens-lyon.fr>
2011-06-29 18:52:52 +0200

    Document the Map.h file

--- libvb/vb/Map.h
+++ libvb/vb/Map.h
@@ -13,9 +13,9 @@ namespace vb {
 
   /** A vertex of a vb::Map.
    *
-   * It inherits from vb::Circle, because it has a position and a radius 
-   * (for circle packings), but this is certainly not optimal, 
-   * especially since we don't relate vb::Map to vb::Figure much anymore 
+   * It inherits from vb::Circle, because it has a position and a radius
+   * (for circle packings), but this is certainly not optimal,
+   * especially since we don't relate vb::Map to vb::Figure much anymore
    * ...
    */
 
@@ -32,10 +32,10 @@ namespace vb {
 
   /** The main class for a planar map.
    *
-   * It is essentially a graph structure with ordered adjacency lists. 
-   * Many functions are provided for building maps, walking through 
-   * them, circle-packing them, etc. Most of that is probably contained 
-   * in the Boost library, or implementable using it, but not that 
+   * It is essentially a graph structure with ordered adjacency lists.
+   * Many functions are provided for building maps, walking through
+   * them, circle-packing them, etc. Most of that is probably contained
+   * in the Boost library, or implementable using it, but not that
    * easily I believe.
    */
 
@@ -52,8 +52,8 @@ namespace vb {
 
       /** The constructor for a vb::MAp with no edges.
        *
-       * Of course the size of the map (number of vertices) can be 
-       * changed afterwards, but adding edges with the << operator will 
+       * Of course the size of the map (number of vertices) can be
+       * changed afterwards, but adding edges with the << operator will
        * not do it.
        *
        * @param nn The number of vertices in the map.
@@ -63,8 +63,8 @@ namespace vb {
 
       /** Find an edge in the map.
        *
-       * Given an edge, find it in one of the adjacency lists of the 
-       * vb::Map; returns NULL if the edge is not present, else return 
+       * Given an edge, find it in one of the adjacency lists of the
+       * vb::Map; returns NULL if the edge is not present, else return
        * an iterator for easier use later.
        *
        * @param e The edge to look for.
@@ -77,7 +77,7 @@ namespace vb {
 
       /** Add an edge to the map.
        *
-       * This looks for an edge in the map, and before it in the 
+       * This looks for an edge in the map, and before it in the
        * adjacency list it adds a new edge with the specified endpoint.
        *
        * @param e  The edge to look for.
@@ -88,7 +88,7 @@ namespace vb {
 
       /** Add an edge to the map.
        *
-       * This looks for an edge in the map, and after it in the 
+       * This looks for an edge in the map, and after it in the
        * adjacency list it adds a new edge with the specified endpoint.
        *
        * @param e  The edge to look for.
@@ -99,9 +99,9 @@ namespace vb {
 
       /** Compute the face associated to an edge.
        *
-       * This returns, as a list of vertex indices, the border of the 
-       * face determined by the edge e. If all goes well (meaning if the 
-       * vb::Map structure is valid), the face is homeomorphic to a 
+       * This returns, as a list of vertex indices, the border of the
+       * face determined by the edge e. If all goes well (meaning if the
+       * vb::Map structure is valid), the face is homeomorphic to a
        * circle, and its border is oriented along e.
        *
        * @param e The base edge of the face.
@@ -111,8 +111,8 @@ namespace vb {
 
       /** Position the given vertices as a regular polygon.
        *
-       * This is useful e.g. for embedding the graph, when the list 
-       * given as first parameter is the outer face of a planar graph, 
+       * This is useful e.g. for embedding the graph, when the list
+       * given as first parameter is the outer face of a planar graph,
        * but this need not be the case.
        *
        * @param face_ext The list of vertices to put along a circle.
@@ -124,20 +124,22 @@ namespace vb {
 
       /** Produce a balanced embedding of the map.
        *
-       * This means that every non-boundary point is the barycenter of 
-       * its neighbors; boundary points (those specified by bd) are not 
+       * This means that every non-boundary point is the barycenter of
+       * its neighbors; boundary points (those specified by bd) are not
        * affected, so it is a good idea to use inscribe() before that.
+       *
+       * @return The energy of the final configuration.
        */
 
       double balance ();
 
       /** Produce a balanced embedding of the map.
        *
-       * This means that every non-boundary point is the barycenter of 
-       * its neighbors; boundary points (those specified by bd) are not 
+       * This means that every non-boundary point is the barycenter of
+       * its neighbors; boundary points (those specified by bd) are not
        * affected, so it is a good idea to use inscribe() before that.
        *
-       * The algorithm is the old one (iterated relaxation), kept for 
+       * The algorithm is the old one (iterated relaxation), kept for
        * further reference.
        */
 
@@ -149,7 +151,7 @@ namespace vb {
 
       /** Inscribe a triangle in the given hexagon.
        *
-       * This is performed when doing barycentric subdivision of a map; 
+       * This is performed when doing barycentric subdivision of a map;
        * if f does not have 6 elements, it does nothing.
        *
        * @param f The hexagon to split into 4 triangles.
@@ -159,9 +161,9 @@ namespace vb {
 
       /** Perform barycentric subdivision of the map.
        *
-       * This (at the moment) only affects faces that are triangles. To 
-       * exclude a face from being split (e.g. the outer face), add an 
-       * inner vertex to it before the subdivision, and it will be 
+       * This (at the moment) only affects faces that are triangles. To
+       * exclude a face from being split (e.g. the outer face), add an
+       * inner vertex to it before the subdivision, and it will be
        * protected.
        */
 
@@ -210,12 +212,12 @@ namespace vb {
 
       /** Maps each vertex of the map using the given Möbius transform.
        *
-       * The Möbius map is given by one complex parameter w and one real 
-       * parameter theta. w is mapped to 0, and theta is the rotation 
-       * term (at least if w=0, if not then the meaning of theta is 
+       * The Möbius map is given by one complex parameter w and one real
+       * parameter theta. w is mapped to 0, and theta is the rotation
+       * term (at least if w=0, if not then the meaning of theta is
        * quite arbitrary).
        *
-       * Note that this does not care for the radii of the circles; it 
+       * Note that this does not care for the radii of the circles; it
        * will break tangencies.
        *
        * @param w     The point that will be mapped to 0.
@@ -226,21 +228,21 @@ namespace vb {
 
       /** Find the minimum of the given function.
        *
-       * This tries to find a map embedding minimizing the given cost 
-       * function. If it is faster to compute the difference in costs 
-       * between one embedding and an updated embedding where exactly 
-       * one of the vertices is moved and/or has its radius updated, the 
-       * algorithm can be hinted about that by giving it a helper 
+       * This tries to find a map embedding minimizing the given cost
+       * function. If it is faster to compute the difference in costs
+       * between one embedding and an updated embedding where exactly
+       * one of the vertices is moved and/or has its radius updated, the
+       * algorithm can be hinted about that by giving it a helper
        * function.
        *
-       * The algorith is very stupid, but works reasonably efficiently 
-       * (for instance, for circle packings when minimizing the sum of 
-       * squares of mismatches, it is actually quicker than ACPA in most 
-       * cases), except when it doesn't: there is no guarantee of 
+       * The algorith is very stupid, but works reasonably efficiently
+       * (for instance, for circle packings when minimizing the sum of
+       * squares of mismatches, it is actually quicker than ACPA in most
+       * cases), except when it doesn't: there is no guarantee of
        * convergence or anything.
        *
-       * This works in-place (i.e. it affects the embedding of the 
-       * current map), and returns the value of the cost function for 
+       * This works in-place (i.e. it affects the embedding of the
+       * current map), and returns the value of the cost function for
        * the final embedding.
        *
        * @param  func       The cost function to minimize.
@@ -251,14 +253,14 @@ namespace vb {
 
       /** Produce a circle packing of the map.
        *
-       * This produces a circle packing whose combinatorics are given by 
-       * the map. It only makes sense if the map is planar, in which 
-       * case the circle configuration will be contained in a disk and 
+       * This produces a circle packing whose combinatorics are given by
+       * the map. It only makes sense if the map is planar, in which
+       * case the circle configuration will be contained in a disk and
        * the specified disks will be internally tangent to it.
        *
-       * Uniqueness is obtained by specifying which vertex should be 
-       * mapped to 0, and which one (not necessarily one of the 
-       * neighbors of the former) is to land on the positive real axis.  
+       * Uniqueness is obtained by specifying which vertex should be
+       * mapped to 0, and which one (not necessarily one of the
+       * neighbors of the former) is to land on the positive real axis.
        * If those two are the same, who knows what will happen.
        *
        * @param _zero The index of the vertex to place at the origin.
@@ -270,30 +272,32 @@ namespace vb {
 
       /** Compute the labels for a circle packing.
        *
-       * This uses the algorithm given by Collins and Stephenson in "A 
-       * circle packing algorithm" (Comput. Geom. 25 (2003), no. 3, 
-       * 233--256), hence the name of the method. It is very stable, 
-       * reasonably quick (though apparently not as quick as 
-       * optimize() in the "good" cases ...), and does not depend on the 
+       * This uses the algorithm given by Collins and Stephenson in "A
+       * circle packing algorithm" (Comput. Geom. 25 (2003), no. 3,
+       * 233--256), hence the name of the method. It is very stable,
+       * reasonably quick (though apparently not as quick as
+       * optimize() in the "good" cases ...), and does not depend on the
        * genus.
        *
-       * Note that there is no ramification, i.e. the total angle at 
+       * Note that there is no ramification, i.e. the total angle at
        * every vertex is 2pi.
        *
-       * Again, boundary vertices are not affected, so one can fix their 
-       * radii in advance. If no vertex is marked as a boundary vertex, 
+       * Again, boundary vertices are not affected, so one can fix their
+       * radii in advance. If no vertex is marked as a boundary vertex,
        * it is not clear what happens.
+       *
+       * @return The value of the cost-function in the final state.
        */
 
       double ACPA ();
 
       /** Given radii for each vertex, compute an embedding of the map.
        *
-       * This is typically used after ACPA(). The algorithm used is 
-       * very stupid, and only makes sense for planar maps. The vertices 
+       * This is typically used after ACPA(). The algorithm used is
+       * very stupid, and only makes sense for planar maps. The vertices
        * given as parameter have to be neighbors, unfortunately.
        *
-       * @todo Find a better algorithm, and possibly a way to make it 
+       * @todo Find a better algorithm, and possibly a way to make it
        * work at least for genus 1.
        *
        * @param _zero The index of a vertex to be mapped to the origin.
@@ -308,11 +312,11 @@ namespace vb {
 
       /** Apply a Möbius transform to the circles of the embedding.
        *
-       * It is similar to mobius() but works on circles instead, so it 
-       * does preserve tangencies. The parameter describe a circle that 
+       * It is similar to mobius() but works on circles instead, so it
+       * does preserve tangencies. The parameter describe a circle that
        * is mapped to a circle centered at the origin.
        *
-       * There is no rotation parameter here, so one should call 
+       * There is no rotation parameter here, so one should call
        * rotate() afterwards if needed.
        *
        * @param w The center of the circle to move to the origin.
@@ -351,48 +355,51 @@ namespace vb {
    * @param x       The list of point coordinates (3 per vertex).
    * @param g       The vector in which to compute the gradient.
    * @param context A pointer to a vb::Map.
+   * @return        The distance to being a circle packing.
    */
 
   double Map_fg_balance (const Vector &x, Vector &g, void *context);
 
   /** Compute the square distance to a circle packing.
    *
-   * This is meant as a base to "real" circle packing helpers, and it 
-   * imposes no constraint at all on the packing except the tangency 
+   * This is meant as a base to "real" circle packing helpers, and it
+   * imposes no constraint at all on the packing except the tangency
    * structure.
    *
    * @param x       The list of point coordinates (3 per vertex).
    * @param g       The vector in which to compute the gradient.
    * @param context A pointer to a vb::Map.
+   * @return        The distance to being a circle packing.
    */
 
   double Map_fg_circle_base (const Vector &x, Vector &g, void *context);
 
   /** Compute the square distance to a circle packing.
    *
-   * The points at which bd[] is true are considered to be of fixed 
-   * radius, so the gradient in their radius is always 0.0. For 
+   * The points at which bd[] is true are considered to be of fixed
+   * radius, so the gradient in their radius is always 0.0. For
    * inscribing a graph in the unit circle, use Map_fg_circle_disk.
    *
    * @param x       The list of point coordinates (3 per vertex).
    * @param g       The vector in which to compute the gradient.
    * @param context A pointer to a vb::Map.
+   * @return        The distance to being a circle packing.
    */
 
   double Map_fg_circle_bd (const Vector &x, Vector &g, void *context);
 
   /** Compute the square distance to a circle packing of the unit disk.
    *
-   * The points for which bd[] is true will correspond to circles that 
+   * The points for which bd[] is true will correspond to circles that
    * are tangent to the unit circle.
    *
    * @param x       The list of point coordinates (3 per vertex).
    * @param g       The vector in which to compute the gradient.
    * @param context A pointer to a vb::Map.
+   * @return        The distance to being a circle packing.
    */
 
   double Map_fg_circle_disk (const Vector &x, Vector &g, void *context);
 }
 
 #endif
-
