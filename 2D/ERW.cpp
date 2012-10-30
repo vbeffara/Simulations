22df21e4a5b66c67cb004a5cf45a56fe79ba4510
Vincent Beffara <vbeffara@ens-lyon.fr>
2012-10-30 16:41:46 +0100

    Make ERW a proper class

--- 2D/ERW.cpp
+++ 2D/ERW.cpp
@@ -13,38 +13,35 @@ using namespace vb;
 
 class ERW : public CoarseImage {
 public:
-  ERW (int n, int L, CL_Parser CLP) : CoarseImage (n,n, str(fmt("An excited random walk (size=%d)") % n), L) {};
+  ERW (int n, CL_Parser CLP) : CoarseImage (n,n, str(fmt("An excited random walk (size=%d)") % n), pow(n,.33)) {};
+
+  void run () {
+    int x=0, y=0, n=true_width;
+
+    while ((x>=-n/2)&&(y>=-n/2)&&(x<n)&&(y<n)) {
+      int first = 1 - at(x+n/2,y+n/2);
+      putpoint (x+n/2,y+n/2,1);
+
+      if (first) {
+        if (x>abs(y))  --x;
+        if (x<-abs(y)) ++x;
+        if (y>abs(x))  --y;
+        if (y<-abs(x)) ++y;
+      } else {
+        int d = prng()&3;
+        x += dx[d]; y += dy[d];
+      }
+    }
+  }
 };
 
 int main(int argc, char ** argv)
 {
   CL_Parser CLP (argc,argv,"n=1000");
   int n = CLP('n');
-  int L = (int) pow((double)n,1./3.);
-
-  ERW img(n,L,CLP);
+  ERW img(n,CLP);
 
   img.show();
-
-  int x=0, y=0;
-
-  while ((x>=-n/2)&&(y>=-n/2)&&(x<n)&&(y<n)) {
-    int first = 1-img(x+n/2,y+n/2);
-    img.putpoint (x+n/2,y+n/2,1);
-
-    if (first) {
-      if (x>abs(y))  --x;
-      if (x<-abs(y)) ++x;
-      if (y>abs(x))  --y;
-      if (y<-abs(x)) ++y;
-    } else {
-      int d = prng()&3;
-      x = x + dx[(int)d];
-      y = y + dy[(int)d];
-    }
-
-    img.step();
-  }
-
+  img.run();
   return 0;
 }
