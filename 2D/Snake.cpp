faba2e93486b753980240cc750737b5d1529b8c1
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-01-17 18:53:24 +0100

    Rewrite Snake.cpp in terms of coo

--- 2D/Snake.cpp
+++ 2D/Snake.cpp
@@ -7,44 +7,47 @@ using namespace vb;
 
 class Snake : public CoarseImage {
   public:
-    Snake (int n_);
-    void step (int dx, int dy);
+    Snake (int n);
+    void step (coo dz);
     void shrink ();
     bool alive () const;
     void output () const;
   private:
-    std::vector<int> x,y;
+    std::vector<coo> z;
 };
 
-Snake::Snake (int n_) : CoarseImage (2*n_, 2*n_, "Self-avoiding snake", pow(n_,.333)), x(1,n_), y(1,n_) {
-  putpoint (n_, n_, 1);
+Snake::Snake (int n) : CoarseImage (2*n, 2*n, "Self-avoiding snake", pow(n,.333)), z(1,coo(n,n)) {
+  put (coo(n,n), 1);
 }
 
-void Snake::step (int dx, int dy) {
-  int nx=x.back()+dx, ny=y.back()+dy;
-  if (at(nx,ny) == 1) return;
-  x.push_back(nx); y.push_back(ny);
-  putpoint (nx,ny,1);
+void Snake::step (coo dz) {
+  coo nz = z.back()+dz;
+  if (at(nz) == 1) return;
+  z.push_back(nz);
+  put (nz,1);
 }
 
 void Snake::shrink () {
-  if (x.size() == 1) return;
-  putpoint (x.back(), y.back(), 0);
-  x.pop_back(); y.pop_back();
+  if (z.size() == 1) return;
+  put (z.back(), 0);
+  z.pop_back();
 }
 
 bool Snake::alive () const {
-  int lx=x.back(), ly=y.back();
+  coo lz=z.back();
+  int lx=real(lz), ly=imag(lz);
   return ((lx>0) && (lx<true_width-1) && (ly>0) && (ly<true_height-1));
 }
 
 void Snake::output () const {
-  OldPath P (x.size()-1, title);
-  for (unsigned i=0; i<x.size()-1; ++i) {
-    if (x[i+1]>x[i]) P[i]=0;
-    if (x[i+1]<x[i]) P[i]=2;
-    if (y[i+1]>y[i]) P[i]=1;
-    if (y[i+1]<y[i]) P[i]=3;
+  OldPath P (z.size()-1, title);
+  for (unsigned i=0; i<z.size()-1; ++i) {
+    coo dz = z[i+1]-z[i];
+    int dx = real(dz), dy = imag(dz);
+    if (dx>0) P[i]=0;
+    if (dx<0) P[i]=2;
+    if (dy>0) P[i]=1;
+    if (dy<0) P[i]=3;
   }
   P.output();
 }
@@ -58,14 +61,7 @@ int main (int argc, char ** argv) {
 
   while (S.alive()) {
     if (prng.bernoulli(e)) S.shrink(); else {
-      int dx=0, dy=0;
-      switch ((int) prng.uniform_int(4)) {
-        case 0: dx=1;  break;
-        case 1: dx=-1; break;
-        case 2: dy=1;  break;
-        case 3: dy=-1; break;
-      }
-      S.step (dx,dy);
+      S.step (dz[prng.uniform_int(4)]);
     }
   }
   S.output(); return 0;
