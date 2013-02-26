99086375981a7faac2799bfe5c06250d3e3f812a
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-02-26 14:29:33 +0100

    Beautify the snake a little bit

--- 2D/RWhull.cpp
+++ 2D/RWhull.cpp
@@ -7,25 +7,21 @@ using namespace std;
 using namespace vb;
 
 vector<int> bridge (int n, bool p=false) {
-    vector<int> v (n,0);
+    vector<int> v (n,-1);
     for (int i=0; i<n/2; ++i) v[i]=1;
-    for (int i=n-1; i>=0; --i) {
-        int j = prng.uniform_int(i+1);
-        if (i!=j) swap(v[i],v[j]);
-    }
+    for (int i=n-1; i>=0; --i) swap (v[i], v[prng.uniform_int(i+1)]);
 
     if (!p) return v;
 
-    int s=0,ms=0,mi=0;
-    for (int i=0;i<n;++i) {
-        s += 2*v[i]-1;
+    int s=0, ms=0, mi=0;
+    for (int i=0; i<n; ++i) {
+        s += v[i];
         if (s<ms) { ms=s; mi=i; }
     }
 
-    vector<int> w(n,0);
-    for (int i=mi+1; i<n; ++i) w[i-mi-1] = v[i];
-    for (int i=0; i<=mi; ++i) w[n-mi-1+i] = v[i];
-
+    vector<int> w;
+    for (int i=mi+1; i<n; ++i) w.push_back (v[i]);
+    for (int i=0; i<=mi; ++i)  w.push_back (v[i]);
     return w;
 }
 
@@ -47,41 +43,44 @@ public:
     int s;
 };
 
+class Snake : public Bitmap<Stat> {
+public:
+    Snake (int n_) : Bitmap<Stat> (6*n_,6*n_,"The path of a snake"), n(n_) {
+        p.push_back(coo(3*n,3*n));
+        put (p.back(),1); show();
+    };
+    void grow (int d) {
+        p.push_back (p.back() + dz[d]);
+        at(p.back()) += 1; step();
+    }
+    void shrink() { p.pop_back(); }
+
+    int n;
+    vector<coo> p;
+};
+
 int main (int argc, char ** argv) {
     CL_Parser CLP (argc,argv,"n=50,i,v");
-    int n=CLP('n');
-    int l=n*n*n*n;
-    bool inf=CLP('i');
-    bool vid=CLP('v');
+    int n=CLP('n'), l=n*n*n*n;
+    bool inf=CLP('i'), vid=CLP('v');
 
-    Bitmap<Stat> img(6*n,6*n,"Random Walk Snake hull");
-
-    vector<coo> p;
-    p.push_back(coo(3*n,3*n));
-    img.at(p.back()) = 1;
-    img.show();
+    Snake S(n);
 
     if (inf) {
-        if (vid) img.snapshot_setup ("RWSH",1.0);
-        while (abs(p.back() - coo(3*n,3*n)) < 3*n-1) {
-            if ((p.size()==1) || (prng.bernoulli(.5))) {
-                p.push_back (p.back() + dz[prng.uniform_int(4)]);
-                img.at(p.back()) += 1; img.step();
-            } else {
-                p.pop_back();
-            }
+        if (vid) S.snapshot_setup ("RWSH",1.0);
+        while (abs(S.p.back() - coo(3*n,3*n)) < 3*n-1) {
+            if ((S.p.size()==1) || (prng.bernoulli(.5))) {
+                S.grow(prng.uniform_int(4));
+            } else S.shrink();
         }
     } else {
         vector<int> b = bridge (l,true);
         for (int i=0; i<l; ++i) {
             if (b[i]==1) {
-                p.push_back (p.back() + dz[prng.uniform_int(4)]);
-                img.at(p.back()) += 1;
-            } else {
-                p.pop_back();
-            }
+                S.grow(prng.uniform_int(4));
+            } else S.shrink();
         }
     }
 
-    img.pause();
+    S.pause();
 }
