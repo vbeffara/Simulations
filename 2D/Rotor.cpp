929651dace520f9d3031c856c3047cf4f45d0aee
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-03-08 11:56:40 +0100

    Newer style rotor-router

--- 2D/Rotor.cpp
+++ 2D/Rotor.cpp
@@ -7,26 +7,24 @@ using namespace vb;
 const Color C[4] = { Color(255,0,0), Color(0,255,0), Color(0,0,255), Color(255,255,0) };
 
 int main (int argc, char ** argv) {
-  CL_Parser CLP (argc, argv, "n=500");
-  int n = CLP('n');
+	CL_Parser CLP (argc, argv, "n=500");
+	int n = CLP('n');
 
-  Image img (n,n,"The Rotor-Router model");
-  if (n<=500) img.show();
+	Image img (n,n,"The Rotor-Router model");
+	ProgressBar PB (n*n*.785398, 2);
 
-  int x=n/2, y=n/2;
+	img.show();
+	coo z(n/2,n/2);
 
-  { ProgressBar PB (n*n*.785398, 2);
-    int surf=0;
+	int surf=0;
 
-    while ((x>0) && (x<n-1) && (y>0) && (y<n-1)) {
-      Color c = img.at(x,y);
-      if      (c == C[0]) { img.putpoint (x,y,C[1]); ++x; }
-      else if (c == C[1]) { img.putpoint (x,y,C[2]); ++y; }
-      else if (c == C[2]) { img.putpoint (x,y,C[3]); --x; }
-      else if (c == C[3]) { img.putpoint (x,y,C[0]); --y; }
-      else                { img.putpoint (x,y,C[0]); x=n/2; y=n/2; PB.set(++surf); }
-    }
-  }
+	while (img.contains(z)) {
+		Color c = img.at(z);
+		if (c == Color(0,0,0,0)) {
+			img.put (z,C[0]); z=coo(n/2,n/2); PB.set(++surf);
+		} else for (int i=0; i<4; ++i)
+			if (c == C[i]) { img.put (z,C[(i+1)%4]); z+=dz[i]; }
+	}
 
-  img.output_png();
+	img.output_png();
 }
