1914537d314f2137833c23d86357eac86439f4fb
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-03-11 19:37:17 +0100

    Start looking for blocked sites

--- 2D/PercSEP.cpp
+++ 2D/PercSEP.cpp
@@ -9,17 +9,19 @@ class Site {
 public:
 	Site (int i) : state(i) {};
 	operator Color() {
-		if (state == 1) return Color(64);
+		if (state == 1) return Color(128);
 		if (state == 2) return RED;
+		if (state == 3) return GREEN;
 		return BLACK;
 	}
-	int state; // 0 closed, 1 empty, 2 occupied
+	int state; // 0 closed, 1 empty, 2 occupied, 3 blocked
 };
 
 int main (int argc, char ** argv) {
-	CL_Parser CLP (argc,argv,"n=400,p=.8,l=.3,d=0");
+	CL_Parser CLP (argc,argv,"n=400,p=.8,l=.3,d=0,t");
 	int n = CLP('n');
 	double p = CLP('p'), l = CLP('l'), d = CLP('d');
+	bool tasym = CLP('t');
 
 	Bitmap<Site> P (n,n,"Exclusion in Percolation");
 	P.show();
@@ -32,14 +34,21 @@ int main (int argc, char ** argv) {
 	coo flow(0);
 
 	for (long t=1 ;; ++t) {
-		if ((t%(100*n*n))==0)
+		if ((t%(100*n*n))==0) {
 			std::cout << t/(100*n*n) << " " << real(flow) << " " << double(real(flow))/t << std::endl;
+			flow = 0;
+		}
 		coo z (prng.uniform_int(n),prng.uniform_int(n));
 		if (P.at(z).state != 2) continue;
+		if (tasym	&& ((P.atp(z+1).state%3)==0)
+		         	&& ((P.atp(z+coo(0,1)).state%3)==0)
+		         	&& ((P.atp(z+coo(0,-1)).state%3)==0)) {
+		    P.put(z,3); continue;
+		}
 		coo s = prng.bernoulli(d) ? 1 : dz[prng.uniform_int(4)];
-		coo nz ((real(z+s)+n)%n, (imag(z+s)+n)%n);
-		if (P.at(nz).state != 1) continue;
-		P.put(z,1); P.put(nz,2);
+		if (tasym && (s==-1)) continue;
+		if (P.atp(z+s).state != 1) continue;
+		P.put(z,1); P.putp(z+s,2);
 		flow += s;
 	}
 }
