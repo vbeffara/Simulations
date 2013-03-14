44962c2d272693ef374896239815bb00ece34e75
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-03-14 19:57:27 +0100

    Shuffle PercSEP around a bit

--- 2D/PercSEP.cpp
+++ 2D/PercSEP.cpp
@@ -5,22 +5,20 @@
 
 using namespace vb;
 
+const Color C[4] = { BLACK, Color(90), GREEN, Color(128,0,0) };
+
 class Site {
 	int state; // 0 closed, 1 empty, 2 occupied, 3 blocked
 public:
 	Site (int i) : state(i) {};
-	operator int() const { return state; }
-	operator Color() const {
-		if (state == 1) return Color(90);
-		if (state == 2) return GREEN;
-		if (state == 3) return Color(128,0,0);
-		return BLACK;
-	}
+	operator int()   const { return state; }
+	operator Color() const { return C[state]; }
 };
 
 class PercSEP : public Bitmap<Site> {
 public:
-	PercSEP (CL_Parser & CLP) : Bitmap<Site> (2*int(CLP('n')), CLP('n'), "Exclusion in Percolation") {
+	PercSEP (CL_Parser & CLP) : Bitmap<Site> (2*int(CLP('n')), CLP('n'), "Exclusion in Percolation"),
+			flow(0), d(CLP('d')), tasym(CLP('t')) {
 		for (int i=0; i<w(); ++i)
 			for (int j=0; j<h(); ++j)
 				if (prng.bernoulli(CLP('p')))
@@ -50,29 +48,30 @@ public:
 			if (at(coo(x,y)) == 101) at(coo(x,y)) = 1;
 		}
 	}
+	void move() {
+		coo z (prng.uniform_int(w()), prng.uniform_int(h()));   	if (at(z) < 2) return;
+		coo s = prng.bernoulli(d) ? 1 : dz[prng.uniform_int(4)];	if (atp(z+s) != 1) return;
+		if (tasym && (s==-1)) return;
+		std::swap (at(z), atp(z+s));
+		step(); flow += s;
+	}
+	coo flow;
+	double d;
+	bool tasym;
 };
 
 int main (int argc, char ** argv) {
 	CL_Parser CLP (argc,argv,"n=400,p=.8,l=.3,d=0,t");
-	double d = CLP('d');
-	bool tasym = CLP('t');
-
 	PercSEP P(CLP); P.show();
-
-	coo flow(0);
 
 	for (long t=1 ;; ++t) {
 		if ((t%(P.w()*P.h()))==0) {
-			if (tasym) P.clean();
+			if (P.tasym) P.clean();
 			int na=0; for (int x=0;x<P.w();++x) for (int y=0;y<P.h();++y) if (P.at(coo(x,y))==2) ++na;
 			if (na==0) exit(0);
-			std::cout << t/(P.w()*P.h()) << " " << na << " " << real(flow) << " " << double(real(flow))/na << std::endl;
-			flow = 0;
+			std::cout << t/(P.w()*P.h()) << " " << na << " " << real(P.flow) << " " << double(real(P.flow))/na << std::endl;
+			P.flow = 0;
 		}
-		coo z (prng.uniform_int(P.w()),prng.uniform_int(P.h())); if (P.at(z) < 2) continue;
-		coo s = prng.bernoulli(d) ? 1 : dz[prng.uniform_int(4)]; if (P.atp(z+s) != 1) continue;
-		if (tasym && (s==-1)) continue;
-		std::swap (P.at(z), P.atp(z+s));
-		P.step(); flow += s;
+		P.move();
 	}
 }
