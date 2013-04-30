21171734f2e86d2e595a42b22cd0df394dccd652
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-04-30 22:33:04 +0200

    Start refactoring Perco-Schramm

--- 2D/Perco-Schramm.cpp
+++ 2D/Perco-Schramm.cpp
@@ -11,88 +11,85 @@ using namespace vb;
 
 double omx = sqrt(3.0);
 
-Color cc[2] = { Color(160,160,255), Color(255,255,100) };
-
 class Perco_Schramm : public Figure {
 public:
-  Perco_Schramm (int w_, int h_) : w(w_), h(h_), mask(w*h,true) {
-    title = "Perco_Schramm";
+	Perco_Schramm (int w_, int h_) : w(w_), h(h_), mask(w*h,true) {
+		title = "Perco_Schramm";
 
-    // Ipe7's mediumorchid4 : 0.478 0.216 0.545
-    p = new Path (std::vector<cpx>(0), Pen(Color(122,55,139),4));
+		p = new Path (std::vector<cpx>(0), Pen(BLUE,4));
 
-    for (int i=0; i < w/2; ++i)     cols.push_back (true);
-    for (int i=0; i < w/2; ++i)     cols.push_back (false);
-    for (int i=0; i < (w-1)*h; ++i) cols.push_back (rand()<rand());
-  }
+		for (int i=0; i < w/2; ++i)     cols.push_back (true);
+		for (int i=0; i < w/2; ++i)     cols.push_back (false);
+		for (int i=0; i < (w-1)*h; ++i) cols.push_back (rand()<rand());
+	}
 
-  void tri_boundary () {
-    for (int j=0; j<h; ++j) {
-      for (int i=0; i<w; ++i)
-        mask[i+w*j] = (i <= (w+j)/2) && (i >= (w-j)/2-1) && (j<h-1);
-      cols[(w-j)/2 + w*j - 1] = true;
-      cols[(w+j)/2 + w*j] = false;
-    }
-  }
+	void tri_boundary () {
+		for (int j=0; j<h; ++j) {
+			for (int i=0; i<w; ++i)
+				mask[i+w*j] = (i <= (w+j)/2) && (i >= (w-j)/2-1) && (j<h-1);
+			cols[(w-j)/2 + w*j - 1] = true;
+			cols[(w+j)/2 + w*j] = false;
+		}
+	}
 
-  void rect_boundary () {
-    for (int j=0; j<h-1; ++j) {
-      cols[w*j] = true;
-      cols[w*j+h] = false;
-    }
-  }
+	void rect_boundary () {
+		for (int j=0; j<h-1; ++j) {
+			cols[w*j] = true;
+			cols[w*j+h] = false;
+		}
+	}
 
-  void perc () { for (int i=0; i<w*h; ++i) if (mask[i]) hex(thepos(i), cc[cols[i]]); }
+	void perc () { for (int i=0; i<w*h; ++i) if (mask[i]) hex(thepos(i), cols[i] ? CYAN : YELLOW); }
 
-  void walk () {
-    int base = w/2-1, dir = 0;
-    p->z.push_back (thepos(base) + cpx(omx,-1));
-    add(p);
-    while (((base+1)%w >= 2) && (base/w <= h-2)) {
-      seg (base,dir,1);
-      if (cols[thenext(base,dir)]) { base = thenext(base,dir); dir = (dir+5)%6; }
-      else                         {                           dir = (dir+1)%6; }
-      seg (base,dir,5);
-    }
-  }
+	void walk () {
+		int base = w/2-1, dir = 0;
+		p->z.push_back (thepos(base) + cpx(omx,-1));
+		add(p);
+		while (((base+1)%w >= 0) && (base/w <= h-2)) {
+			seg (base,dir,1);
+			if (cols[thenext(base,dir)]) { base = thenext(base,dir); dir = (dir+5)%6; }
+			else                         {                           dir = (dir+1)%6; }
+			seg (base,dir,5);
+		}
+	}
 
 private:
-  int w, h;
-  std::vector<bool> cols, mask;
-  Path *p;
+	int w, h;
+	std::vector<bool> cols, mask;
+	Path *p;
 
-  void hex (cpx xy, Color c) {
-    std::vector<cpx> coo;
-    coo.push_back(xy + cpx(omx,1));  coo.push_back(xy + cpx(0,2));
-    coo.push_back(xy + cpx(-omx,1)); coo.push_back(xy + cpx(-omx,-1));
-    coo.push_back(xy + cpx(0,-2));   coo.push_back(xy + cpx(omx,-1));
-    add (new Polygon(coo, Pen(0,1,c,1)));
-  }
+	void hex (cpx xy, Color c) {
+		std::vector<cpx> coo;
+		coo.push_back(xy + cpx(omx,1));  coo.push_back(xy + cpx(0,2));
+		coo.push_back(xy + cpx(-omx,1)); coo.push_back(xy + cpx(-omx,-1));
+		coo.push_back(xy + cpx(0,-2));   coo.push_back(xy + cpx(omx,-1));
+		add (new Polygon(coo, Pen(0,1,c,1)));
+	}
 
-  cpx thepos (int i) { return cpx(omx*(((i/w)%2)+2*(i%w)) , 3*(i/w)); }
+	cpx thepos (int i) { return cpx(omx*(((i/w)%2)+2*(i%w)) , 3*(i/w)); }
 
-  int follow (int base, int dir) {
-    static int fola[6] = { 1, w, w-1, -1, -w-1, -w };
-    static int folb[6] = { 1, w+1, w, -1, -w, -w+1 };
-    return ((base/w)%2 ? folb : fola) [dir] + base;
-  }
+	int follow (int base, int dir) {
+		static int fola[6] = { 1, w, w-1, -1, -w-1, -w };
+		static int folb[6] = { 1, w+1, w, -1, -w, -w+1 };
+		return ((base/w)%2 ? folb : fola) [dir] + base;
+	}
 
-  int thenext (int base, int dir) { return follow (base, (dir+1)%6); }
+	int thenext (int base, int dir) { return follow (base, (dir+1)%6); }
 
-  void seg (int base, int dir, int rot) {
-    cpx x1y1 = thepos(base);
-    cpx x2y2 = thepos(follow(base,dir));
-    cpx x3y3 = thepos(follow(base,(dir+rot)%6));
-    p->z.push_back ((x1y1+x2y2+x3y3)*(1.0/3));
-  }
+	void seg (int base, int dir, int rot) {
+		cpx x1y1 = thepos(base);
+		cpx x2y2 = thepos(follow(base,dir));
+		cpx x3y3 = thepos(follow(base,(dir+rot)%6));
+		p->z.push_back ((x1y1+x2y2+x3y3)*(1.0/3));
+	}
 };
 
 int main (int argc, char ** argv) {
-  Perco_Schramm RS (60,59);
-  RS.size (700,600);
-  //RS.tri_boundary();
-  RS.rect_boundary();
-  RS.perc();
-  RS.walk(); RS.show(); RS.pause(); RS.output();
-  return 0;
+	Perco_Schramm RS (60,59);
+	RS.size (700,600);
+	RS.tri_boundary();
+	// RS.rect_boundary();
+	RS.perc();
+	RS.walk(); RS.show(); RS.pause(); RS.output();
+	return 0;
 }
