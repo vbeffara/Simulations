7bca25b012d28e84609156589670996de490cf0a
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-05-10 16:54:58 +0200

    Make test_console use prng_uniform rather than rand()

--- tests/test_console.cpp
+++ tests/test_console.cpp
@@ -1,8 +1,8 @@
 
 #include <vb/Console.h>
+#include <vb/PRNG.h>
 
 using namespace vb;
-using namespace std;
 
 double x2 (void *x) {
   double X = *(double*)x; return X*2;
@@ -12,18 +12,18 @@ int main (int argc, char ** argv) {
   double x=1.1;
 
   Console W1;
-  W1.watch (new Value<double> (x,"x"));
+  W1.watch (x,"x");
   W1.show();
 
   for (unsigned long i=0; i<3e8; ++i) {
-    x = rand();
+    x = prng.uniform_real();
     global_clock.step();
   }
 
   W1.watch (new Value_calc<double> (x2,&x,"Double of x"));
 
   for (unsigned long i=0; i<3e8; ++i) {
-    x = rand();
+    x = prng.uniform_real();
     global_clock.step();
   }
 
