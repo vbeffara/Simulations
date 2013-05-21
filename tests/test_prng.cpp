b9043a0f1fc9ca4efb365688aef237060dd54b74
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-05-21 14:23:16 +0200

    One more PRNG test, storing state

--- tests/test_prng.cpp
+++ tests/test_prng.cpp
@@ -6,34 +6,42 @@ using namespace vb;
 using namespace std;
 
 int main (int argc, char ** argv) {
-    CL_Parser CLP (argc,argv,"n=500000000");
+    CL_Parser CLP (argc,argv,"n=100000000");
     int n=CLP('n');
     long long s;
     double t=0;
 
-    cerr << "vb::PRNG::bernoulli() ...               ";
+    ostringstream store; store << prng;
+
+    cerr << "vb::PRNG::bernoulli() ...              \ttime = ";
     s=0; t=global_clock.time();
     for (int i=0; i<n; ++i) s += prng.bernoulli();
-    cerr << global_clock.time()-t << ", sum = " << s << endl;
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
 
-    cerr << "direct vb::PRNG() ...                   ";
+    cerr << "direct vb::PRNG() ...                  \ttime = ";
     s=0; t=global_clock.time();
     for (int i=0; i<n; ++i) s += prng()%2;
-    cerr << global_clock.time()-t << ", sum = " << s << endl;
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
 
-    cerr << "boost::bernoulli_distribution ...       ";
+    cerr << "boost::bernoulli_distribution ...      \ttime = ";
     s=0; t=global_clock.time();
     boost::bernoulli_distribution<> dist(.5);
     for (int i=0; i<n; ++i) s += dist(prng);
-    cerr << global_clock.time()-t << ", sum = " << s << endl;
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
 
-    cerr << "many boost::bernoulli_distributions ... ";
+    cerr << "many boost::bernoulli_distributions ...\ttime = ";
     s=0; t=global_clock.time();
     for (int i=0; i<n; ++i) s += (boost::bernoulli_distribution<> (.5))(prng);
-    cerr << global_clock.time()-t << ", sum = " << s << endl;
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
 
-    cerr << "UNIX rand() ...                         ";
+    cerr << "UNIX rand() ...                        \ttime = ";
     s=0; t=global_clock.time();
     for (int i=0; i<n; ++i) s += rand()%2;
-    cerr << global_clock.time()-t << ", sum = " << s << endl;
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
+
+    cerr << "Stored state, first version ...        \ttime = ";
+    istringstream use (store.str()); use >> prng;
+    s=0; t=global_clock.time();
+    for (int i=0; i<n; ++i) s += prng.bernoulli();
+    cerr << global_clock.time()-t << ",  \tsum = " << s << endl;
 }
