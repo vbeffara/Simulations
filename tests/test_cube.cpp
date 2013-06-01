b516d859c405e70991e8ea785ebab47aea3ee34b
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-06-01 14:32:47 +0200

    Nicer PovRay scene (unfinished)

--- tests/test_cube.cpp
+++ tests/test_cube.cpp
@@ -26,15 +26,24 @@ int main (int argc, char ** argv) {
 
 	std::cout	<< "#include \"colors.inc\"" << std::endl
 	         	<< "#include \"stones.inc\"" << std::endl
-	         	<< "camera { location <" << 2*C.sx << "," << 3*C.sy/2 << "," << -C.sz
-	         		<< "> look_at <" << C.sx/2 << "," << C.sy/3 << "," << C.sz/2 << "> angle 50 }" << std::endl
-	         	<< "light_source { <" << -C.sx << "," << 2*C.sy << "," << -2*C.sz << "> color White }" << std::endl
+	         	<< "camera { location <" << 1.75*C.sx << "," << 5*C.sy/4 << "," << -C.sz
+	         		<< "> look_at <" << C.sx/2 << "," << C.sy/2 << "," << C.sz/2 << "> angle 60 }" << std::endl
+	         	<< "light_source { <" << C.sx/4 << "," << 3*C.sy/2 << "," << -2*C.sz << "> color White*2 }" << std::endl
+	         	<< "plane { x, " << - C.sx/4 << " texture { pigment { color White } } finish { reflection {.6} ambient 0.2 diffuse 0 } }" << std::endl
+	         	<< "plane { y, " << - C.sy/4 << " texture { pigment { color White } } finish { reflection {.4} ambient 0.2 diffuse 0.4 } }" << std::endl
+	         	<< "plane { z, " << 5*C.sz/4 << " texture { pigment { color White } } finish { reflection {.6} ambient 0.2 diffuse 0 } }" << std::endl
+	         	<< "cylinder { <0,0,0>, <" << C.sx << ",0,0>, .1 texture { pigment { color Red } } }" << std::endl
+	         	<< "cylinder { <0,0,0>, <0," << C.sy << ",0>, .1 texture { pigment { color Red } } }" << std::endl
+	         	<< "cylinder { <0," << C.sy << ",0>, <" << C.sx << "," << C.sy << ",0>, .1 texture { pigment { color Red } } }" << std::endl
+	         	<< "cylinder { <" << C.sx << ",0,0>, <" << C.sx << "," << C.sy << ",0>, .1 texture { pigment { color Red transmit .5 } } }" << std::endl
+	         	<< "cylinder { <" << C.sx << "," << C.sy << "," << C.sz << ">, <" << C.sx << "," << C.sx << ",0>, .1 texture { pigment { color Red } } }" << std::endl
+	         	<< "cylinder { <0,0,0>, <0,0," << C.sz << ">, .1 texture { pigment { color Red } } }" << std::endl
 	         	<< "background { color White }" << std::endl;
 	for (int x=0; x<C.sx; ++x)
 		for (int y=0; y<C.sy; ++y)
 			for (int z=0; z<C.sz; ++z)
 				if (C.at(x,y,z)==255)
 					std::cout << "box { <" << x << "," << y << "," << z << ">,<" << x+1 << "," << y+1 << "," << z+1
-							<< "> texture { pigment { color rgbt <.5,.7,1,.2> } } }" << std::endl;
+							<< "> texture { pigment { color rgb <.5,.7,1> } } }" << std::endl;
     return 0;
 }
