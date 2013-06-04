38a88c5b1267324064467d49bd1308f0f384301d
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-06-04 02:44:28 +0200

    Some boilerplate reduction

--- libvb/Pov.cpp
+++ libvb/Pov.cpp
@@ -2,39 +2,33 @@
 #include <vb/Pov.h>
 
 namespace vb {
-    std::ostream & Pov_Camera::output_pov (std::ostream & os) {
-        return os << "camera { location " << a << " look_at " << b << " angle " << d << " }";
+    std::ostream & Pov_Object::output_pov (std::ostream & os) {
+        os << type << " { ";
+        bool b=false;
+        foreach (tri t, pts)     	{ if (b) os << ", "; os << t; b=true; }
+        foreach (double d, coefs)	{ if (b) os << ", "; os << d; b=true; }
+        pov_contents(os);
+        return os << " }";
     }
 
-    std::ostream & Pov_Light_Source::output_pov (std::ostream & os) {
-        return os << "light_source { " << a << " color White }";
-    }
+    std::ostream & Pov_Camera::pov_contents (std::ostream & os)      	{ return os << "location " << a << " look_at " << b << " angle " << d; }
+    std::ostream & Pov_Light_Source::pov_contents (std::ostream & os)	{ return os << " color White"; }
+    std::ostream & Pov_Sphere::pov_contents (std::ostream & os)      	{ return os << " texture { pigment { color Red } }"; }
+    std::ostream & Pov_Cylinder::pov_contents (std::ostream & os)    	{ return os << " texture { pigment { color Red } }"; }
+    std::ostream & Pov_Box::pov_contents (std::ostream & os)         	{ return os << " texture { pigment { color rgb <.3,.7,1> } }"; }
+    std::ostream & Pov_Plane::pov_contents (std::ostream & os)       	{ return os << " texture { pigment { color White } } finish { reflection {.8} ambient 0 diffuse 0 }"; }
 
-    std::ostream & Pov_Sphere::output_pov (std::ostream & os) {
-        return os << "sphere { " << a << ", " << r << " texture { pigment { color Red } } }";
-    }
-
-    std::ostream & Pov_Cylinder::output_pov (std::ostream & os) {
-        if (c) os << new Pov_Sphere (a,r) << new Pov_Sphere (b,r);
-        return os << "cylinder { " << a << ", " << b << ", " << r << " texture { pigment { color Red } } }";
-    }
-
-    std::ostream & Pov_Box::output_pov (std::ostream & os) {
-        return os << "box { " << a << ", " << b << " texture { pigment { color rgb <.3,.7,1> } } }";
-    }
-
-    std::ostream & Pov_Plane::output_pov (std::ostream & os) {
-        return os   << "plane { " << a << ", " << d
-                    << " texture { pigment { color White } } finish { reflection {.8} ambient 0 diffuse 0 } }";
-    }
-
-    std::ostream & Pov_Frame::output_pov (std::ostream & os) { return os
-        << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(b.x,a.y,a.z), .1, 1) << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(a.x,b.y,a.z), .1, 1)
-        << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(a.x,a.y,b.z), .1, 1) << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(b.x,a.y,a.z), .1, 1)
-        << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(a.x,b.y,a.z), .1, 1) << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(b.x,b.y,b.z), .1, 1)
-        << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(a.x,b.y,a.z), .1, 1) << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(a.x,a.y,b.z), .1, 1)
-        << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(b.x,b.y,b.z), .1, 1) << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(b.x,a.y,a.z), .1, 1)
-        << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(a.x,a.y,b.z), .1, 1) << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(b.x,b.y,b.z), .1, 1);
+    std::ostream & Pov_Frame::pov_contents (std::ostream & os) { return os
+        << new Pov_Sphere (tri(a.x,a.y,a.z), .1) << new Pov_Sphere (tri(a.x,a.y,b.z), .1)
+        << new Pov_Sphere (tri(a.x,b.y,a.z), .1) << new Pov_Sphere (tri(a.x,b.y,b.z), .1)
+        << new Pov_Sphere (tri(b.x,a.y,a.z), .1) << new Pov_Sphere (tri(b.x,a.y,b.z), .1)
+        << new Pov_Sphere (tri(b.x,b.y,a.z), .1) << new Pov_Sphere (tri(b.x,b.y,b.z), .1)
+        << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(a.x,a.y,b.z), .1) << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(a.x,b.y,a.z), .1)
+        << new Pov_Cylinder (tri(a.x,a.y,a.z), tri(b.x,a.y,a.z), .1) << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(a.x,a.y,b.z), .1)
+        << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(a.x,b.y,a.z), .1) << new Pov_Cylinder (tri(a.x,b.y,b.z), tri(b.x,b.y,b.z), .1)
+        << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(a.x,a.y,b.z), .1) << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(b.x,b.y,b.z), .1)
+        << new Pov_Cylinder (tri(b.x,a.y,b.z), tri(b.x,a.y,a.z), .1) << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(a.x,b.y,a.z), .1)
+        << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(b.x,a.y,a.z), .1) << new Pov_Cylinder (tri(b.x,b.y,a.z), tri(b.x,b.y,b.z), .1);
     }
 
     std::ostream & Pov_Scene::output_pov (std::ostream & os) {
--- libvb/vb/Pov.h
+++ libvb/vb/Pov.h
@@ -2,45 +2,47 @@
 #include <vb/common.h>
 
 namespace vb {
-    class Pov_Object { public:
-		virtual std::ostream & output_pov (std::ostream & os) =0;
+    class Pov_Object { public: std::string type; std::vector<tri> pts; std::vector<double> coefs;
+        Pov_Object (std::string s) : type(s) {};
 		virtual ~Pov_Object () {};
+        virtual std::ostream & output_pov (std::ostream & os);
+        virtual std::ostream & pov_contents (std::ostream & os) { return os; };
 	};
     std::ostream & operator<< (std::ostream & os, Pov_Object * o) { return o->output_pov(os) << std::endl; }
 
     class Pov_Camera : public Pov_Object { public: tri a,b; double d;
-		Pov_Camera (tri aa, tri bb, double dd) : a(aa), b(bb), d(dd) {}
-        std::ostream & output_pov (std::ostream & os);
+		Pov_Camera (tri aa, tri bb, double dd) : Pov_Object("camera"), a(aa), b(bb), d(dd) {}
+        std::ostream & pov_contents (std::ostream & os);
     };
 
-    class Pov_Light_Source : public Pov_Object { public: tri a;
-		Pov_Light_Source (tri aa) : a(aa) {}
-        std::ostream & output_pov (std::ostream & os);
+    class Pov_Light_Source : public Pov_Object { public:
+		Pov_Light_Source (tri a) : Pov_Object("light_source") { pts.push_back(a); }
+        std::ostream & pov_contents (std::ostream & os);
     };
 
-    class Pov_Sphere : public Pov_Object { public: tri a; double r;
-		Pov_Sphere (tri aa, double rr) : a(aa), r(rr) {}
-        std::ostream & output_pov (std::ostream & os);
+    class Pov_Sphere : public Pov_Object { public:
+		Pov_Sphere (tri a, double r) : Pov_Object("sphere") { pts.push_back(a); coefs.push_back(r); }
+        std::ostream & pov_contents (std::ostream & os);
     };
 
-    class Pov_Cylinder : public Pov_Object { public: tri a,b; double r; bool c;
-        Pov_Cylinder (tri aa, tri bb, double rr, bool cc = false) : a(aa), b(bb), r(rr), c(cc) {}
-        std::ostream & output_pov (std::ostream & os);
+    class Pov_Cylinder : public Pov_Object { public:
+        Pov_Cylinder (tri a, tri b, double r) : Pov_Object("cylinder") { pts.push_back(a); pts.push_back(b); coefs.push_back(r); }
+        std::ostream & pov_contents (std::ostream & os);
     };
 
-    class Pov_Box : public Pov_Object { public: tri a,b;
-		Pov_Box (tri aa, tri bb) : a(aa), b(bb) {}
-        std::ostream & output_pov (std::ostream & os);
+    class Pov_Box : public Pov_Object { public:
+		Pov_Box (tri a, tri b) : Pov_Object("box") { pts.push_back(a); pts.push_back(b); }
+        std::ostream & pov_contents (std::ostream & os);
     };
 
-    class Pov_Plane : public Pov_Object { public: tri a; double d;
-		Pov_Plane (tri aa, double dd) : a(aa), d(dd) {}
-        std::ostream & output_pov (std::ostream & os);
+    class Pov_Plane : public Pov_Object { public:
+		Pov_Plane (tri a, double d) : Pov_Object("plane") { pts.push_back(a); coefs.push_back(d); }
+        std::ostream & pov_contents (std::ostream & os);
     };
 
     class Pov_Frame : public Pov_Object { public: tri a,b;
-		Pov_Frame (tri aa, tri bb) : a(aa), b(bb) {}
-        std::ostream & output_pov (std::ostream & os);
+		Pov_Frame (tri aa, tri bb) : Pov_Object("union"), a(aa), b(bb) {}
+        std::ostream & pov_contents (std::ostream & os);
     };
 
     class Pov_Scene { public: std::string title; std::vector <Pov_Object*> objs;
