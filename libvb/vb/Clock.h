cedd1b96126f3db68a1b9e675711ef911587b3ee
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-06-20 19:04:56 +0200

    All of vb::Clock fits in one page

--- libvb/vb/Clock.h
+++ libvb/vb/Clock.h
@@ -2,8 +2,7 @@
 #include <vector>
 
 namespace vb {
-	class Task {
-	public:
+	class Task { public:
 		Task (long p, void t (void*), void *d) : period(p), last(-1), active(true), task(t), data(d) {}
 
 		void run () { task(data); }
@@ -16,26 +15,28 @@ namespace vb {
 		void * data;         	///< Context passed to vb::Task::task at each run.
 	};
 
-	class Clock {
-	public:
-		Clock ();
+	class Clock { public:
+		Clock () : next(1), slice(10), n_call(0) {}
 
-		long clock () const;  ///< Compute the elapsed time since creation, in clock ticks.
-		double time () const; ///< Compute the elapsed time since creation, in seconds.
-		long count () const;  ///< Compute the elapsed time since creation, in hundredths of a second.
+		long  	clock	() const	{ return ::clock();                       	}
+		double	time 	() const	{ return double(clock()) / CLOCKS_PER_SEC;	}
+		long  	count	() const	{ return time() * 100;                    	}
+		void  	step 	()      	{ ++n_call; --next; if (!next) run();     	}
 
-		void step () { ++n_call; --next; if (!next) run(); }
+		int add (long period, void task (void*), void *data = 0) { T.push_back(Task(period,task,data)); return T.size()-1; }
 
-		void run ();
+		void remove (int i) { if (i>=0) T[i].active = false; }
 
-		int add (long period, void task (void*), void *data = 0);
-
-		void remove (int i);
+		void run () {
+			for (Task & t : T) if (t.active && (count() >= t.last + t.period)) { t.run(); t.last = count(); }
+			if (count() <= 100) slice += slice/10;
+			else slice = std::min (n_call / count(), slice + slice/10);
+			next = slice;
+		}
 
 	private:
 		long next;
-		unsigned long long slice;
-		unsigned long long n_call;
+		unsigned long long slice, n_call;
 		std::vector <Task> T;
 	};
 
