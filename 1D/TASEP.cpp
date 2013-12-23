91a0a5592043746817fe880aa945a5f23615304e
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-12-23 19:50:48 +0100

    TASEP in random environment

--- 1D/TASEP.cpp
+++ 1D/TASEP.cpp
@@ -9,7 +9,8 @@ vector<Color> C { WHITE, RED, BLACK };
 class TASEP : public vector<int> {
 	public:
 		TASEP (CL_Parser & CLP) : vector<int>(int(CLP('n')),0), p(size(),1) {
-			for (int & e : *this) e = 2*prng.bernoulli(CLP('r')); 
+			for (int & e : *this) { e = 2*prng.bernoulli(CLP('r')); if (prng.bernoulli(CLP('d')) && (e==2)) e = 1; }
+			for (double & q : p) q = prng.uniform_real(CLP('e'),1.0000001);
 		}
 		void step () {
 			int i = prng.uniform_int(size()); if (prng.bernoulli(1-p[i])) return;
@@ -20,14 +21,12 @@ class TASEP : public vector<int> {
 };
 
 int main (int argc, char ** argv) {
-	CL_Parser CLP (argc,argv,"n=1200,r=.4"); TASEP T(CLP);
-	for (double & p : T.p) p = prng.uniform_real();
-	for (int & e : T) if (prng.bernoulli(.01)) e = 1;
+	CL_Parser CLP (argc,argv,"n=1280,r=.4,e=0,d=0"); TASEP T(CLP);
 
 	Image I (T.size(),700,"TASEP"); I.show();
-	for (int t=0,u=0 ;; ++t) { T.step(); if (sqrt(2*t/T.size())>u) {
-		u = sqrt(2*t/T.size());
-		for (int i=0; i<T.size(); ++i) I.put(coo(i,u%I.h()),C[T[i]]); ++u; 
-		if (u==I.h()) I.pause();
-	} }
+	for (int t=0,u=0 ;; ++t,++u) {
+		for (int j=0; j<u; ++j) for (int i=0; i<T.size(); ++i) T.step();
+		for (int i=0; i<T.size(); ++i) I.put(coo(i,t%I.h()),C[T[i]]);
+		if (t==I.h()) I.pause();
+	}
 }
