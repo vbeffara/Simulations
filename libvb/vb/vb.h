3b6fc073312f57c5413c65ab5584bfb2015685af
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-12-23 19:49:28 +0100

    One header file to include them all (mostly for doxygen)

---
+++ libvb/vb/vb.h
@@ -0,0 +1,15 @@
+#pragma once /// \file
+
+#include <vb/dxdy.h>
+#include <vb/CL_Parser.h>
+#include <vb/Console.h>
+#include <vb/Cube.h>
+#include <vb/fmt.h>
+#include <vb/Hypermap.h>
+#include <vb/Lattice.h>
+#include <vb/Map.h>
+#include <vb/Path.h>
+#include <vb/Point.h>
+#include <vb/Pov.h>
+#include <vb/ProgressBar.h>
+#include <vb/TriMatrix.h>
