91ffe3dc6878cae252084c8b4f82f7be54f5a75d
Vincent Beffara <vbeffara@ens-lyon.fr>
2013-12-26 18:52:39 +0100

    Use boost string algorithms, they work

--- libvb/Hub.cpp
+++ libvb/Hub.cpp
@@ -1,29 +1,28 @@
 #include <vb/Hub.h>
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <getopt.h>
+#include <boost/algorithm/string.hpp>
 
 namespace vb {
 	Hub::Hub (std::string t, int argc, char ** argv, std::string c) : title(t), help("Syntax : " + c), getopt_arg("h") {
-			size_t cut;
-			while ((cut=c.find(",")) != std::string::npos) { newparam (c.substr(0,cut)); c = c.substr(cut+1); }
-			if (c.length()) newparam (c);
+		std::vector<std::string> cs;
+		boost::split (cs, c, boost::is_any_of(", "), boost::token_compress_on);
+		for (auto s : cs) newparam(s);
 
-			parse(argc,argv);
+		parse(argc,argv);
 
-			std::ostringstream o;
-			if (size()) {
-				bool first = true;
-				o << " (";
-				for (auto i : *this) { o << (first ? "" : ",") << i.first << "=" << i.second; first = false; }
-				o << ")";
-			}
-			title += o.str();
+		if (size()) {
+			cs.clear();
+			for (auto i : *this) cs.push_back (std::string(1,i.first) + "=" + i.second);
+			title += " (" + boost::join (cs,", ") + ")";
 		}
+	}
 
 	void Hub::newparam (const std::string &s) {
 		if (s.length() == 1)	{ getopt_arg += s.substr(0,1);      	(*this)[s[0]] = "0";        	has_arg[s[0]] = false; }
-		else                	{ getopt_arg += s.substr(0,1) + ':';	(*this)[s[0]] = s.substr(2);	has_arg[s[0]] = true; }
+		else                	{ getopt_arg += s.substr(0,1) + ":";	(*this)[s[0]] = s.substr(2);	has_arg[s[0]] = true; }
 	}
 
 	void Hub::parse (int argc, char **argv) {
