2edb0dcbd9c2897f38782af58442f63e011d1741
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-01-07 20:44:05 +0100

    Separate bits of Auto into Auto.cpp

---
+++ libvb/Auto.cpp
@@ -0,0 +1,23 @@
+#include <vb/Auto.h>
+
+namespace vb {
+	Task::Task (TimePoint t, Duration d, std::function<void()> f) : std::function<void()> (f), next(t), period(d), active(true) {}
+
+	Auto::Auto (double t) : start(now()), next(1), slice(10), n_call(0), task {add_task(t, [this]{this->update();})} {}
+
+	Auto::~Auto () { remove_task(task); }
+
+    int Auto::add_task (double period, std::function<void()> task) { 
+		tasks.emplace_back(now(),Duration(period),task);
+		return tasks.size()-1;
+	}
+
+	void Auto::remove_task (int i) { tasks[i].active = false; }
+
+	void Auto::run () {
+		for (Task & t : tasks) if (t.active && (now() >= t.next)) { t(); t.next = now() + t.period; }
+		double time = Duration(now()-start).count();
+		if (time < 1) slice += slice/10; else slice = std::min (n_call / (time*100), slice + slice/10.0);
+		next = slice;
+	}
+}
--- libvb/vb/Auto.h
+++ libvb/vb/Auto.h
@@ -10,7 +10,7 @@ namespace vb {
 
 	class Task : public std::function<void()> {
 	public:
-		Task (TimePoint t, Duration d, std::function<void()> f) : std::function<void()> (f), next(t), period(d), active(true) {}
+		Task (TimePoint t, Duration d, std::function<void()> f);
 		TimePoint next;
 		Duration period;
 		bool active;
@@ -18,31 +18,23 @@ namespace vb {
 
 	class Auto : public std::chrono::steady_clock {
 	public:
-		Auto (double t) : start(now()), next(1), slice(10), n_call(0), task {add_task(t, [this]{this->update();})} {}
-		virtual ~Auto () { remove_task(task); }
+		Auto (double t);
+		virtual ~Auto ();
 		virtual void update() =0;
 
-		void step () { ++n_call; --next; if (!next) run(); }
+		void step () { ++n_call; --next; if (next==0) run(); }
 
-		static int add_task (double period, std::function<void()> task) { 
-			tasks.emplace_back(now(),Duration(period),task);
-			return tasks.size()-1;
-		}
-
-		static void remove_task (int i) { tasks[i].active = false; }
+		static int add_task (double period, std::function<void()> task);
+		static void remove_task (int i);
 
 		static std::vector <Task> tasks;
 		TimePoint start;
 
 	private:
-		void run () {
-			for (Task & t : tasks) if (t.active && (now() >= t.next)) { t(); t.next = now() + t.period; }
-			double time = Duration(now()-start).count();
-			if (time < 1) slice += slice/10; else slice = std::min (n_call / (time*100), slice + slice/10.0);
-			next = slice;
-		}
+		void run ();
 
-		unsigned long long next, slice, n_call;
+		int next, slice;
+		long long n_call;
 		int task;
 	};
 }
