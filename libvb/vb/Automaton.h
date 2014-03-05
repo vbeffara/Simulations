4a07a02e497d10a9ce383277b4f9db58999ef594
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-03-05 14:55:59 +0100

    Teach a rule to check and apply itself

--- libvb/vb/Automaton.h
+++ libvb/vb/Automaton.h
@@ -2,27 +2,30 @@
 #include <vb/Bitmap.h>
 
 namespace vb {
-	template <class T> class Rule {
-	public:
+	template <class T> class Automaton;
+
+	template <class T> class Rule { public:
 		typedef std::vector < std::pair<int,T> > Pattern;
 
 		Rule (Pattern in, Pattern out) : cond(in), move(out) {}
+
+		bool check   (Automaton<T> &A, int i) { for (auto & c : cond) if (A.atp(i+c.first) != c.second) return false; return true; }
+		void trigger (Automaton<T> &A, int i) { if (check(A,i)) for (auto & m : move) A.atp(i+m.first) = m.second; }
+
+	private:
 		Pattern cond, move;
 	};
 
 	template <class T> class Automaton : public std::vector<T> { public:
 		Automaton (int n_, T s, std::string title, bool p = true) : std::vector<T>(n_,s), n(n_), y(0), lt(0), pause(p), I(n,500,title) {}
 
-		T      	& atp (int i)      	{ return std::vector<T>::at((i%n+n)%n); }
-		T const	& atp (int i) const	{ return std::vector<T>::at((i%n+n)%n); }
+		T & atp (int i) { int j = i%n; if (j<0) j+=n; return (*this)[j]; }
 
 		void show () { I.show(); }
 
 		void step () {
-			int i = prng.uniform_int(n); double U = prng.uniform_real(0,lt); int j=0;
-			while (U>rates[j]) U -= rates[j++];
-			for (auto & c : rules[j].cond) if (atp(i+c.first) != c.second) return;
-			for (auto & m : rules[j].move) atp(i+m.first) = m.second;
+			double U = prng.uniform_real(0,lt); int j=0; while (U>rates[j]) U -= rates[j++];
+			rules[j].trigger (*this, prng.uniform_int(n));
 		}
 
 		void swipe () {
@@ -31,9 +34,7 @@ namespace vb {
 			++y; if (y==500) { y=0; if (pause) I.pause(); }
 		}
 
-		void add_rule (double r, typename Rule<T>::Pattern in, typename Rule<T>::Pattern out) {
-			lt += r; rates.push_back(r); rules.push_back (Rule<T> (in,out));
-		}
+		void add_rule (double r, Rule<T> R) { lt += r; rates.push_back(r); rules.push_back(R); }
 
 	private:
 		std::vector<double> rates;
