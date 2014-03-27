f0f36ad7df949b07c04e351960a07f119abb2247
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-03-27 11:31:29 +0100

    Separate interface/implementation for QuadTree

---
+++ libvb/QuadTree.cpp
@@ -0,0 +1,48 @@
+#include <vb/QuadTree.h>
+
+namespace vb {
+	QuadTree::QuadTree (coo UL, coo BR, int M) :
+		n(0), ul(UL), br(BR), center((ul+br)/2), iul(br), ibr(ul), m(M), leaf(true) {};
+
+	QuadTree::~QuadTree () {
+		if (!leaf) for (int i=0; i<4; ++i) delete children[i];
+	}
+
+	int QuadTree::index (coo z) const {
+		if (z.y<center.y)	{ if (z.x<center.x) return 0; else return 1; }
+		else             	{ if (z.x<center.x) return 2; else return 3; }
+	}
+
+	void QuadTree::insert (coo z) {
+		++n;
+		iul.x = std::min(iul.x,z.x); iul.y = std::min(iul.y,z.y);
+		ibr.x = std::max(ibr.x,z.x); ibr.y = std::max(ibr.y,z.y);
+		if (leaf)	{ pts.push_back(z); if (n>m) split(); }
+		else     	{ children[index(z)] -> insert (z); }
+	}
+
+	void QuadTree::split () {
+		children[0] = new QuadTree (ul,                	center,            	m);
+		children[1] = new QuadTree (coo(center.x,ul.y),	coo(br.x,center.y),	m);
+		children[2] = new QuadTree (coo(ul.x,center.y),	coo(center.x,br.y),	m);
+		children[3] = new QuadTree (center,            	br,                	m);
+		for (auto & z : pts) children[index(z)] -> insert (z);
+		std::vector<coo>().swap(pts); leaf = false;
+	}
+
+	int QuadTree::idist (coo z) const { return std::min ({z.x-ul.x, 	z.y-ul.y, 	br.x-z.x, 	br.y-z.y}); 	}
+	int QuadTree::odist (coo z) const { return std::max ({z.x-ibr.x,	z.y-ibr.y,	iul.x-z.x,	iul.y-z.y});	}
+
+	void QuadTree::nn (coo z, QuadIndex & qi) const {
+		if (leaf) for (auto & w : pts) {
+			int newnorm = sup (z-w);
+			if (newnorm < qi.d) { qi.d = newnorm; qi.z = w; }
+		} else {
+			int i0 = index(z); if (children[i0] -> n) children[i0] -> nn (z,qi);
+			for (int i=0; i<4; ++i) if (i != i0) {
+				QuadTree *q = children[i]; if ((q -> n) && (q -> odist(z) < qi.d))
+					children[i] -> nn (z,qi);
+			}
+		}
+	}
+}
--- libvb/vb/QuadTree.h
+++ libvb/vb/QuadTree.h
@@ -2,47 +2,32 @@
 
 #include <vb/coo.h>
 #include <vector>
-#include <iostream>
 
 namespace vb {
 	struct QuadIndex { coo z; int d; };
 
-	class QuadTree { public:
-		QuadTree (coo UL, coo BR, int M, QuadTree * p = 0) : ul(UL), br(BR), center((ul+br)/2), parent(p), n(0), m(M), leaf(true) {};
-		~QuadTree () { if (!leaf) for (int i=0; i<4; ++i) delete children[i]; }
+	class QuadTree {
+	public:
+		QuadTree (coo UL, coo BR, int M);
+		~QuadTree ();
 
-		int index (coo z) const {	if (z.y<center.y)	{ if (z.x<center.x) return 0; else return 1; }
-		                         	else             	{ if (z.x<center.x) return 2; else return 3; } }
+		void insert (coo z);
 
-		void insert (coo z) { ++n; if (leaf) { pts.push_back(z); if (n>m) split(); } else { children[index(z)] -> insert (z); } }
+		void nn (coo z, QuadIndex & qi) const;
 
-		void split () {
-			children[0] = new QuadTree (coo(ul.x,ul.y),        	coo(center.x,center.y),	m, this);
-			children[1] = new QuadTree (coo(center.x,ul.y),    	coo(br.x,center.y),    	m, this);
-			children[2] = new QuadTree (coo(ul.x,center.y),    	coo(center.x,br.y),    	m, this);
-			children[3] = new QuadTree (coo(center.x,center.y),	coo(br.x,br.y),        	m, this);
-			for (auto & z : pts) children[index(z)] -> insert (z);
-			std::vector<coo>().swap(pts); leaf = false;
-		}
+		int n;
 
-		int odist (coo z) { return std::max ({z.x-br.x, z.y-br.y, ul.x-z.x, ul.y-z.y}); }
+	private:
+		int index (coo z) const;
+		int idist (coo z) const;
+		int odist (coo z) const;
 
-		void nn (coo z, QuadIndex & qi) const {
-			if (leaf) for (auto & w : pts) {
-				int newnorm = sup (z-w);
-				if (newnorm < qi.d) { qi.d = newnorm; qi.z = w; }
-			} else {
-				int i0 = index(z); children[i0] -> nn (z,qi);
-				for (int i=0; i<4; ++i) if ((i != i0) && (children[i] -> odist(z) < qi.d)) children[i] -> nn (z,qi);
-			}
-		}
+		void split ();
 
-		int dist (coo z) const { QuadIndex qi { 0, sup(z) }; nn (z,qi); return qi.d; }
-
-		coo ul,br,center;
+		coo ul,br,center,iul,ibr;
 		std::vector <coo> pts;
-		QuadTree * children[4], * parent;
-		int n,m;
+		QuadTree * children[4];
+		int m;
 		bool leaf;
 	};
 }
