830a320f1aeb007b44717d5a1d2969e3b3d320bb
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-05-28 19:20:00 +0200

    Put the action distribution weights in ACP

--- 2D/AgingCP.cpp
+++ 2D/AgingCP.cpp
@@ -5,30 +5,23 @@
 using namespace std; using namespace vb;
 
 class Site { public:
-	Site (int age) : a(age) {}
-	operator int () { return a; }
-	operator Color() {
-		if (a==0) return 0;
-		else return Indexed(a);
-	}
+	Site (int age) : a(age)	{ }
+	operator int ()        	{ return a; }
+	operator Color ()      	{ return a==0 ? BLACK : Indexed(a); }
 	int a;
 };
-
-vector<double> P { .005, .095, .2, .3, .4 };
 
 class ACP : public Bitmap<Site> { public:
 	ACP (Hub & H) : Bitmap<Site> (int(H['n']), int(H['n']), H.title) { put (coo(w()/2,h()/2),1); }
 	void run() {
 		while (true) {
-			coo z = rand();
-			int action = prng.discrete (P);
-			if (action == 0) { at(z)=0; continue; }
-			if (action == 1) { int a = at(z); if ((a>0) && (a<5)) { at(z).a ++; } continue; }
-			if (action > int(at(z))+1) continue;
-			coo nz = z + dz[prng.uniform_int(4)];
-			if (int(atp(nz)) == 0) putp(nz,1);
+			coo z = rand(); int action = prng.discrete (P);
+			if (action == 0)                	{ at(z)=0; }
+			else if (action == 1)           	{ int a = at(z); if (a>0) put(z, min(a+1,5)); }
+			else if (action-1 <= int(at(z)))	{ coo nz = z + dz[prng.uniform_int(4)]; if (atp(nz) == 0) putp(nz,1); }
 		}
 	}
+	const vector<double> P { .005, .095, .2, .3, .4 };
 };
 
 int main (int argc, char ** argv) {
