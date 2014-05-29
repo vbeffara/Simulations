c1b81ea036fb8c10ce69f3449209faab22056f5d
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-05-29 19:53:20 +0200

    Make the parameters in AgingCP all customizable

--- 2D/AgingCP.cpp
+++ 2D/AgingCP.cpp
@@ -12,19 +12,25 @@ class Site { public:
 };
 
 class ACP : public Bitmap<Site> { public:
-	ACP (Hub & H) : Bitmap<Site> (int(H['n']), int(H['n']), H.title) { put (coo(w()/2,h()/2),1); }
-	void run() {
-		while (true) {
-			coo z = rand(); int action = prng.discrete (P);
-			if (action == 0)                	{ at(z)=0; }
-			else if (action == 1)           	{ int a = at(z); if (a>0) put(z, min(a+1,5)); }
-			else if (action-1 <= int(at(z)))	{ coo nz = z + dz[prng.uniform_int(4)]; if (atp(nz) == 0) putp(nz,1); }
-		}
+	ACP (Hub & H) : Bitmap<Site> (int(H['n']), int(H['n']), H.title), kid(H['z']), maxage (H['m']) {
+		put (coo(w()/2,h()/2),1);
+		P = { H['d'], H['a'] };
+		for (int i=0; i<int(H['m']) - kid; ++i) P.push_back (double(H['b']) + double(H['r'])*i);
+		double s=0; for (double u : P) s+=u; for (double &u : P) u /= s;
 	}
-	const vector<double> P { .005, .095, .2, .3, .4 };
+	void run() {
+		step();
+		coo z = rand(); if (at(z)==0) return;
+		int action = prng.discrete (P);
+		if (action == 0)                    	{ put (z,0); }
+		else if (action == 1)               	{ int a = at(z); if (a>0) put(z, min(a+1,maxage)); }
+		else if (action-1+kid <= int(at(z)))	{ coo nz = z + dz[prng.uniform_int(4)]; if (atp(nz) == 0) putp(nz,1); }
+	}
+	int kid, maxage;
+	vector<double> P;
 };
 
 int main (int argc, char ** argv) {
-	Hub H ("Contact process with aging", argc, argv, "n=400");
-	ACP A (H); A.show(); A.run();
+	Hub H ("Contact process with aging", argc, argv, "n=400,d=.01,a=.1,z=0,m=5,b=.2,r=.1");
+	ACP A (H); A.show(); while (true) A.run();
 }
