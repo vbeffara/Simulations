4530f1888e977adbb89642dc1b63ff9c8a3d3bc8
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-06-12 15:36:03 +0200

    2D/DDLA: clean up the code a lot

--- 2D/DDLA.cpp
+++ 2D/DDLA.cpp
@@ -1,94 +1,46 @@
 #include <vb/CL_Parser.h>
-#include <vb/Image.h>
-#include <vb/PRNG.h>
 #include <vb/Point.h>
 
-using namespace vb;
-using namespace std;
-
-int cursum;
-int n,maxx,maxy;
-double p;
-bool s;
+using namespace vb; using namespace std;
 
 class DDLA : public CoarseImage {
 public:
-	DDLA (CL_Parser & CLP) : CoarseImage (CLP('n'),CLP('n'),"A directed DLA cluster", CLP('f') ? pow(double(n),.33) : 1) {};
+	DDLA (CL_Parser & CLP) : CoarseImage (CLP('n'),CLP('n'),"A directed DLA cluster", CLP('f') ? pow(CLP('n'),.33) : 1),
+			f(CLP('f')), n(CLP('n')), cursum(0), p(CLP('p')) {
+		put(0,1);
+		pq << Point(1,prng.exponential()/p) << Point(J,prng.exponential()/(1-p));
+	};
 
-	void reshape (int x, int y) {
-		if (x==maxx) ++maxx;
-		if (y==maxy) ++maxy;
-		if (x+y>cursum) cursum=x+y;
-	}
+	bool ok (coo z) {	if ( (z.x >= n) || (z.y >= n) || (z.x+z.y > cursum) ) return 1;
+	                 	if (at(z)) return 0; else return ok (z+dz[prng.bernoulli(p)]); }
 
-	bool ok (coo z) {
-		int xx=z.x, yy=z.y;
-		coo zz=z;
-		bool just_started=true;
-		int ans=-1;
+	void fill (coo z) {	if ( (z.x > 0) && (z.y < n-1) && at(z-1+J) && !at(z-1) ) { put(z-1,1); fill (z-1); }
+	                   	if ( (z.y > 0) && (z.x < n-1) && at(z+1-J) && !at(z-J) ) { put(z-J,1); fill (z-J); } }
 
-		while (ans == -1) {
-			if (prng.bernoulli(p)) { ++xx; zz+=coo(1,0); } else { ++yy; zz+=coo(0,1); }
+	void run () {
+		Point pt (pq);
 
-			if ((xx>=n) || (yy>=n) || (xx+yy > cursum)) ans = 1;
-			if (at(zz) == 1) ans = 0;
-
-			if (s && (!just_started) && (xx<n-1) && (yy<n-1) && ((at(zz+coo(1,0))==1) || (at(zz+coo(0,1))==1))) ans = 0;
-			just_started = false;
+		if ((pt.x == n-1) || (pt.y == n-1)) return;
+		if (!at(pt)) {
+			double curtime = pt.t;
+			if (ok(pt)) {
+				put(pt,1); cursum = max (cursum, pt.x+pt.y); if (f) fill (pt);
+				if ( (pt.x<n-1) && (at(pt+1)==0) ) pq << Point(pt+1,curtime+prng.exponential()/p);
+				if ( (pt.y<n-1) && (at(pt+J)==0) ) pq << Point(pt+J,curtime+prng.exponential()/(1-p));
+			} else {
+				pq << Point(pt,curtime+prng.exponential());
+			}
 		}
-
-		return ans;
-	}
-
-	void addapoint (coo z) {
-		put (z,1);
-		if (f) {
-			int x=z.x, y=z.y;
-			if ( (x>0) && (y<n-1) && (at(z-coo(1,0)+coo(0,1))==1) && (at(z-coo(1,0))==0) ) addapoint (z-1);
-			if ( (y>0) && (x<n-1) && (at(z+coo(1,0)-coo(0,1))==1) && (at(z-coo(0,1))==0) ) addapoint (z-J);
-		}
+		run ();
 	}
 
 	bool f;
+	int n,cursum;
+	double p;
+	PointQueue pq;
 };
 
-int main (int argc, char **argv) {
-	CL_Parser CLP (argc,argv,"n=500,p=.5,s,t,f");
-	n = CLP('n');
-	p = CLP('p');
-	s = CLP('s');
-
-	PointQueue queue;
-
-	maxx=1; maxy=1; cursum=0;
-
-	DDLA img (CLP);
-	img.f = CLP('f');
-	img.show();
-
-	img.put(0,1);
-	queue << Point(coo(1,0),prng.exponential()) << Point(coo(0,1),prng.exponential());
-
-	int running=1;
-	while (running) {
-		Point pt (queue);
-
-		if ((pt.x==n-1) || (pt.y==n-1)) running=0;
-		else if (img.at(pt) == 0) {
-			double curtime = pt.t;
-			if (img.ok(pt)) {
-				img.addapoint (pt);
-				img.reshape (pt.x,pt.y);
-				if ( (pt.x<n-1) && (img.at(pt+coo(1,0))==0) ) queue << Point(pt+coo(1,0),curtime+prng.exponential()/p);
-				if ( (pt.y<n-1) && (img.at(pt+coo(0,1))==0) ) queue << Point(pt+coo(0,1),curtime+prng.exponential()/(1-p));
-				if ( s && (pt.x>0) && (img.at(pt-coo(1,0))==0) ) queue << Point(pt-coo(1,0),curtime+prng.exponential());
-				if ( s && (pt.y>0) && (img.at(pt-coo(0,1))==0) ) queue << Point(pt-coo(0,1),curtime+prng.exponential());
-			} else {
-				queue << Point(pt,curtime+prng.exponential());
-			}
-		}
-	}
-
-	img.output();
-	return 0;
+int main (int argc, char ** argv) {
+	CL_Parser CLP (argc,argv,"n=750,p=.5,f");
+	DDLA img (CLP); img.show(); img.run(); img.output();
 }
