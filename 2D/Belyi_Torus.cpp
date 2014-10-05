798e4ea683482f6fcf418c3151d13a7f8717c1d9
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-10-05 11:59:43 +0200

    2d/Belyi_Torus: much better generator for pairings

--- 2D/Belyi_Torus.cpp
+++ 2D/Belyi_Torus.cpp
@@ -1,10 +1,13 @@
+#include <vb/ProgressBar.h>
 #include <vb/Toroidal.h>
 #include <cassert>
 
-// S-A+F=0. A = (3/2)F. S=A-F=F/2. a=3F.
-// sigma alpha phi = id
-
 using namespace vb; using namespace std;
+
+unsigned long long fact   	(unsigned long long n)                      	{ return (n<2 ? 1 : n*fact(n-1));     	}
+unsigned long long binom  	(unsigned long long n, unsigned long long k)	{ return fact(n)/fact(k)/fact(n-k);   	}
+unsigned long long catalan	(unsigned long long n)                      	{ return binom(2*n,n)/(n+1);          	}
+unsigned long long npair  	(unsigned long long n)                      	{ return fact(n)/fact(n/2)/pow(2,n/2);	}
 
 bool connected (Permutation s, Permutation a) {
 	int n=s.size(); vector<int> l(n); for (int i=0; i<n; ++i) l[i]=i;
@@ -17,28 +20,51 @@ bool connected (Permutation s, Permutation a) {
 	return true;
 }
 
-vector<Cycles> pairings_go (vector<unsigned> & p) {
-	if (p.size() == 0) return {};
-	if (p.size() == 2) return {{p}};
-	vector<Cycles> out; int i=p[0];
-	for (unsigned o : p) {
-		if ((o/3)==(i/3)) continue;
-		vector<unsigned> new_p;
-		for (unsigned j : p) if ((j!=i) && (j!=o)) new_p.push_back(j);
-		for (auto & c : pairings_go (new_p)) {
-			c.push_back ({p[0],o});
-			out.push_back (c);
+class Pairing_Iterator { public:
+	vector<unsigned> c;
+
+	void go (vector<unsigned> & p) {
+		if (p.size() == 0) {
+			l.push_back (Permutation(n)); Permutation &cc = l.back();
+			for (int i=0; i<n/2; ++i) { cc[c[2*i]]=c[2*i+1]; cc[c[2*i+1]]=c[2*i]; }
+			pb->set(l.size());
+		} else {
+			unsigned i=p[0];
+			for (int k=1; k<p.size(); ++k) {
+				unsigned o = p[k];
+				c.push_back(i); c.push_back(o);
+				vector<unsigned> new_p;
+				for (unsigned j : p) if ((j!=i) && (j!=o)) new_p.push_back(j);
+				go (new_p);
+				c.pop_back(); c.pop_back();
+			}
 		}
 	}
-	return out;
-}
 
-vector<Permutation> pairings (int a) {
-	vector<unsigned> all; for (int i=0; i<a; ++i) all.push_back(i);
-	vector<Permutation> out;
-	for (auto & c : pairings_go(all)) out.push_back (c);
-	return out;
-}
+	Pairing_Iterator (int n_, int i_, bool d) : n(n_), i(i_) {
+		if (d) {
+			pb = new ProgressBar (npair(n));
+			vector<unsigned> all; for (int i=0; i<n; ++i) all.push_back(i);
+			go(all);
+			delete pb;
+		}
+	};
+
+	void         	operator++	()                   	{ ++i;                            	}
+	bool         	operator!=	(Pairing_Iterator &o)	{ return (n != o.n) || (i != o.i);	}
+	Permutation &	operator* 	()                   	{ return l[i];                    	}
+
+	unsigned n,i;
+	vector<Permutation> l;
+	ProgressBar * pb;
+};
+
+class pairings { public:
+	pairings              	(int n_) : n(n_)	{                                           	}
+	Pairing_Iterator begin	()              	{ return Pairing_Iterator(n,0,true);        	};
+	Pairing_Iterator end  	()              	{ return Pairing_Iterator(n,npair(n),false);	};
+	int n;
+};
 
 int main (int argc, char ** argv) {
 	Hub H ("Toroidal enumeration", argc, argv, "f=4,m=17");
@@ -46,10 +72,7 @@ int main (int argc, char ** argv) {
 
 	Cycles phi_c; for (unsigned i=0; i<a/3; ++i) phi_c.push_back ({3*i,3*i+1,3*i+2}); Permutation phi(phi_c);
 
-	vector<Permutation> alphas = pairings(a);
-	cout << alphas.size() << " pairings." << endl << endl;
-
-	int i=0; for (Permutation & alpha : alphas) {
+	int i=0; for (Permutation alpha : pairings(a)) {
 		                                                                            	if (!connected(phi,alpha))	continue;
 		Permutation sigma = (alpha*phi).inverse(); Hypermap M (sigma,alpha,phi);    	if (M.genus() != 1)       	continue;
 		bool good=true; for (auto & c : sigma.cycles()) if (c.size()<=2) good=false;	if (!good)                	continue;
