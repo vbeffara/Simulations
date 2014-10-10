15e176d9d29248df11f1f2bf6bde87a8e6c45570
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-10-10 14:00:47 +0200

    Three more hypermaps in the library

--- libvb/vb/Hypermap_lib.h
+++ libvb/vb/Hypermap_lib.h
@@ -3,11 +3,14 @@
 
 namespace vb {
 	Hypermap H_T ();         	// Triangle on the sphere
-	Hypermap H_TT ();        	// Two triangles, this one is invalid but some of the algorithms work.
+	Hypermap H_TT ();        	// Two triangles, this one is invalid but some of the algorithms work
+	Hypermap H_TL ();        	// Triangular lattice
+	Hypermap H_TL2 ();       	// Triangular lattice with one additional vertex of degree 2
+	Hypermap H_TL3 ();       	// Triangular lattice with an eye
 	Hypermap H_H1 ();        	// This is the planar hypermap in Zvonkine's paper. Encode using cycles.
-	Hypermap H_H2 ();        	// This is a toric graph whose lift is the centered square lattice. Encode using images.
-	Hypermap H_H67 ();       	// This is H2 with one additional site.
-	Hypermap H_H3 ();        	// The envelope-house in Zvonkine.
+	Hypermap H_H2 ();        	// This is a toric graph whose lift is the centered square lattice. Encode using images
+	Hypermap H_H67 ();       	// This is H2 with one additional site
+	Hypermap H_H3 ();        	// The envelope-house in Zvonkine
 	Hypermap H_C5 ();        	// C5 graph
 	Hypermap H_SV ();        	// SV graph
 	Hypermap H_B ();         	// Bipartite version of H67
@@ -19,6 +22,9 @@ namespace vb {
 	Hypermap H_lib (std::string s) {
 		if (s == "T")   	return H_T   	();
 		if (s == "TT")  	return H_TT  	();
+		if (s == "TL")  	return H_TL  	();
+		if (s == "TL2") 	return H_TL2 	();
+		if (s == "TL3") 	return H_TL3 	();
 		if (s == "H1")  	return H_H1  	();
 		if (s == "H2")  	return H_H2  	();
 		if (s == "H67") 	return H_H67 	();
