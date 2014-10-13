70085d2b46e2e894886826a20c8ccf54311dadf3
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-10-13 17:27:59 +0200

    tests/test_hypermap: a dessin of some tree

--- tests/test_hypermap.cpp
+++ tests/test_hypermap.cpp
@@ -4,33 +4,41 @@
 
 using namespace vb; using namespace std;
 
+vector<pair<cpx,unsigned>> roots (const Spheroidal & T) {
+	vector<pair<cpx,unsigned>> out;
+
+	for (int i=0; i<T.sigma.size(); ++i) {
+		unsigned label = T.initial[i]; if (!(label&2)) continue;
+		auto & v = T.V[T.E[i].src]; cpx z = v.z; unsigned d = v.adj.size()/2;
+		bool there=false; for (auto w : out) if (w.first==z) there=true; if (there) continue;
+		if (!there) out.push_back({z,d});
+	}
+
+	return out;
+}
+
 int main (int argc, char ** argv) {
 	Hub H ("Testing",argc,argv,"n=3,m=228,d");
 	// Hypermap M { {{1},{3},{5},{0,4,2}}, {{0,1},{2,3},{4,5}}, {{0,1,2,3,4,5}} }; // Tripod
-	// Hypermap M { {{1},{5},{7},{3,4},{0,6,2}}, {{0,1},{2,3},{4,5},{6,7}}, {{0,1,2,4,5,3,6,7}} }; // Long tripod
-	Hypermap M { {{1},{5},{7},{9},{0,8,2},{4,3,6}}, {{0,1},{2,3},{4,5},{6,7},{8,9}}, {{0,1,2,4,5,6,7,3,8,9}} }; // H shape
-
-	cerr << "sigma:   " << M.sigma.passport() << endl;
-	cerr << "alpha:   " << M.alpha.passport() << endl;
-	cerr << "phi:     " << M.phi.passport()   << endl;
-	cerr << "genus:   " << M.genus()   << endl;
-
-	cerr << endl;
+	// Hypermap M { {{1},{5},{7},{3,4},{2,6,0}}, {{2,1},{0,3},{4,5},{6,7}}, {{2,1,0,4,5,3,6,7}} }; // Long tripod
+	Hypermap M { {{1},{5},{7},{9},{2,8,0},{4,3,6}}, {{2,1},{0,3},{4,5},{6,7},{8,9}}, {{2,1,0,4,5,6,7,3,8,9}} }; // H shape
 
 	if (!H['d']) M.dessin();
-	for (int i=0; i<int(H['n']); ++i) M.split_edges();
-	M.simplify();
+	M.relabel (Transposition(M.sigma.size(),0,M.alpha[0]));
 
-	Spheroidal T (M,H); T.pack();
+	for (int i=0; i<int(H['n']); ++i) M.split_edges(); Spheroidal T (M,H); T.pack();
 
-	unsigned i=0; while (!(T.initial[i]&8)) ++i; cpx z = T.V[T.E[i].src].z;
+	unsigned i=0; while (!(T.initial[i]&8)) ++i; auto & v = T.V[T.E[i].src];
+	T.linear (1,-v.z); T.inversion(); T.linear (-1/v.r,0); T.output_pdf();
 
-	T.linear (1,-z); T.inversion();
-	double mr = fabs(T.V[0].r); for (auto v : T.V) mr = std::max (mr, fabs(v.r)); T.linear (1/mr,0);
-
-	cerr << "sigma:   " << M.sigma.passport() << endl;
-	cerr << "alpha:   " << M.alpha.passport() << endl;
-	cerr << "phi:     " << M.phi.passport()   << endl;
-
+	{ cpx z; while ((z = T.V[T.E[0].src].z) != 0.0) T.mobiusto0 (z); }
+	T.linear (std::polar(1.0,-T.E[0].a));
 	T.output_pdf();
+
+	cerr << endl << endl;
+	for (auto zd : roots(T)) cerr << zd.second << " " << zd.first << endl;
+
+	cerr << endl;
+	for (auto z : roots(T)) cerr << "(z - (" << real(z.first) << " + " << imag(z.first) << " I))^" << z.second << " ";
+	cerr << endl;
 }
