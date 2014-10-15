e8e48740f5a5c27d8dc8761a8729a0be932d9f49
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-10-15 19:12:41 +0200

    tests/test_hypermap: completely refactor

--- tests/test_hypermap.cpp
+++ tests/test_hypermap.cpp
@@ -7,40 +7,55 @@
 
 using namespace vb; using namespace std;
 
-vector<pair<cpx,unsigned>> roots (const Spheroidal & T) {
-	vector<pair<cpx,unsigned>> out;
-
-	for (int i=0; i<T.sigma.size(); ++i) {
-		unsigned label = T.initial[i]; if (!(label&2)) continue;
-		auto & v = T.V[T.E[i].src]; cpx z = v.z; unsigned d = v.adj.size()/2;
-		bool there=false; for (auto w : out) if (w.first==z) there=true; if (there) continue;
-		if (!there) out.push_back({z,d});
+class Constellation { public:
+	cpx lead() {
+		Polynomial P ({1}); for (int i=0; i<b.size(); ++i) for (int j=0; j<bd[i]; ++j) P.add_root(b[i]);
+		cpx avg = 0, avg2 = 0; int d=0;
+		for (int i=0; i<w.size(); ++i) { d += wd[i]; avg += P(w[i])*cpx(wd[i]); avg2 += P(w[i])*P(w[i])*cpx(wd[i]); }
+		avg /= d; avg2 /= d; return 1.0/avg;
 	}
 
-	return out;
-}
-
-vector<pair<cpx,unsigned>> ones (const Spheroidal & T) {
-	vector<pair<cpx,unsigned>> out;
-
-	for (int i=0; i<T.sigma.size(); ++i) {
-		unsigned label = T.initial[i]; if (!(label&4)) continue;
-		auto & v = T.V[T.E[i].src]; cpx z = v.z; unsigned d = v.adj.size()/2;
-		bool there=false; for (auto w : out) if (w.first==z) there=true; if (there) continue;
-		if (!there) out.push_back({z,d});
+	void compute() {
+		Polynomial P ({1}); for (int i=0; i<b.size(); ++i) for (int j=0; j<bd[i]; ++j) P.add_root(b[i]); Ps = {P};
+		for (auto & c : Ps[0]) c *= l;
+		for (int i=0; i<b.size(); ++i) for (int j=Ps.size(); j<bd[i]; ++j) Ps.push_back(Ps.back().derivative());
+		for (int i=0; i<w.size(); ++i) for (int j=Ps.size(); j<wd[i]; ++j) Ps.push_back(Ps.back().derivative());
 	}
 
-	return out;
+	vector<cpx>     	b,w;
+	vector<unsigned>	bd,wd;
+	cpx             	l=1.0;
+
+	vector<Polynomial>	Ps;
+};
+
+ostream & operator<< (ostream & os, const Constellation & C) {
+	os << "Black vertices: " << endl;
+	for (int i=0; i<C.b.size(); ++i) {
+		os << "| " << C.bd[i] << "\t" << C.b[i];
+		for (int j=0; j<C.bd[i]; ++j) os << "\t" << C.Ps[j](C.b[i]);
+		os << endl;
+	}
+	os << endl;
+	os << "White vertices: " << endl;
+	for (int i=0; i<C.w.size(); ++i) {
+		os << "| " << C.wd[i] << "\t" << C.w[i];
+		for (int j=0; j<C.wd[i]; ++j) os << "\t" << C.Ps[j](C.w[i]);
+		os << endl;
+	}
+	os << endl;
+	os << "P[z] = " << C.Ps[0] << endl;
+	return os;
 }
 
-double cost (vector<double> xy, const Spheroidal & T) {
-	vector<pair<cpx,unsigned>> black = roots(T);
-	vector<pair<cpx,unsigned>> white = ones(T);
-	int n1 = black.size(), n2 = white.size(); assert (xy.size()==2*(n1+n2)+2);
-	for (int i=0; i<n1; ++i) { black[i].first = cpx (xy[2*i],xy[2*i+1]); }
-	for (int i=0; i<n2; ++i) { white[i].first = cpx (xy[2*n1+2*i],xy[2*n1+2*i+1]); }
+double cost (vector<double> xy, Constellation & C) {
+	int n1 = C.b.size(), n2 = C.w.size(); assert (xy.size()==2*(n1+n2)+2);
 
-	Polynomial P ({1}); for (auto z : black) for (int i=0; i<z.second; ++i) P.add_root(z.first);
+	vector<pair<cpx,unsigned>> black, white;
+	for (int i=0; i<n1; ++i) black.push_back ({cpx (xy[2*i],xy[2*i+1]),C.bd[i]});
+	for (int i=0; i<n2; ++i) white.push_back ({cpx (xy[2*n1+2*i],xy[2*n1+2*i+1]),C.wd[i]});
+
+	Polynomial P ({1}); for (int i=0; i<n1; ++i) for (int j=0; j<C.bd[i]; ++j) P.add_root(black[i].first);
 
 	for (auto & c : P) c *= cpx (xy[2*n1+2*n2],xy[2*n1+2*n2+1]);
 
@@ -50,74 +65,63 @@ double cost (vector<double> xy, const Spheroidal & T) {
 	return out;
 }
 
-pair<vector<cpx>,vector<cpx>> find (vector<cpx> bs, vector<cpx> ws, const cpx l, const Spheroidal & T) {
+void find (Constellation & C) {
 	vector<double> bw;
-	for (auto b : bs) { bw.push_back(real(b)); bw.push_back(imag(b)); }
-	for (auto w : ws) { bw.push_back(real(w)); bw.push_back(imag(w)); }
-	bw.push_back(real(l)); bw.push_back(imag(l));
-	double c = cost (bw,T), old_c = c+1, eps = sqrt(c), nc = c;
+	for (auto b : C.b) { bw.push_back(real(b)); bw.push_back(imag(b)); }
+	for (auto w : C.w) { bw.push_back(real(w)); bw.push_back(imag(w)); }
+	bw.push_back(real(C.l)); bw.push_back(imag(C.l));
+	double c = cost (bw,C), old_c = c+1, eps = sqrt(c), nc = c;
 
 	while (eps>1e-100) {
 		cout << c << " (" << eps << ")          \r";
 		old_c = c; bool flag = false;
 		for (int i=0; i<bw.size(); ++i) {
-			bw[i] += eps; nc = cost (bw,T); if (nc<c) { c=nc; flag=true; } else { bw[i] -= eps; }
-			bw[i] -= eps; nc = cost (bw,T); if (nc<c) { c=nc; flag=true; } else { bw[i] += eps; }
+			bw[i] += eps; nc = cost (bw,C); if (nc<c) { c=nc; flag=true; } else { bw[i] -= eps; }
+			bw[i] -= eps; nc = cost (bw,C); if (nc<c) { c=nc; flag=true; } else { bw[i] += eps; }
 		}
 		if (!flag) eps /= 2;
 	}
 
-	int nb = bs.size();
-	for (int i=0; i<bs.size(); ++i) bs[i] = cpx (bw[2*i],bw[2*i+1]);
-	for (int i=0; i<ws.size(); ++i) ws[i] = cpx (bw[2*nb+2*i],bw[2*nb+2*i+1]);
-	return {bs,ws};
+	int nb = C.b.size(), nw = C.w.size();
+	for (int i=0; i<C.b.size(); ++i) C.b[i] = cpx (bw[2*i],bw[2*i+1]);
+	for (int i=0; i<C.w.size(); ++i) C.w[i] = cpx (bw[2*nb+2*i],bw[2*nb+2*i+1]);
+	C.l = cpx (bw[2*nb+2*nw],bw[2*nb+2*nw+1]); C.compute();
 }
 
-void belyi (Spheroidal & T) {
-	Polynomial P ({1}); for (auto z : roots(T)) for (int i=0; i<z.second; ++i) P.add_root(z.first);
+void belyi (Constellation & C) {
+	cpx lead = C.Ps[0].back(); double lambda = pow(abs(lead),1.0/C.Ps[0].degree());
+	for (int i=1; i<C.Ps[0].size(); ++i) C.Ps[0][i] /= pow(lambda,i);
 
-	cpx avg = 0, avg2 = 0; int d=0;
-	for (auto z : ones(T)) { d += z.second; avg += P(z.first)*cpx(z.second); avg2 += P(z.first)*P(z.first)*cpx(z.second); }
-	avg /= d; avg2 /= d; for (auto & c : P) c /= avg;
-
-	cpx lead = P.back(); double lambda = pow(abs(lead),1.0/(P.size()-1));
-	for (int i=1; i<P.size(); ++i) P[i] /= pow(lambda,i);
-	T.linear(lambda); T.output_pdf();
-
-	vector<Polynomial> Ps (1,P); for (int i=0; i<P.degree(); ++i) Ps.push_back(Ps.back().derivative());
+	for (auto & z : C.b) z *= lambda; for (auto & z : C.w) z *= lambda; C.l = C.lead(); C.compute();
 
 	double err=0;
-	for (auto z : roots(T)) for (int i=0; i<z.second; ++i) err += abs(Ps[i](z.first));
-	for (auto z : ones(T))  for (int i=0; i<z.second; ++i) err += abs(Ps[i](z.first) - (i==0?1.0:0.0));
+	for (int k=0; k<C.b.size(); ++k) for (int i=0; i<C.bd[k]; ++i) err += abs(C.Ps[i](C.b[k]));
+	for (int k=0; k<C.w.size(); ++k) for (int i=0; i<C.wd[k]; ++i) err += abs(C.Ps[i](C.w[k]) - (i==0?1.0:0.0));
 
-	for (auto & c : P) {
-		if (fabs(real(c))<err) c = cpx(0,imag(c));
-		if (fabs(imag(c))<err) c = cpx(real(c),0);
+	cout << endl << endl << "Remaining error: " << err << endl;
+	cout << setprecision (log10(1/err)-1) << fixed;
+	cout << endl << C << endl;
+}
+
+Constellation marked (const Spheroidal & T) {
+	Constellation C;
+
+	for (int i=0; i<T.sigma.size(); ++i) {
+		if (!(T.initial[i]&2)) continue;
+		auto & v = T.V[T.E[i].src]; cpx z = v.z; unsigned d = v.adj.size()/2;
+		bool there=false; for (auto w : C.b) if (w==z) there=true; if (there) continue;
+		if (!there) { C.b.push_back(z); C.bd.push_back(d); }
 	}
 
-	cout << "Remaining error: " << err << endl;
-	cout << setprecision (log10(1/err));
-	cout << endl << "Black vertices:" << endl;
 
-	for (auto z : roots(T)) {
-		cout << "  " << z.second << "\t" << fixed << z.first << scientific;
-		for (int i=0; i<z.second; ++i) { cout << "\t" << Ps[i](z.first); err += abs(Ps[i](z.first)); }
-		cout << endl;
-	}
-	cout << endl << "White vertices:" << endl;
-	for (auto z : ones(T)) {
-		cout << "  " << z.second << "\t" << fixed << z.first << scientific;
-		for (int i=0; i<z.second; ++i) { cout << "\t" << Ps[i](z.first) - (i==0?1.0:0.0); err += abs(Ps[i](z.first) - (i==0?1.0:0.0)); }
-		cout << endl;
+	for (int i=0; i<T.sigma.size(); ++i) {
+		if (!(T.initial[i]&4)) continue;
+		auto & v = T.V[T.E[i].src]; cpx z = v.z; unsigned d = v.adj.size()/2;
+		bool there=false; for (auto w : C.w) if (w==z) there=true; if (there) continue;
+		if (!there) { C.w.push_back(z); C.wd.push_back(d); }
 	}
 
-	for (auto & c : P) {
-		if (fabs(real(c))<err) c = cpx(0,imag(c));
-		if (fabs(imag(c))<err) c = cpx(real(c),0);
-	}
-
-	// cout << setprecision (log10(1/err)+2);
-	cout << endl << fixed << P << endl;
+	C.l = C.lead(); C.compute(); return C;
 }
 
 int main (int argc, char ** argv) {
@@ -142,50 +146,19 @@ int main (int argc, char ** argv) {
 
 	for (int i=0; i<int(H['n']); ++i) M.split_edges(); Spheroidal T (M,H); T.pack(); cout << "               \r";
 
-	unsigned i=0; while (!(T.initial[i]&8)) ++i; auto & v = T.V[T.E[i].src];
-	T.linear (1,-v.z); T.inversion(); T.linear (-1/v.r,0); T.output_pdf();
+	unsigned i=0; while (!(T.initial[i]&8)) ++i; auto & v = T.V[T.E[i].src]; T.linear (1,-v.z); T.inversion(); T.linear (-1/v.r,0); T.output_pdf();
+	{ cpx z; while ((z = T.V[T.E[0].src].z) != 0.0) T.mobiusto0 (z); } T.linear (std::polar(1.0,-T.E[0].a)); T.output_pdf();
 
-	{ cpx z; while ((z = T.V[T.E[0].src].z) != 0.0) T.mobiusto0 (z); }
-	T.linear (std::polar(1.0,-T.E[0].a)); T.output_pdf();
+	Constellation C = marked(T);
+	cout << endl << C << endl;
 
-	Polynomial P ({1});
-	for (auto z : roots(T)) for (int i=0; i<z.second; ++i) P.add_root(z.first);
+	cpx lead = C.Ps[0].back(); double lambda = pow(abs(lead),1.0/C.Ps[0].degree());
+	for (auto & z : C.b) z *= lambda; for (auto & z : C.w) z *= lambda; C.l = C.lead(); C.compute();
 
-	cpx avg = 0, avg2 = 0; int d=0;
-	for (auto z : ones(T)) { d += z.second; avg += P(z.first)*cpx(z.second); avg2 += P(z.first)*P(z.first)*cpx(z.second); }
-	avg /= d; avg2 /= d; for (auto & c : P) c /= avg;
+	find(C);
 
-	cpx lead = P.back(); double lambda = pow(abs(lead),1.0/(P.size()-1));
-	for (int i=1; i<P.size(); ++i) P[i] /= pow(lambda,i);
-	T.linear(lambda); T.output_pdf();
+	cpx z0=C.b[0]; for (auto & z : C.b) z -= z0; for (auto & z : C.w) z -= z0;
+	cpx r = (C.b.size()>1 ? polar(1.0,-arg(C.b[1])) : polar(1.0,-arg(C.w[0]))); for (auto & z : C.b) z *= r; for (auto & z : C.w) z *= r;
 
-	vector<Polynomial> Ps (1,P); for (int i=0; i<P.degree(); ++i) Ps.push_back(Ps.back().derivative());
-	vector<cpx> bs,ws;
-
-	cout << endl << P << endl;
-	cout << endl << "Black vertices:" << endl;
-	for (auto z : roots(T)) {
-		bs.push_back(z.first);
-		cout << "  " << z.second << "\t" << z.first;
-		for (int i=0; i<z.second; ++i) cout << "\t" << Ps[i](z.first);
-		cout << endl;
-	}
-	cout << endl << "White vertices:" << endl;
-	for (auto z : ones(T)) {
-		ws.push_back(z.first);
-		cout << "  " << z.second << "\t" << z.first;
-		for (int i=0; i<z.second; ++i) cout << "\t" << Ps[i](z.first) - (i==0?1.0:0.0);
-		cout << endl;
-	}
-
-	cout << endl;
-	auto coo = find(bs,ws,P[P.degree()],T);
-	auto oldroots = roots(T); for (auto & v : T.V) for (int i=0; i<oldroots.size(); ++i) if (oldroots[i].first == v.z) v.z = coo.first[i];
-	auto oldones  = ones(T);  for (auto & v : T.V) for (int i=0; i<oldones.size();  ++i) if (oldones[i].first  == v.z) v.z = coo.second[i];
-
-	T.linear (1.0,-roots(T)[0].first);
-	if (roots(T).size()>1) T.linear (polar(1.0,-arg(roots(T)[1].first)));
-	else T.linear (polar(1.0,-arg(ones(T)[0].first)));
-
-	belyi(T);
+	belyi(C);
 }
