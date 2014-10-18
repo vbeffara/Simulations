e874ae9674af53f48e73af42a301993b3a46f6f5
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-10-18 11:50:44 +0200

    libvb/Hypermap_lib: tweak for better layout

--- libvb/vb/Hypermap_lib.h
+++ libvb/vb/Hypermap_lib.h
@@ -7,10 +7,10 @@ namespace vb {
 	Hypermap H_TL ();        	// Triangular lattice
 	Hypermap H_TL2 ();       	// Triangular lattice with one additional vertex of degree 2
 	Hypermap H_TL3 ();       	// Triangular lattice with an eye
-	Hypermap H_H1 ();        	// This is the planar hypermap in Zvonkine's paper. Encode using cycles.
+	Hypermap H_H1 ();        	// Planar hypermap, Zvonkin p.3 fig.4
 	Hypermap H_H2 ();        	// This is a toric graph whose lift is the centered square lattice. Encode using images
 	Hypermap H_H67 ();       	// This is H2 with one additional site
-	Hypermap H_H3 ();        	// The envelope-house in Zvonkine
+	Hypermap H_H3 ();        	// Planar map, Zvonkin p.4 fig.5
 	Hypermap H_C5 ();        	// C5 graph
 	Hypermap H_SV ();        	// SV graph
 	Hypermap H_B ();         	// Bipartite version of H67
