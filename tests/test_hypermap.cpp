e5d305dfe4207b59e2b6231d4219715707eccce2
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-11-04 17:29:38 +0100

    tests/test_hypermap: pause before quitting if asked

--- tests/test_hypermap.cpp
+++ tests/test_hypermap.cpp
@@ -6,12 +6,12 @@
 using namespace vb; using namespace std;
 
 int main (int argc, char ** argv) {
-	Hub H ("Testing",argc,argv,"n=2,m=228,v,l,q,g=m_cube");
+	Hub H ("Testing",argc,argv,"n=2,m=228,v,l,q,g=m_cube,p");
 
 	auto M = HLib().at(H['g']);
 	cout << M << endl;
 
-	Constellation<double> C2 {M,H,H['n']}; if (H['v']) C2.show(); C2.findm(); delete C2.img;
+	Constellation<double> C2 {M,H,H['n']}; if (H['v']) C2.show(); C2.findm(); if (H['v']&&H['p']) { C2.slow=true; C2.img->update(); C2.img->pause(); } delete C2.img;
 	if (!(H['q']||H['l'])) { C2.belyi(); cout << endl << C2; return 0; }
 
 	Constellation<long double> C3 = cconvert <double, long double> (C2); if (H['v']) C3.show(); C3.findm(); delete C3.img;
