40b0a05eb8b63c274480f1181060a9bcbbe53e4d
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-11-05 21:02:30 +0100

    tests/test_hypermap: now long double is fast enough to always use it

--- tests/test_hypermap.cpp
+++ tests/test_hypermap.cpp
@@ -6,19 +6,15 @@
 using namespace vb; using namespace std;
 
 int main (int argc, char ** argv) {
-	Hub H ("Testing",argc,argv,"n=2,m=228,v,l,q,g=m_cube,p");
+	Hub H ("Testing",argc,argv,"n=2,m=228,v,q,g=m_cube,p");
 
 	auto M = HLib().at(H['g']);
 	cout << M << endl;
 
-	Constellation<double> C2 {M,H,H['n']}; if (H['v']) C2.show(); C2.findn();
+	Constellation<long double> C {M,H,H['n']};
+	C.findn();
+	if (H['v']) { C.show(); C.belyi(); C.slow=true; C.img->update(); C.img->pause(); delete C.img; }
+	if (!H['q']) { C.belyi(); cout << endl << C; return 0; }
 
-	if (H['v']&&H['p']) { C2.belyi(); C2.slow=true; C2.img->update(); C2.img->pause(); } delete C2.img;
-	if (!(H['q']||H['l'])) { C2.belyi(); cout << endl << C2; return 0; }
-
-	Constellation<long double> C3 = cconvert <double, long double> (C2); if (H['v']) C3.show(); C3.findn(); delete C3.img;
-	if (!H['q']) { C3.belyi(); cout << endl << C3; return 0; }
-
-	// Constellation<float128>	C4 = cconvert <long double,float128> (C3); if (H['v']) C4.show(); C4.findm(); delete C4.img;
-	// C4.belyi(); cout << endl << C4;
+	Constellation<float128>	Cq = cconvert <long double,float128> (C); Cq.findm(); Cq.belyi(); cout << endl << Cq;
 }
