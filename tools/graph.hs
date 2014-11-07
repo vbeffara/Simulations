7c35cbe0f5a97423bc80e537acb94b9b0dd0c679
Vincent Beffara <vbeffara@ens-lyon.fr>
2014-11-07 18:24:12 +0100

    tools/graph.hs: option to show system includes

--- tools/graph.hs
+++ tools/graph.hs
@@ -1,20 +1,22 @@
 #!/usr/bin/env runhaskell
 
-import Data.List (isPrefixOf)
+import Data.List            (isPrefixOf)
+import System.Environment   (getArgs)
 import System.FilePath.Find
-import Text.Printf (printf)
+import Text.Printf          (printf)
 
-inc :: String -> [String]
-inc = filter (isPrefixOf "vb/") . map (drop 10 . init) . filter (isPrefixOf "#include") . lines
+inc :: Bool -> String -> [String]
+inc f = (if f then id else filter (isPrefixOf "vb/")) . map (drop 10 . init) . filter (isPrefixOf "#include") . lines
 
 main :: IO ()
 main = do
+    args <- getArgs
     putStrLn "digraph {"
     putStrLn "  ordering = \"out\""
     putStrLn "  mclimit = 100"
     files <- find always (extension ==? ".h") "libvb"
     mapM_ (print . drop 6) files
     contents <- mapM readFile files
-    let arrows = concat $ zipWith (map . (,)) (map (drop 6) files) $ map inc contents
+    let arrows = concat $ zipWith (map . (,)) (map (drop 6) files) $ map (inc ("-f" `elem` args)) contents
     mapM_ (uncurry (printf "  \"%s\" -> \"%s\"\n")) arrows
     putStrLn "}"
