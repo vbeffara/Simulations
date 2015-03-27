2fabd2dbacfa3d7ddb50e11cbdccd0734383d54d
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-03-27 11:56:54 +0100

    3D/Ising3: Use vb::Hub instead of vb::CL_Parser

--- 3D/Ising3.cpp
+++ 3D/Ising3.cpp
@@ -1,20 +1,19 @@
 #include <vb/Cube.h>
+#include <vb/Hub.h>
 #include <vb/PRNG.h>
-#include <vb/CL_Parser.h>
 #include <vb/ProgressBar.h>
-#include <iomanip>
 
 using namespace vb; using namespace std;
 
-class Ising3 : public Cube { public: CL_Parser & clp; int b; bool k; double beta; vector<double> glaub,kaw;
-	Ising3 (CL_Parser & CLP) : Cube (CLP('n'), CLP('n'), CLP('n'), "The 3d Ising model"), clp(CLP), b(0), k(CLP('k')), beta(clp('b')) {
-		for (int k=0; k<=6; ++k) glaub.push_back(exp(k*beta) / (exp(k*beta) + exp((6-k)*beta)));
+class Ising3 : public Cube { public: int b; bool k; double beta; vector<double> glaub,kaw;
+	Ising3 (int n, double k_, double b_, std::string c) : Cube (n, n, n, H.title), b(0), k(k_), beta(b_) {
+		for (int k=0; k<=6; ++k)  glaub.push_back(exp(k*beta) / (exp(k*beta) + exp((6-k)*beta)));
 		for (int k=0; k<=12; ++k) kaw.push_back(1/(1+exp(2*beta*(k-6))));
-		if     	(string(clp('c')) == "cube")     	bc_cube();
-		else if	(string(clp('c')) == "dobrushin")	bc_dobrushin();
-		else if	(string(clp('c')) == "step")     	bc_step();
-		else if	(string(clp('c')) == "hexagon")  	bc_hexagon();
-		else   	                                 	bc_bernoulli(double(clp('p')));
+		if     	(c == "cube")     	bc_cube();
+		else if	(c == "dobrushin")	bc_dobrushin();
+		else if	(c == "step")     	bc_step();
+		else if	(c == "hexagon")  	bc_hexagon();
+		else   	                  	bc_bernoulli(H['p']);
 	};
 
 	void bc_bernoulli (double p=.5)	{ for (auto c = begin(); c != end(); ++c) put(c, prng.bernoulli(p)                      	? 255 : 0);	b=0; }
@@ -42,18 +41,18 @@ class Ising3 : public Cube { public: CL_Parser & clp; int b; bool k; double beta
 };
 
 int main (int argc, char ** argv) {
-	CL_Parser CLP (argc, argv, "n=50,b=1,t=0,p=.5,k,c=bernoulli|cube|dobrushin|hexagon|step,s=0,m,i");
-	int T = CLP('t'); if (T==0) T = 2*int(CLP('n'));
-    Ising3 C (CLP); C.show();
-    { ProgressBar P (T); int s = CLP('s'); for (int t=0; t<T; ++t) {
+	H.init ("3D Ising model", argc,argv, "n=50,b=1,t=0,p=.5,k,c=bernoulli,s=0,m,i");
+    Ising3 C (H['n'], H['k'], H['b'], H['c']); C.show();
+	int T = H['t']; if (T==0) T = 2*int(H['n']);
+    { ProgressBar P (T); int s = H['s']; for (int t=0; t<T; ++t) {
 		if (s && (t % (T/s)) == 0) {
 			std::ostringstream os; os << "snapshot_" << std::setw(4) << std::setfill('0') << t/(T/s);
 			C.output_pov (os.str());
 		}
 		C.swipe(); P.set(t);
 	} }
-	if (CLP('m')) C.mirrors=false;
-	if (CLP('i')) for (auto c = C.begin(); c != C.end(); ++c) C.put(c, (c.y>C.sy/2) ? C.at(c)/2 : C.at(c));
+	if (H['m']) C.mirrors=false;
+	if (H['i']) for (auto c = C.begin(); c != C.end(); ++c) C.put(c, (c.y>C.sy/2) ? C.at(c)/2 : C.at(c));
 
 	C.output_pov();
 }
