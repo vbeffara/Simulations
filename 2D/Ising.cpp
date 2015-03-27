0ee2bd66b1b68a956f432cf4e08c4eda77104b73
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-03-27 14:05:02 +0100

    2D/Ising: complete rewrite

--- 2D/Ising.cpp
+++ 2D/Ising.cpp
@@ -1,80 +1,40 @@
-#include <vb/CL_Parser.h>
+#include <vb/Hub.h>
 #include <vb/Image.h>
-#include <vb/PRNG.h>
 
-using namespace vb;
+using namespace vb; using namespace std;
 
-int main(int argc, char *argv[])
-{
-  long i,x,y,xy,f1,f2,nstep;
-  unsigned long p[5];
+const Color LEFT(BLACK), RIGHT(WHITE);
+const double betac = log(1+sqrt(2));
 
-  /* arguments -> beta et n
-   * beta_critical = log(1+sqrt(2)) = 0.88137359...
-   */
+int main(int argc, char *argv[]) {
+	H.init ("Ising model", argc,argv, "b=.88137359,n=300");
+	int n = H['n'];
+	double beta = H['b'];
 
-  CL_Parser CLP (argc,argv,"b=.88137359,n=300");
-  double beta = CLP('b');
-  long n = CLP('n');
+	Image img(n,n, H.title);
 
-  Image img(n,n, "An Ising configuration");
+	if (beta > betac)	{ for (int x=1; x<n-1; x++) for (int y=1; y<n-1; y++) img.put(coo(x,y), x<(n>>1)          	? LEFT : RIGHT);	}
+	else             	{ for (int x=1; x<n-1; x++) for (int y=1; y<n-1; y++) img.put(coo(x,y), prng.bernoulli(.5)	? LEFT : RIGHT);	}
 
-  /* conditions initiales / au bord */
+	for (int i=0; i<(n>>1); i++)	{ img.put(coo(i,0),LEFT);   	img.put(coo(i,n-1),LEFT); 	}
+	for (int i=(n>>1); i<n; i++)	{ img.put(coo(i,0),RIGHT);  	img.put(coo(i,n-1),RIGHT);	}
+	for (int i=0; i<n; i++)     	{ img.put(coo(0,i),LEFT);   	                          	}
+	for (int i=0; i<n; i++)     	{ img.put(coo(n-1,i),RIGHT);	                          	}
 
-#define LEFT 0
-#define RIGHT 255
+	img.show();
 
-  if (beta > log(1+sqrt(2.0))) { /* Basse temp. */
-    for (x=1;x<n-1;x++) {
-      for (y=1;y<n-1;y++) {
-        img.put(coo (x,y), (x<(n>>1)?LEFT:RIGHT));
-      }
-    }
-  } else { /* Haute temp. */
-    for (x=1;x<n-1;x++) {
-      for (y=1;y<n-1;y++) {
-        img.put(coo (x,y), (prng.bernoulli(.5)?LEFT:RIGHT));
-      }
-    }
-  }
+	vector<double> p(5,0); for (int i=0; i<5; ++i) p[i] = exp(-i*beta);
+	int nstep = 10 + n * 0.01 / fabs(beta-betac);
 
-  for (i=0;i<(n>>1);i++) { img.put(coo (i,0),LEFT);  img.put(coo (i,n-1),LEFT); }
-  for (i=0;i<n;i++)      { img.put(coo (0,i),LEFT); }
-  for (i=(n>>1);i<n;i++) { img.put(coo (i,0),RIGHT); img.put(coo (i,n-1),RIGHT); }
-  for (i=0;i<n;i++)      { img.put(coo (n-1,i),RIGHT); }
+	for (int i=0; i<nstep; i++) {
+		for (int x=1; x<n-1; x++) {
+			for (int y=1; y<n-1; y++) {
+				coo z(x,y); int s=0;
+				for (int d=0; d<4; ++d) if (img.at(z) == img.at(z+dz[d])) s++; else s--;
+				if ( (s<=0) || (prng.bernoulli(p[s])) ) img.put(coo(x,y),(255-img.at(z)));
+			}
+		}
+	}
 
-  img.show();
-
-  /* Initialisations */
-
-  p[0]=0;
-  p[1]=(long)(exp(-beta) * prng.max());
-  p[2]=(long)(exp(-2*beta) * prng.max());
-  p[3]=(long)(exp(-3*beta) * prng.max());
-  p[4]=(long)(exp(-4*beta) * prng.max());
-
-  /* Simulation */
-
-  nstep = (int) ( 10 + n * 0.01 / fabs(beta-log(1.0+sqrt(2.0))) );
-
-  for (i=0;i<nstep;i++) { /* Ouais bof */
-    for (x=1;x<n-1;x++) {
-      for (y=1;y<n-1;y++) {
-        f1 = 0; f2 = 0; xy = x + n*y;
-
-        if (img.at(coo(xy,0)) == img.at(coo(xy-1,0))) { f2++; } else { f1++; }
-        if (img.at(coo(xy,0)) == img.at(coo(xy-n,0))) { f2++; } else { f1++; }
-        if (img.at(coo(xy,0)) == img.at(coo(xy+1,0))) { f2++; } else { f1++; }
-        if (img.at(coo(xy,0)) == img.at(coo(xy+n,0))) { f2++; } else { f1++; }
-
-        if ( (f2<=f1) || (prng() < p[f2-f1]) ) {
-          img.put(coo(x,y),(255-img.at(coo(xy,0))));
-        }
-      }
-    }
-  }
-
-  img.output();
-
-  return 0;
+	img.output();
 }
