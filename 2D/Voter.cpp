51fd07a2e611550cd074b1fbc802ff44d0d9b0d4
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-03-27 11:16:58 +0100

    2D/Voter: complete rewrite

--- 2D/Voter.cpp
+++ 2D/Voter.cpp
@@ -1,48 +1,22 @@
-
-#include <vb/CL_Parser.h>
+#include <vb/Hub.h>
 #include <vb/Image.h>
-#include <vb/PRNG.h>
 
 using namespace vb;
 
+class Voter : public Image { public:
+	Voter (int n, double p, int d) : Image (n,n,H.title) {
+		for (auto & v : *this)
+			if (d>1)	v = Indexed(prng.uniform_int(d));
+			else    	v = prng.bernoulli(p) ? WHITE : BLACK;
+	}
+
+	void up () { coo z = rand(); put(z,atp(z+dz[prng.uniform_int(4)])); }
+};
+
 int main (int argc, char **argv) {
-  CL_Parser CLP (argc,argv,"n=500,p=.5,d=1");
-  int n = CLP('n');
-  double p = CLP('p');
-  int d = CLP('d');
+	H.init ("Voter model", argc,argv, "n=500,p=.5,d=1");
 
-  Image img (n,n,"Voter model");
+	Voter V (H['n'],H['p'],H['d']);
 
-  for (int i=0;i<n;++i) {
-    for (int j=0;j<n;++j) {
-      if (d>1) img.put(coo(i,j),(int)prng.uniform_int((1<<d)));
-      else img.put(coo(i,j),prng.bernoulli(p));
-    }
-  }
-
-  img.show();
-
-  for (long t=0;;++t) {
-    int i = (int)prng.uniform_int(n);
-    int j = (int)prng.uniform_int(n);
-    char newcolor = img.at(coo(i,j));
-
-    switch ((int)prng.uniform_int(4)) {
-      case 0:
-        if (i<n-1) newcolor = img.at(coo(i+1,j));
-        break;
-      case 1:
-        if (i>0) newcolor = img.at(coo(i-1,j));
-        break;
-      case 2:
-        if (j<n-1) newcolor = img.at(coo(i,j+1));
-        break;
-      case 3:
-        if (j>0) newcolor = img.at(coo(i,j-1));
-        break;
-    }
-    img.put(coo(i,j),newcolor);
-  }
-
-  return 0;
+	V.show(); while (true) V.up();
 }
