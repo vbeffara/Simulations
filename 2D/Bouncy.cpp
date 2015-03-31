73cf18aa997eb24af098e0e00ec9aa28fd3a43e9
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-03-31 15:58:07 +0200

    2D/Bouncy: new program, bouncy IDLA

---
+++ 2D/Bouncy.cpp
@@ -0,0 +1,34 @@
+#include <vb/CoarseImage.h>
+#include <vb/Hub.h>
+#include <map>
+
+using namespace vb; using namespace std;
+
+template <typename T> int sign (T val) { return (T(0) < val) - (val < T(0)); }
+
+class Bouncy : public CoarseImage { public:
+	Bouncy (int n, function<coo(coo)> j) : CoarseImage (n,n,pow(n,.25)), jump(j) { z0 = {n/2,n/2}; };
+
+	void run () {
+		coo z (0,0); while (contains(z)) {
+			int already = at(z); put(z,1);
+			z += already ? dz[prng.uniform_int(4)] : jump(z);
+		}
+	}
+
+	function <coo(coo)> jump;
+};
+
+int main (int argc, char ** argv) {
+	map <string, function<coo(coo)>> jumps;
+
+	jumps.emplace ( "none", [](coo z) {                          	return coo(0,                  	0);                  	} );
+	jumps.emplace ( "line", [](coo z) { double p = H['p'];       	return coo(- p*z.x,            	- p*z.y);            	} );
+	jumps.emplace ( "idla", [](coo z) {                          	return coo(- z.x,              	- z.y);              	} );
+	jumps.emplace ( "sign", [](coo z) {                          	return coo(- sign(z.x),        	- sign(z.y));        	} );
+	jumps.emplace ( "sqrt", [](coo z) { double l = sqrt(norm(z));	return coo(- sign(z.x)*sqrt(l),	- sign(z.y)*sqrt(l));	} );
+
+	H.init ("Bouncy internal DLA", argc,argv, "n=2000,j=sqrt,p=.01");
+	Bouncy B (H['n'],jumps.at(H['j']));
+	B.show(); B.run(); B.pause();
+}
