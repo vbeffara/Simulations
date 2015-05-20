deea648cabae2730b0a4dc065ec6b2aee4697fe5
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-05-20 17:19:19 +0200

    3D/Ising3: store starting configurations in a map for easier extensibility

--- 3D/Ising3.cpp
+++ 3D/Ising3.cpp
@@ -6,25 +6,10 @@
 using namespace vb; using namespace std;
 
 class Ising3 : public Cube { public: int b; bool k; double beta; vector<double> glaub,kaw;
-	Ising3 (int n, double k_, double b_, std::string c) : Cube (n, n, n), b(0), k(k_), beta(b_) {
+	Ising3 (int n, double k_, double b_) : Cube (n, n, n), b(0), k(k_), beta(b_) {
 		for (int k=0; k<=6; ++k)  glaub.push_back(exp(k*beta) / (exp(k*beta) + exp((6-k)*beta)));
 		for (int k=0; k<=12; ++k) kaw.push_back(1/(1+exp(2*beta*(k-6))));
-		if     	(c == "cube")     	bc_cube();
-		else if	(c == "dobrushin")	bc_dobrushin();
-		else if	(c == "step")     	bc_step();
-		else if	(c == "hexagon")  	bc_hexagon();
-		else   	                  	bc_bernoulli(H['p']);
 	};
-
-	void bc_bernoulli (double p=.5)	{ for (auto c = begin(); c != end(); ++c) put(c, prng.bernoulli(p)                      	? 255 : 0);	b=0; }
-	void bc_dobrushin ()           	{ for (auto c = begin(); c != end(); ++c) put(c, c.y<sy/2                               	? 255 : 0);	b=1; }
-	void bc_step ()                	{ for (auto c = begin(); c != end(); ++c) put(c, (c.y<sy/2) || ((c.y==sy/2)&&(c.z>sz/2))	? 255 : 0);	b=1; }
-	void bc_cube ()                	{ for (auto c = begin(); c != end(); ++c) put(c, 0); b=0;
-	    for (int x=sx/10; x<9*sx/10; ++x) for (int y=sy/10; y<9*sy/10; ++y) for (int z=sz/10; z<9*sz/10; ++z) put(coo3(x,y,z), 255);
-	}
-	void bc_hexagon () { b=1; for (int i=0; i<sx; ++i) for (int j=0; j<sx; ++j) {
-		put (coo3(i,j,sz-1), 255); put (coo3(0,i,j), 255); put (coo3(i,0,j), 255);
-	}}
 
 	int nbsum (coo3 c) { int S=0; for (int i=0; i<6; ++i) S += atp(c+dz3[i]) ? 1 : 0; return S; }
 
@@ -40,9 +25,24 @@ class Ising3 : public Cube { public: int b; bool k; double beta; vector<double> 
 	void swipe () { for (int t=0; t<sx*sy*sz; ++t) { coo3 c = rand(b); spin(c); } }
 };
 
+class BCs : public map <string, function<void(Ising3&)>> { public:
+	BCs () {
+		emplace ("bernoulli", [](Ising3 &I) { I.b=0; for (auto c = I.begin(); c != I.end(); ++c) if (prng.bernoulli(H['p'])) I.put(c,255); } );
+		emplace ("dobrushin", [](Ising3 &I) { I.b=1; for (auto c = I.begin(); c != I.end(); ++c) if (c.y<I.sy/2) I.put(c,255); } );
+		emplace ("step", [](Ising3 &I) { I.b=1; for (auto c = I.begin(); c != I.end(); ++c) if ((c.y<I.sy/2) || ((c.y==I.sy/2)&&(c.z>I.sz/2))) I.put(c,255); } );
+		emplace ("111",  [](Ising3 &I) { I.b=1; for (auto c = I.begin(); c != I.end(); ++c) if (c.x+c.y+(I.sz-c.z) < (I.sx+I.sy+I.sz)/2) I.put(c,255); } );
+		emplace ("hexagon", [](Ising3 &I) { I.b=1; for (int i=0; i<I.sx; ++i) for (int j=0; j<I.sx; ++j) {
+			I.put (coo3(i,j,I.sz-1), 255); I.put (coo3(0,i,j), 255); I.put (coo3(i,0,j), 255);
+		}} );
+		emplace ("cube", [](Ising3 &I) { I.b=0;
+		    for (int x=I.sx/10; x<9*I.sx/10; ++x) for (int y=I.sy/10; y<9*I.sy/10; ++y) for (int z=I.sz/10; z<9*I.sz/10; ++z) I.put(coo3(x,y,z), 255);
+		} );
+	}
+};
+
 int main (int argc, char ** argv) {
 	H.init ("3D Ising model", argc,argv, "n=50,b=1,t=0,p=.5,k,c=bernoulli,s=0,m,i");
-    Ising3 C (H['n'], H['k'], H['b'], H['c']); C.show();
+    Ising3 C (H['n'], H['k'], H['b']); BCs()[H['c']] (C); C.show();
 	int T = H['t']; if (T==0) T = 2*int(H['n']);
     { ProgressBar P (T); int s = H['s']; for (int t=0; t<T; ++t) {
 		if (s && (t % (T/s)) == 0) {
