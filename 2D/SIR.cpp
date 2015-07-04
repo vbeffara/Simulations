3234c25b8d78b88156edfef847c64b82c9282f2b
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-07-04 15:30:15 +0200

    2D/SIR: manage a vector of active vertices, much faster!

--- 2D/SIR.cpp
+++ 2D/SIR.cpp
@@ -8,27 +8,27 @@ const Color none(0), prey(GREEN), pred(RED);
 class SIR : public Image { public:
     SIR (int n, double l_) : Image (n,n), l (l_) {
         int n0 = H['d'] ? 0 : int(H['n'])/2;
-        for (int i=n0-10; i<n0+10; ++i) for (int j=n0-10; j<n0+10; ++j)
-            if (contains(coo(i,j))) { put(coo(i,j),prey); ++ np; }
-        put (coo(n0,n0),pred); -- np;
+        for (int i=n0-10; i<n0+10; ++i) for (int j=n0-10; j<n0+10; ++j) if (contains(coo(i,j))) put(coo(i,j),prey);
+        put (coo(n0,n0),pred);
+        for (auto c = begin(); c!= end(); ++c) if (*c == prey) fringe.push_back(c);
         show();
     }
 
-    void step () {
-        coo z = rand(); if (at(z) != prey) return;
+    void go () {
+        int i = prng.uniform_int(fringe.size()); coo z = fringe[i];
         coo nz = z + dz[prng.uniform_int(H['d'] ? 2 : 4)]; if (!contains(nz)) return;
-        if ((at(nz) == none) && ((l>1) || prng.bernoulli(l)))   { put(nz,prey); ++ np; }
-        if ((at(nz) == pred) && ((l<1) || prng.bernoulli(1/l))) { put(z,pred);  -- np; }
+        if ((at(nz) == none) && ((l>1) || prng.bernoulli(l)))   { put(nz,prey); fringe.push_back(nz); }
+        if ((at(nz) == pred) && ((l<1) || prng.bernoulli(1/l))) { put(z,pred); fringe[i] = fringe.back(); fringe.pop_back(); }
     }
 
-    int np = 0;
     double l;
+    std::vector<coo> fringe;
 };
 
 int main (int argc, char ** argv) {
-    H.init ("SIR process on the lattice", argc, argv, "n=600,l=.5,d");
+    H.init ("SIR process on the lattice", argc, argv, "n=600,l=.5,d,s=1");
 
     SIR img (H['n'],H['l']);
-    while (img.np>0) img.step();
+    for (int t=0; ! img.fringe.empty(); ++t) { img.go(); if (t % int(H['s']) == 1) img.update(); }
     img.output();
 }
