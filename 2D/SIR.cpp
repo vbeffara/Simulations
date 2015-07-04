77c84382a76eb782b3713a004d24182aa8066d5a
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-07-04 14:57:18 +0200

    2D/SIR: only the preys are active

--- 2D/SIR.cpp
+++ 2D/SIR.cpp
@@ -1,25 +1,24 @@
 #include <vb/Hub.h>
 #include <vb/Image.h>
 
-#define VOID  Color(0)
-#define PREY  Color(0,255,0)
-#define PRED  Color(255,0,0)
-
 using namespace vb;
+
+const Color none(0), prey(GREEN), pred(RED);
 
 class SIR : public Image { public:
     SIR (int n, double l_) : Image (n,n), l (l_) {
         int n0 = H['d'] ? 0 : int(H['n'])/2;
         for (int i=n0-10; i<n0+10; ++i) for (int j=n0-10; j<n0+10; ++j)
-            if (contains(coo(i,j))) { put(coo(i,j),PREY); ++ np; }
-        put (coo(n0,n0),PRED); -- np;
+            if (contains(coo(i,j))) { put(coo(i,j),prey); ++ np; }
+        put (coo(n0,n0),pred); -- np;
         show();
     }
 
     void step () {
-        coo z = rand(), nz = z + dz[prng.uniform_int(H['d'] ? 2 : 4)]; if (!contains(nz)) return;
-        if ((at(z) == PREY) && (at(nz) == VOID) && ((l>1) || prng.bernoulli(l)))   { put(nz,PREY); ++ np; }
-        if ((at(z) == PRED) && (at(nz) == PREY) && ((l<1) || prng.bernoulli(1/l))) { put(nz,PRED); -- np; }
+        coo z = rand(); if (at(z) != prey) return;
+        coo nz = z + dz[prng.uniform_int(H['d'] ? 2 : 4)]; if (!contains(nz)) return;
+        if ((at(nz) == none) && ((l>1) || prng.bernoulli(l)))   { put(nz,prey); ++ np; }
+        if ((at(nz) == pred) && ((l<1) || prng.bernoulli(1/l))) { put(z,pred);  -- np; }
     }
 
     int np = 0;
