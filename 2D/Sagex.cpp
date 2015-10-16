eba3fba7cdc0d925d91d8bc41d40ba6d0367c275
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-10-16 23:35:17 +0200

    2D/Sagex: faster by not tossing random ints every time

--- 2D/Sagex.cpp
+++ 2D/Sagex.cpp
@@ -13,7 +13,7 @@ public:
 	operator Color() const { return C[state]; }
 	coo jump() {
 		if (H['z']) { return dz[prng.uniform_int(2) + (state==2 ? 2 : 0)]; }
-		int out = 1 + prng.uniform_int(3); if ((out==2) && (state==1)) out = 0; return dz[out];
+		int out = prng.uniform_int(4); if (out + 2*state == 4) out = 2-out; return dz[out];
 	}
 };
 
@@ -25,14 +25,14 @@ public:
 
 	void go () {
 		show(); if (H['v']) snapshot_setup("Sagex",1);
-		while (true) {
-			coo z = this->rand(), dz = at(z).jump();
-			if ((int(at(z))!=0) && (int(atp(z+dz))==0)) { putp(z+dz,int(at(z))); put(z,0); }
+		for (int t=H['t']; t!=1; --t) {
+			coo z = this->rand(); if (int(at(z))==0) continue;
+			coo dz = at(z).jump(); if (int(atp(z+dz))==0) { putp(z+dz,int(at(z))); put(z,0); }
 		}
 	}
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Seb's Sagex process", argc,argv, "n=400,l=.8,p=.5,v,z");
+	H.init ("Seb's Sagex process", argc,argv, "n=512,l=.22,p=.5,v,z,t=0");
 	Sagex (H['n']).go();
 }
