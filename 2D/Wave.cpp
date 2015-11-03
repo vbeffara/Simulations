e861fc837d36b5d6f3b62d8db6b00b04a6293057
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-11-03 11:51:27 +0100

    2D/Wave: better locality, much faster computation

--- 2D/Wave.cpp
+++ 2D/Wave.cpp
@@ -3,34 +3,30 @@
 
 using namespace vb; using namespace std;
 
+class mode { public:
+	mode (double a_, double t_, double l_) : a(a_), t(t_), l(l_), c(cos(t)), s(sin(t)) {}
+	double operator() (cpx z) { return a * cos((real(z)*c+imag(z)*s) / l); }
+	double a,t,l,c,s;
+};
+
 class Wave : public Bitmap<Color> { public:
-	Wave (int n, int k, double l_) : Bitmap<Color> (n,n), l(l_) {
+	Wave (int n, int k, double l_) : Bitmap<Color> (n,n) {
 		for (int i=0; i<k; ++i) {
-			a.push_back(prng.gaussian());
-			th.push_back(prng.uniform_real(0,2.0*M_PI));
+			m.push_back ( { prng.gaussian(), prng.uniform_real(0,2.0*M_PI), l_ } );
+			if (H['r']) m.push_back ( { m.back().a, - m.back().t, m.back().l } );
 		}
 	}
 
-	cpx v (cpx z) {
-		cpx out = 0;
-		if (H['r']) z -= cpx(0,h()/2);
-		for (int i=0; i<a.size(); ++i) {
-			out += a[i] * exp(I * (z.real()*cos(th[i]) + z.imag()*sin(th[i])) / l);
-			if (H['r']) out += a[i] * exp(I * (z.real()*cos(th[i]) - z.imag()*sin(th[i])) / l);
-		}
-		return out;
-	}
+	double v (cpx z) { double out = 0; for (auto mm : m) out += mm(z); return out; }
 
-	Color c (cpx z) { return v(z).real() > 0 ? RED : BLUE; }
+	Color c (cpx z) { return Indexed (v(z) > 0 ? 1 : 2); }
 
 	void go () {
-		show();
-		std::function<Color(cpx)> f = [&](cpx z) { return c(z); };
-		tessel(0,{w()-1,h()-1},aa(f)); pause();
+		std::function<Color(cpx)> f = [&](cpx z) { return c (z - cpx(0,h()/2)); };
+		show(); tessel(0,{w()-1,h()-1},aa(f)); pause();
 	};
 
-	vector<double> a,th;
-	double l;
+	vector<mode> m;
 };
 
 int main (int argc, char ** argv) {
