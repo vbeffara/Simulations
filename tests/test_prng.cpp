452d246e40087ae5f7294263f82dae5a08f31d9b
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-11-20 10:45:13 +0100

    Add a multithreaded version to test_prng

--- tests/test_prng.cpp
+++ tests/test_prng.cpp
@@ -1,72 +1,54 @@
+#include <vb/Hub.h>
 #include <vb/PRNG.h>
-#include <vb/CL_Parser.h>
 #include <random>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#include <cilk/reducer_opadd.h>
 
 using namespace vb;
 using namespace std;
 
 double time () { return double(clock()) / CLOCKS_PER_SEC; }
 
-class local_bernoulli_distribution {
-public:
-	explicit local_bernoulli_distribution (double p = 0.5)
-		: thr(p * unsigned(-1)) {}
-	bool operator() (std::mt19937 & e) { return e() < thr; }
-private:
-	const unsigned thr;
-};
-
 int main (int argc, char ** argv) {
-	CL_Parser CLP (argc,argv,"n=100000000");
-	int n=CLP('n');
-	long long s;
-	double t=0;
+	H.init ("Testing PRNG methods", argc,argv, "n=100000000");
+	int n=H['n'];
+	double s, t=0;
 
 	string state = prng.state();
 
-	cerr << "vb::PRNG::bernoulli() ...              \ttime = ";
+	cerr << "vb::PRNG::gaussian() ...               \ttime = ";
 	s=0; t=time();
-	for (int i=0; i<n; ++i) s += prng.bernoulli();
+	for (int i=0; i<n; ++i) { double o = prng.gaussian(); s += o*o; }
 	cerr << time()-t << ",  \tsum = " << s << endl;
 
-	cerr << "direct vb::PRNG() ...                  \ttime = ";
+	cerr << "boost::gaussian_distribution ...       \ttime = ";
 	s=0; t=time();
-	for (int i=0; i<n; ++i) s += prng()%2;
-	cerr << time()-t << ",  \tsum = " << s << endl;
-
-	cerr << "boost::bernoulli_distribution ...      \ttime = ";
-	s=0; t=time();
-	boost::bernoulli_distribution<> dist(.5);
+	boost::normal_distribution<> dist(0,1);
 	boost::mt19937 boostengine;
-	for (int i=0; i<n; ++i) s += dist(boostengine);
+	for (int i=0; i<n; ++i) { double o = dist(boostengine); s += o*o; }
 	cerr << time()-t << ",  \tsum = " << s << endl;
 
-	cerr << "many boost::bernoulli_distributions ...\ttime = ";
+	cerr << "many boost::gaussian_distributions ...\ttime = ";
 	s=0; t=time();
-	for (int i=0; i<n; ++i) s += (boost::bernoulli_distribution<> (.5))(prng);
+	for (int i=0; i<n; ++i) { double o = (boost::normal_distribution<> (0,1))(prng); s += o*o; }
 	cerr << time()-t << ",  \tsum = " << s << endl;
 
 	cerr << "C++11 style ...                        \ttime = ";
 	s=0; t=time();
-	std::bernoulli_distribution dist2(.5);
+	std::normal_distribution<> dist2(0,1);
 	std::mt19937_64 engine;
-	for (int i=0; i<n; ++i) s += dist2(engine);
-	cerr << time()-t << ",  \tsum = " << s << endl;
-
-	cerr << "C++11 style, tuned distribution ...    \ttime = ";
-	s=0; t=time();
-	local_bernoulli_distribution distt(.5);
-	std::mt19937 enginet;
-	for (int i=0; i<n; ++i) s += distt(enginet);
-	cerr << time()-t << ",  \tsum = " << s << endl;
-
-	cerr << "UNIX rand() ...                        \ttime = ";
-	s=0; t=time();
-	for (int i=0; i<n; ++i) s += rand()%2;
+	for (int i=0; i<n; ++i) { double o = dist2(engine); s += o*o; }
 	cerr << time()-t << ",  \tsum = " << s << endl;
 
 	cerr << "Stored state, first version ...        \ttime = ";
 	prng.state(state); s=0; t=time();
-	for (int i=0; i<n; ++i) s += prng.bernoulli();
+	for (int i=0; i<n; ++i) { double o = prng.gaussian(); s += o*o; }
 	cerr << time()-t << ",  \tsum = " << s << endl;
+
+	cerr << "CILK based multithreaded               \ttime = ";
+	auto go = []() { static thread_local PRNG p; auto o = p.gaussian(); return o*o; };
+	cilk::reducer <cilk::op_add<double>> ps (0);
+	cilk_for (int i=0; i<n; ++i) *ps += go();
+	cerr << time()-t << ",  \tsum = " << ps.get_value() << endl;
 }
