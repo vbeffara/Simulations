0c21008f637ccdf5a2bef8fb3075630bed82dc19
Vincent Beffara <vbeffara@ens-lyon.fr>
2015-11-26 13:23:47 +0100

    2D/Sagex: store z in each site

--- 2D/Sagex.cpp
+++ 2D/Sagex.cpp
@@ -12,27 +12,28 @@ public:
 	operator int()   const { return state; }
 	operator Color() const { return C[state]; }
 	coo jump() {
-		if (H['z']) { return dz[prng.uniform_int(2) + (state==2 ? 2 : 0)]; }
+		if (z) { return dz[prng.uniform_int(2) + (state==2 ? 2 : 0)]; }
 		int out = prng.uniform_int(4); if (out + 2*state == 4) out = 2-out; return dz[out];
 	}
+	bool z;
 };
 
 class Sagex : public Bitmap<Site> {
 public:
-	Sagex (int n) : Bitmap<Site> (n,n) {
-		for (auto & s : *this) if (prng.bernoulli(H['l'])) s = prng.bernoulli(H['p']) ? 1 : 2;
+	Sagex (int w, int h) : Bitmap<Site> (w,h) {
+		for (auto & s : *this) { if (prng.bernoulli(H['l'])) s = prng.bernoulli(H['p']) ? 1 : 2; s.z = H['z']; }
 	}
 
 	void go () {
 		show(); if (H['v']) snapshot_setup("Sagex",1);
 		while (true) {
 			coo z = this->rand(); if (int(at(z))==0) continue;
-			coo dz = at(z).jump(); if (int(atp(z+dz))==0) { putp(z+dz,int(at(z))); put(z,0); }
+			coo dz = at(z).jump(); if (int(atp(z+dz))==0) { putp(z+dz, at(z)); put(z,0); }
 		}
 	}
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Seb's Sagex process", argc,argv, "n=512,l=.22,p=.5,v,z");
-	Sagex (H['n']).go();
+	H.init ("Seb's Sagex process", argc,argv, "n=400,w=0,l=.22,p=.5,v,z");
+	Sagex (int(H['w']) ? H['w'] : H['n'], H['n']).go();
 }
