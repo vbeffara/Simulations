b29bf1945cbc4b8bc3f510d4d1d136551b7c898c
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-01-13 17:17:14 +0100

    Rewrite tools/graph in Python, step 2: with the -f option

--- tools/graph.py
+++ tools/graph.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 from glob import glob
+from sys import argv
+
+full = 0
+if len(argv) >= 2 and argv[1] == "-f":
+	full = 1
 
 vbh = sorted (glob ("libvb/vb/*.h"))
 
@@ -14,7 +19,7 @@ for f in vbh:
 	for l in open(f):
 		if l.startswith("#include"):
 			h = l[10:-2]
-			if h.startswith("vb/"):
+			if h.startswith("vb/") or full:
 				print '  "%s" -> "%s"' % (f[6:],h)
 
 print "}"
