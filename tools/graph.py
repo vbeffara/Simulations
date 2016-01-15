6af16f211f7ef517e55f8150c82027fa08ceb214
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-01-15 11:47:40 +0100

    In tools/graph.py, include the executables with -x

--- tools/graph.py
+++ tools/graph.py
@@ -3,15 +3,13 @@
 from glob import glob
 from sys import argv
 
-full = 0
-if len(argv) >= 2 and argv[1] == "-f":
-	full = 1
-
-vbh = sorted (glob ("libvb/vb/*.h"))
+full = "-f" in argv
+xfull = "-x" in argv
 
 print "digraph {"
 print "  rankdir = LR"
 
+vbh = sorted (glob ("libvb/vb/*.h"))
 for f in vbh:
 	print '"%s"[style="filled",fillcolor="lightblue"];' % f[6:]
 
@@ -21,5 +19,15 @@ for f in vbh:
 			h = l[10:-2]
 			if h.startswith("vb/") or full:
 				print '  "%s" -> "%s"' % (f[6:],h)
+
+if xfull:
+	vbc = sorted (glob ("[123]*/*.cpp"))
+	for f in vbc:
+		print '"%s"[style="filled",fillcolor="pink"];' % f
+		for l in open(f):
+			if l.startswith("#include"):
+				h = l[10:-2]
+				if h.startswith("vb/") or full:
+					print '  "%s" -> "%s"' % (f,h)
 
 print "}"
