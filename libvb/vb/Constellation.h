b970e53869d29d82401e797e9d28f42f49ba241d
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-01-18 17:42:48 +0100

    Silence one more warning

--- libvb/vb/Constellation.h
+++ libvb/vb/Constellation.h
@@ -19,6 +19,8 @@ namespace vb {
 		Constellation                      	();
 		template <typename U> Constellation	(const Constellation<U> & C);
 
+		virtual ~ Constellation () {}
+
 		virtual	Vector<cplx>	vec     	()                       	const	=0;
 		virtual	void        	readvec 	(const Vector<cplx> & xy)	     	=0;
 		virtual	Vector<cplx>	vcost   	()                       	const	=0;
