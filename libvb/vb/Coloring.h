477d2684a8bc6a6f214c42f2b0509d1444267d6e
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-01-21 17:51:55 +0100

    Refactor a bit

--- libvb/vb/Coloring.h
+++ libvb/vb/Coloring.h
@@ -7,45 +7,40 @@
 
 namespace vb {
 	class Coloring : public Picture { public:
-		Coloring (int wd, int ht, std::function <Color(coo)> ff);
+		Coloring (int wd, int ht, std::function <Color(cpx)> ff) : Picture(wd,ht), f(ff) {}
 
-		void tessel (coo ul, coo lr);
+		virtual void show() {
+			Picture::show();
+			stage = (Color *) (cairo_image_surface_get_data (surface));
+			center = {pixel_w()/2,pixel_h()/2}; eps = 2.0/pixel_w();
+			tessel ({0,0}, {pixel_w()-1,pixel_h()-1});
+		}
+
+		coo center;
+		double eps;
+		bool aa = false;
+		std::function <Color(cpx)> f;
+
+	private:
+		cpx c_to_z (coo c) const { return eps * cpx(c - center); }
 
 		Color * stage = nullptr;
 
 		Color & at (coo z) { return stage[z.x + stride * z.y]; }
 
-		std::function <Color(coo)> f;
-
-		virtual void show() {
-			Picture::show();
-			stage = (Color *) (cairo_image_surface_get_data (surface));
-			tessel ({0,0}, {pixel_w()-1,pixel_h()-1});
-		}
-	};
-
-	Coloring::Coloring (int wd, int ht, std::function <Color(coo)> ff) :
-		Picture(wd,ht), f(ff) {}
-
-	template <typename T> std::function <Color(coo)> aa (std::function <Color(std::complex<Color>)> f) {
-		return [&](coo c) {
-			using cplx = std::complex<Color>;
-			cplx z (c.x,c.y); int r(0), g(0), b(0), a(0);
+		Color color (coo c) const {
+			if (!aa) return f(c_to_z(c));
+			cpx z = c_to_z(c); int r(0), g(0), b(0), a(0);
 			for (int i=-1; i<=1; ++i) for (int j=-1; j<=1; ++j) {
-				Color c = f(z + cplx{i/3.0,j/3.0}); r+=c.r; g+=c.g; b+=c.b; a+=c.a;
+				Color c = f (z + eps*cpx(i,j)/3.0);
+				r+=c.r; g+=c.g; b+=c.b; a+=c.a;
 			}
 			return Color(r/9,g/9,b/9,a/9);
-		};
-	}
+		}
 
-    void Coloring::tessel (coo ul, coo lr) {
-		if (!visible()) return;
+        void line (coo s, coo d, int l) { for (int i=0; i<l; ++i) { coo c = s+d*i; at(c) = color(c); } }
 
-        std::function<void(coo,coo,int)> line = [&] (coo s, coo d, int l) {
-            cilk_for (int i=0; i<l; ++i) { at(s+d*i) = f(s+d*i); }
-        };
-
-        std::function<void(coo,coo)> go = [&] (coo ul, coo lr) {
+        void go (coo ul, coo lr) {
             Color tmp = at(ul); bool mono = true; coo z = ul;
             for (; mono && (z != coo {lr.x,ul.y}); z += {1,0}) mono = mono && (at(z) == tmp);
             for (; mono && (z != coo {lr.x,lr.y}); z += {0,1}) mono = mono && (at(z) == tmp);
@@ -61,17 +56,18 @@ namespace vb {
             line (ul_,dd_,size);
 
             cilk_spawn go (ul,lr_); go (ul_,lr); cilk_sync;
-        };
+        }
 
-        auto main = std::async (std::launch::async, [&]() {
-            line (ul, coo(1,0), lr.x-ul.x);
-            line (coo(lr.x,ul.y), coo(0,1), lr.y-ul.y);
-            line (lr, coo(-1,0), lr.x-ul.x);
-            line (coo(ul.x,lr.y), coo(0,-1), lr.y-ul.y);
-            go (ul, lr);
-        } );
+	    void tessel (coo ul, coo lr) {
+	        auto main = std::async (std::launch::async, [&]() {
+	            line (ul, coo(1,0), lr.x-ul.x);
+	            line (coo(lr.x,ul.y), coo(0,1), lr.y-ul.y);
+	            line (lr, coo(-1,0), lr.x-ul.x);
+	            line (coo(ul.x,lr.y), coo(0,-1), lr.y-ul.y);
+	            go (ul, lr);
+	        } );
 
-        while (main.wait_for(std::chrono::milliseconds(100)) != std::future_status::ready) update(); main.get();
-    }
-
+	        while (main.wait_for(std::chrono::milliseconds(100)) != std::future_status::ready) update(); main.get();
+	    }
+	};
 }
--- tests/sample.cpp
+++ tests/sample.cpp
@@ -3,15 +3,10 @@
 
 using namespace vb;
 
-Color wave (coo z) {
-	static const int n = H['n'];
-	int d = norm (z-coo(n/2,n/2));
-	double useless = 0.01; for (int t=1; t<1000; ++t) useless = sin(useless); d += useless; // Just to take time
-	return vb::Color ( 1+(2*d*((1<<8)-1))/(n*n) );
-}
+Color wave (cpx zz) { return Color (int(1000*norm(zz))%256); }
 
 int main (int argc, char *argv[]) {
-	H.init ("Sample",argc,argv,"n=500");
-	Coloring S (H['n'],H['n'],wave);
+	H.init ("Sample",argc,argv,"n=500,a");
+	Coloring S (H['n'],H['n'],wave); S.aa = H['a'];
 	S.show(); S.pause(); S.output();
 }
