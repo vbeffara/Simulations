44f2bca08e8ca36cb55395328992fb8fdba032bd
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-01-31 11:54:17 +0100

    RandomPolynomial: more careful computation of binomial coefficients

--- 2D/RandomPolynomial.cpp
+++ 2D/RandomPolynomial.cpp
@@ -1,17 +1,19 @@
 #include <vb/Coloring.h>
 #include <vb/Hub.h>
 #include <vb/PRNG.h>
+#include <boost/math/special_functions/binomial.hpp>
 
 using namespace vb; using namespace std;
 
-double sqfact (int i) {
-	double out = 1;
-	for (int k=2; k<=i; ++k) out *= sqrt(k);
-	return out;
+double sm3 (int i, int j, int k) {
+	if (i<j) return sm3(j,i,k);
+	if (i<k) return sm3(k,i,j);
+	if (i==0) return 1.0;
+	return sqrt(double(i+j+k)/double(i)/(9.0*M_PI)) * sm3 (i-1,j,k);
 }
 
 class RPoly { public:
-	class Coef { public: int i, j; double a; };
+	class Coef { public: int i, j, k; double a, b; };
 
 	RPoly (int n) {
 		map<string,function<double()>> generators;
@@ -20,15 +22,15 @@ class RPoly { public:
 
 		for (int i=0; i<=n; ++i)
 			for (int j=0; j<=n-i; ++j) {
-				double v = generators[H['g']]();
-				v *= sqfact(n)/(sqfact(i)*sqfact(j)*sqfact(n-i-j));
-				coefs.push_back ({i,j,v});
+				double a = generators[H['g']]();
+				double b = a * sm3(i,j,n-i-j);
+				coefs.push_back ({i,j,n-i-j,a,b});
 			}
 	}
 
 	double operator() (double x, double y) {
 		double out = 0;
-		for (auto c : coefs) out += c.a * pow(x,c.i) * pow(y,c.j);
+		for (auto c : coefs) out += c.b * pow(x,c.i) * pow(y,c.j);
 		return out;
 	}
 
@@ -40,8 +42,8 @@ class RPoly { public:
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Random polynomial in 2 variables", argc, argv, "n=10,a,g=gaussian,s=0");
+	H.init ("Random polynomial in 2 variables", argc, argv, "n=100,g=gaussian,s=0");
 	int s = H['s']; if (s) prng.seed(s);
-	RPoly P (H['n']); Coloring C (cpx(-10,-10),cpx(10,10),800,P); C.aa = H['a'];
+	RPoly P (H['n']); Coloring C (cpx(-10,-10),cpx(10,10),800,P);
 	C.show(); while (C.visible()) { C.update(); Fl::wait(); } C.output();
 }
