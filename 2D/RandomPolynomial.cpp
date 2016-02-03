577a5ab1db403c2cbebc4289648da0b3fea7b228
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-02-03 10:50:54 +0100

    Implement Horner for RandomPolynomial, it is a lot faster

--- 2D/RandomPolynomial.cpp
+++ 2D/RandomPolynomial.cpp
@@ -1,49 +1,51 @@
 #include <vb/Coloring.h>
 #include <vb/Hub.h>
 #include <vb/PRNG.h>
-#include <boost/math/special_functions/binomial.hpp>
+#include <vb/math.h>
 
 using namespace vb; using namespace std;
 
-double sm3 (int i, int j, int k) {
-	if (i<j) return sm3(j,i,k);
-	if (i<k) return sm3(k,i,j);
+template <typename T> T sm3 (int i, int j, int k) {
+	if (i<j) return sm3<T> (j,i,k);
+	if (i<k) return sm3<T> (k,i,j);
 	if (i==0) return 1.0;
-	return sqrt(double(i+j+k)/double(i)/(9.0*M_PI)) * sm3 (i-1,j,k);
+	return sqrt(T(i+j+k)/T(i)/(T(9)*pi_<T>())) * sm3<T> (i-1,j,k);
 }
 
-class RPoly { public:
-	class Coef { public: int i, j, k; double a, b; };
-
-	RPoly (int n) {
+template <typename T> class RPoly { public:
+	RPoly (int n) : A(n+1) {
 		map<string,function<double()>> generators;
 		generators.emplace ("gaussian",  [](){ return prng.gaussian(0,1); });
 		generators.emplace ("bernoulli", [](){ return prng.bernoulli(.5) ? 1 : -1; });
 
 		for (int i=0; i<=n; ++i)
 			for (int j=0; j<=n-i; ++j) {
-				double a = generators[H['g']]();
-				double b = a * sm3(i,j,n-i-j);
-				coefs.push_back ({i,j,n-i-j,a,b});
+				T a (generators[H['g']]());
+				T b = a * sm3<T> (i,j,n-i-j);
+				A[i].push_back(b);
 			}
 	}
 
-	double operator() (double x, double y) {
-		double out = 0;
-		for (auto c : coefs) out += c.b * pow(x,c.i) * pow(y,c.j);
+	T operator() (T x, T y) {
+		T out (0); int n = A.size()-1;
+		for (int i=n; i>=0; --i) {
+			T pi (0);
+			for (int j=n-i; j>=0; --j) { pi = y*pi + A[i][j]; }
+			out = x*out + pi;
+		}
 		return out;
 	}
 
 	Color operator() (cpx z) {
-		return HSV ( (*this)(real(z),imag(z))>0 ? 0 : .5, .8, .8);
+		return HSV ( (*this)(T(real(z)),T(imag(z)))>0 ? 0 : .5, .8, .8);
 	}
 
-	vector<Coef> coefs;
+	vector<vector<T>> A;
 };
 
 int main (int argc, char ** argv) {
 	H.init ("Random polynomial in 2 variables", argc, argv, "n=100,g=gaussian,s=0");
 	int s = H['s']; if (s) prng.seed(s);
-	RPoly P (H['n']); Coloring C (cpx(-10,-10),cpx(10,10),800,P);
-	C.show(); C.output(); Fl::run();
+	RPoly<double> P (H['n']); Coloring C (cpx(-10,-10),cpx(10,10),800,P);
+	C.show(); C.output(); // Fl::run();
 }
