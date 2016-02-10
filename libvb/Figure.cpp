2aa4b79115437d2602a18297e127e31003f19106
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-02-10 23:01:15 +0100

    Separate Shape from Figure

--- libvb/Figure.cpp
+++ libvb/Figure.cpp
@@ -1,68 +1,8 @@
-/// @file
-/// Implementation of the vb::Figure and related classes.
-
 #include <vb/Figure.h>
 #include <vb/Hub.h>
+#include <cairo-pdf.h>
 
 namespace vb {
-
-	void Segment::draw (cairo_t * cr){
-			cairo_move_to (cr, z1.real(), z1.imag());
-			cairo_line_to (cr, z2.real(), z2.imag());
-	}
-
-	void Dot::draw (cairo_t *) { }
-
-	void Circle::draw (cairo_t * cr) {
-		cairo_new_sub_path (cr);
-		cairo_arc (cr, z.real(), z.imag(), r, 0, 2*M_PI);
-	}
-
-	double Path::left () {
-		double m=z[0].real();
-		for (unsigned int i=1; i<z.size(); ++i) if (std::isnormal(real(z[i]))) m = std::min (m, z[i].real());
-		return m;
-	}
-
-	double Path::right () {
-		double m=z[0].real();
-		for (unsigned int i=1; i<z.size(); ++i) if (std::isnormal(real(z[i]))) m = std::max (m, z[i].real());
-		return m;
-	}
-
-	double Path::top () {
-		double m=z[0].imag();
-		for (unsigned int i=1; i<z.size(); ++i) if (std::isnormal(real(z[i]))) m = std::max (m, z[i].imag());
-		return m;
-	}
-
-	double Path::bottom () {
-		double m=z[0].imag();
-		for (unsigned int i=1; i<z.size(); ++i) if (std::isnormal(real(z[i]))) m = std::min (m, z[i].imag());
-		return m;
-	}
-
-	void Path::draw (cairo_t * cr) {
-		bool down = false; int l = 0;
-		for (auto p : z) {
-			if (std::isnan(real(p))) { down = false; continue; }
-			if (!down) {
-				if ((l>100) && (!this->p.ff)) { cairo_stroke(cr); l=0; }
-				cairo_move_to (cr, p.real(), p.imag());
-				if (!std::isnan(real(p))) down = true;
-			}
-			else { cairo_line_to (cr, p.real(), p.imag()); }
-			++l;
-		}
-	};
-
-	void Polygon::draw (cairo_t * cr) {
-		cairo_move_to (cr, z.back().real(), z.back().imag());
-		for (unsigned int i=0; i<z.size(); ++i) cairo_line_to (cr, z[i].real(), z[i].imag());
-	};
-
-	/*********************************************************/
-
 	Figure::Figure (bool _o) : Picture (600,600), ortho(_o) {}
 
 	double Figure::left () {
---
+++ libvb/vb/Shape.h
@@ -0,0 +1,71 @@
+#pragma once /// @file
+#include <vb/Color.h>
+#include <vb/cpx.h>
+#include <cairo.h>
+
+namespace vb {
+	class Pen { public:
+		Pen (Color c_=0, double w_=1.0, Color f_=255, bool ff_=false) : c(c_), f(f_), w(w_), ff(ff_) { }
+		Color c,f; double w; bool ff;
+	};
+
+	class Shape { public:
+		Shape (Pen p_) : p(p_) { }
+		virtual ~Shape() {}
+
+		virtual double left () =0; virtual double right () =0; virtual double top () =0; virtual double bottom () =0;
+		virtual void draw (cairo_t * cr) =0;
+
+		Pen p;
+	};
+
+	class Segment : public Shape { public:
+		Segment (cpx zz1, cpx zz2, Pen p = Pen()) : Shape(p), z1(zz1), z2(zz2) {}
+
+		double left ()   { return std::min(z1.real(),z2.real()); }
+		double right ()  { return std::max(z1.real(),z2.real()); }
+		double top ()    { return std::max(z1.imag(),z2.imag()); }
+		double bottom () { return std::min(z1.imag(),z2.imag()); }
+		void draw (cairo_t * cr);
+
+		cpx z1,z2;
+	};
+
+	class Dot : public Shape { public:
+		Dot (cpx zz, Pen p = Pen()) : Shape(p), z(zz) {}
+
+		double left ()   { return z.real(); }
+		double right ()  { return z.real(); }
+		double top ()    { return z.imag(); }
+		double bottom () { return z.imag(); }
+		void draw (cairo_t * cr);
+
+		cpx z;
+	};
+
+	class Circle : public Shape { public:
+		Circle (cpx z_, double r_, Pen p = Pen()) : Shape(p), z(z_), r(r_) {}
+
+		double left ()   { return z.real() - r; }
+		double right ()  { return z.real() + r; }
+		double top ()    { return z.imag() + r; }
+		double bottom () { return z.imag() - r; }
+		void draw (cairo_t * cr);
+
+		cpx z; double r;
+	};
+
+	class Path : public Shape { public:
+		Path (std::vector<cpx> z_, Pen p = Pen()) : Shape(p), z(z_) {}
+
+		double left (); double right (); double top (); double bottom ();
+		void draw (cairo_t * cr);
+
+		std::vector<cpx> z;
+	};
+
+	class Polygon : public Path { public:
+		Polygon (std::vector<cpx> z, Pen p = Pen()) : Path(z,p) {}
+		void draw (cairo_t * cr);
+	};
+}
