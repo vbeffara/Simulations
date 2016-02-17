518a5f1584bc26153f9c404788e1bfcc86354d6a
Vincent Beffara <vbeffara@ens-lyon.fr>
2016-02-17 18:26:44 +0100

    Stuck: many changes (track, output ...)

--- 2D/Stuck.cpp
+++ 2D/Stuck.cpp
@@ -22,15 +22,30 @@ int Stat::min=0, Stat::max=1;
 class Stuck : public Bitmap<Stat> { public:
 	Stuck (Hub H) : Bitmap<Stat> (2*int(H['n']),2*int(H['n'])), alpha(H['a']), beta(H['b']) {
 		for (int i=0; i<w()/2; ++i) for (int j=0; j<h()/2; ++j) at(coo(2*i,2*j))=-1;
+		z = {w()/2,h()/2};
 		C.watch (alpha, "Alpha");
 		C.manage (alpha, 0.142, 0.35);
-		C.lambda ((std::function<int()>) ([this](){return this->nsup();}), "Support");
+		C.lambda<int>	 ([this](){ return nsup(); }, "Support");
+		C.lambda<double> ([this](){ return - log(double(nsup())) / log(alpha-1.0/7); }, "Prediction");
 	}
+
 	void show () { C.show(); Bitmap<Stat>::show(); }
+
+	void center () {
+		coo c (0,0);
+		for (int x=0; x<w(); ++x) for (int y=0; y<h(); ++y) if (at(coo(x,y))==Stat::max) c = {x-w()/2,y-h()/2};
+		c.x -= c.x % 2; c.y -= c.y % 2;
+		if (c != coo(0,0)) {
+			static Array<Stat> tmp (*this);
+			for (int x=0; x<w(); ++x) for (int y=0; y<h(); ++y) { coo z(x,y); tmp.at(z) = atp(z+c); }
+			Array<Stat>::operator= (tmp);
+			z-=c;
+		}
+	}
+
 	void run() {
-		coo z (w()/2,h()/2);
 		vector<double> l(4);
-		for (;;) {
+		while(visible()) {
 			for (int i=0; i<4; ++i) {
 				coo d=dz[i], dl=dz[(i+1)%4], dr=dz[(i+3)%4];
 				l[i] = alpha * (atp(z+d+d+d) + atp(z+d+d+dl) + atp(z+d+d+dr)) - atp(z+d);
@@ -40,24 +55,31 @@ class Stuck : public Bitmap<Stat> { public:
 			for (int i=0; i<4; ++i) { l[i] = exp(beta*(l[i]-ml)); sl+=l[i]; }
 			for (int i=0; i<4; ++i) { l[i] /= sl; }
 			coo d = dz[prng.discrete(l)];
-			putp(z+d,atp(z+d)+1);
-			z += d+d;
+			putp(z+d,atp(z+d)+1); z += d+d;
+		}
+		if (H['o']) for (int x=0; x<w(); x+=2) {
+			for (int y=1; y<h(); y+=2) cout << x << " " << y << " " << at(coo(x,y)) << endl;
+			cout << endl;
 		}
 	}
+
 	void update () {
+		if (H['c']) center();
 		int m=Stat::max;
 		for (int v : *this) if (v > 0) m = min (m,v);
 		if (m<Stat::max) Stat::min=m;
 		Bitmap<Stat>::update();
 	}
+
 	int nsup () { int n=0; for (Stat v : *this) if ((v.s>0) && (v.ml>.9*Stat::max)) ++n; return n; }
 
 	double alpha, beta;
+	coo z;
 	Console C;
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Stuck walk on the square lattice",argc,argv,"n=200,a=.1432,b=5,v=0");
+	H.init ("Stuck walk on the square lattice",argc,argv,"n=200,a=.1432,b=5,v=0,c,o");
 	Stuck S(H); S.show();
 	if (double(H['v'])>0) S.snapshot_setup("Stuck",H['v']);
 	S.run();
