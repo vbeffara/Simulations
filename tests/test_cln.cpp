6404003579bdda2ac3eb8fd024105d88b1b297e6
Vincent Beffara <vbeffara@gmail.com>
2016-03-01 15:51:42 +0100

    Streamline CLN test cases

--- tests/test_cln.cpp
+++ tests/test_cln.cpp
@@ -6,11 +6,9 @@
 
 using namespace vb; using namespace std; using namespace cln; using cln::complex;
 
-cl_N sum (std::function <cl_N(int)> f) {
-	cl_N out (0), old (1);
-	for (int n=0; out!=old; ++n) {
-		old = out; out += f(n);
-	}
+template <typename T> T sum (std::function <T(int)> f) {
+	T out (0), old (1);
+	for (int n=0; out!=old; ++n) { old = out; out += f(n); }
 	return out;
 }
 
@@ -20,9 +18,10 @@ cl_N pow (const cl_N & a, const cl_N & b) { return exp(b*log(a)); }
 
 cl_N theta1 (const cl_N & q, const cl_N & z) {
 	cl_N q14 = exp(log(q)/4);
-	return sum ([&](int n) {
+	std::function <cl_N(int)> f = [&](int n) {
 		return 2 * q14 * pow(cl_N(-1),n) * pow(q, n*(n+1)) * sin((2*n+1)*z);
-	});
+	};
+	return sum (f);
 }
 
 double time () {
@@ -32,13 +31,27 @@ double time () {
 	return dur.count();
 }
 
+template <typename T> void test (string s, int n) {
+	double t = time(); T z = 0;
+	for (int i=0; i<n; ++i) z = cos(z);
+	ostringstream os; os << time()-t;
+	cout << s << " | time = " << os.str() << "\t | result = " << z << endl;
+}
+
 int main (int argc, char ** argv) {
 	H.init ("Testing numerical types", argc,argv, "n=100000");
 	int n=H['n'];
-	double t=0;
 
-	cout << setprecision(100);
 	default_float_format = float_format(100);
+	cout << setprecision(100);
+
+	test <float>       ("Standard float ", n);
+	test <double>      ("Standard double", n);
+	test <long double> ("Long double    ", n);
+	test <cl_N>        ("CLN 100        ", n);
+	test <gmp100>      ("GMP 100        ", n);
+
+	cout << endl;
 
 	cout << "sin(1)" << endl;
 	cout << "  gmp100:      " << sin(gmp100(1)) << endl;
@@ -55,35 +68,6 @@ int main (int argc, char ** argv) {
 	cout << "  CLN(100):    " << q_(cl_float(1)/10) << endl;
 	cout << "  Mathematica: 0.9510565162951535721164393333793821434056986341257502224473056444301531700851935017187928109708113817 +"
 						<< "0.3090169943749474241022934171828190588601545899028814310677243113526302314094512248536036020946955687 I" << endl;
-	cout << endl;
-
-	cout << "double:     time = "; t = time();
-	double s = 0; for (int i=0; i<n; ++i) s = cos(s);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << s << endl;
-
-	cout << "CLN short:  time = "; t = time();
-	auto cs = cl_F("0.0s0"); for (int i=0; i<n; ++i) cs = cos(cs);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << cs << endl;
-
-	cout << "CLN single: time = "; t = time();
-	auto c1 = cl_F("0.0f0"); for (int i=0; i<n; ++i) c1 = cos(c1);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << c1 << endl;
-
-	cout << "CLN double: time = "; t = time();
-	auto c2 = cl_F("0.0d0"); for (int i=0; i<n; ++i) c2 = cos(c2);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << c2 << endl;
-
-	cout << "CLN float:  time = "; t = time();
-	auto c = cl_float(0.0); for (int i=0; i<n; ++i) c = cos(c);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << c << endl;
-
-	cout << "gmp100:     time = "; t = time();
-	gmp100 g (0); for (int i=0; i<n; ++i) g = cos(g);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << g << endl;
-
-	cout << "CLN 100:    time = "; t = time();
-	auto cc = cl_float(0); for (int i=0; i<n; ++i) cc = cos(cc);
-	cout << setprecision(10) << time()-t << ", \tresult = " << setprecision(100) << cc << endl;
 
 	cout << endl;
 
