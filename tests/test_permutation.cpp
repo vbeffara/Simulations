d9f9efcd09cc60b6c8be4a5c05433d464ba00d1d
Vincent Beffara <vbeffara@gmail.com>
2016-03-21 23:09:46 +0100

    Outsource alpha generation

--- tests/test_permutation.cpp
+++ tests/test_permutation.cpp
@@ -4,27 +4,33 @@
 
 using namespace vb; using namespace std;
 
+auto permutations (vector<unsigned> s) {
+	return Stream<Permutation> ([s](Sink<Permutation> & yield) {
+		int n=0; for (auto i : s) n += i; Permutation p (n);
+		do { if (p.signature() == s) yield(p); } while (next_permutation(p.begin(),p.end()));
+	});
+}
+
 auto maps (vector<unsigned> s, vector<unsigned> a, vector<unsigned> p) {
 	return Stream<Hypermap> ([s,a,p](Sink<Hypermap> & yield) {
 		Cycles cs; int i=0;
 		for (int l : s) { vector<unsigned> c; for (int j=0; j<l; ++j) c.push_back(i++); cs.push_back (c); }
-		Permutation sigma (cs), alpha (sigma.size());
+		Permutation sigma (cs);
 		vector<Hypermap> hs;
-		do {
-			if (alpha.signature() != a) continue;
+		for (auto alpha : permutations(a)) {
 			if (!connected(sigma,alpha)) continue;
 			Permutation phi = (sigma*alpha).inverse(); if (phi.signature() != p) continue;
 			Hypermap h(sigma,alpha,phi);
 			h.normalize(); bool done=0;
 			for (auto & hh : hs) if (h==hh) { done=1; break; }
 			if (!done) { hs.push_back(h); yield(h); }
-		} while (next_permutation(alpha.begin(),alpha.end()));
+		}
 	});
 }
 
 int main (int, char **) {
 	int n=0;
-	for (auto m : maps (vector<unsigned> {2,2,3,3}, vector<unsigned> {2,2,2,2,2}, vector<unsigned> {3,3,4})) {
+	for (auto m : maps ({2,2,3,3}, {2,2,2,2,2}, {3,3,4})) {
 		cout << m;
 		++n;
 	}
