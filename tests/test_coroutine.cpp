3ff9efd86dfd861f6b2d40a9f106d1de26c403d8
Vincent Beffara <vbeffara@gmail.com>
2016-03-21 15:29:38 +0100

    Closer to implementation, makes template parameter deduction possible

--- tests/test_coroutine.cpp
+++ tests/test_coroutine.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 using namespace vb;
 
-template <typename T> using coro = boost::coroutines::coroutine<T>;
-template <typename T> using Stream = typename coro<T>::pull_type;
-template <typename T> using Sink = typename coro<T>::push_type;
+template <typename T> using Stream = boost::coroutines::pull_coroutine<T>;
+template <typename T> using Sink = boost::coroutines::push_coroutine<T>;
 
 auto genperm (int n) {
     return Stream<Permutation> ([n](Sink<Permutation> & yield) {
@@ -18,27 +17,20 @@ auto genperm (int n) {
 }
 
 bool good (const Permutation & p) { return p[0]==2; }
+int sec (const Permutation & p) { return p[2]; }
 
-template <typename T> auto filter (Stream<T> & S, auto && f) {
+template <typename T> auto filter (auto && f, Stream<T> & S) {
 	return Stream<T> ([&](Sink<T> & yield) { for (auto x : S) if (f(x)) yield(x); });
 }
 
-template <typename T> auto filter (auto && f) {
-	return [&](Stream<T> & S) { return filter<T> (S,f); };
-}
-
-template <typename U, typename V> auto fmap (Stream<U> & S, auto && f) {
+template <typename U, typename V> auto fmap (auto f, Stream<U> & S) {
 	return Stream<V> ([&](Sink<V> & yield) { for (auto x : S) yield(f(x)); });
-}
-
-template <typename U, typename V> auto fmap (auto && f) {
-	return [&](Stream<U> & S) { return fmap<U,V> (S,f); };
 }
 
 int main(int argc, char ** argv) {
     H.init ("Testing coroutines", argc, argv, "n=5");
-    Stream<Permutation> S = genperm(H['n']);
-    Stream<Permutation> SS = filter<Permutation> (good) (S);
-    Stream<int> SSS = fmap<Permutation,int> ([](Permutation p) { return p[2]; }) (SS);
+    auto S = genperm (H['n']);
+    auto SS = filter (good,S);
+    auto SSS = fmap<Permutation,int> (sec,SS);
     for (auto p : SSS) cout << p << endl;
 }
