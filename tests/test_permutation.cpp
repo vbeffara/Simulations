97c41f08636897df5f85578c3cf5c3100a83b17d
Vincent Beffara <vbeffara@gmail.com>
2016-03-22 14:33:45 +0100

    Enumerate partitions, and make a more useful test

--- tests/test_permutation.cpp
+++ tests/test_permutation.cpp
@@ -75,10 +75,26 @@ auto maps (vector<unsigned> s, vector<unsigned> a, vector<unsigned> p) {
 	});
 }
 
+Stream <vector<unsigned>> partitions (unsigned n, unsigned m = 1) {
+	return Stream <vector<unsigned>> ([n,m](Sink <vector<unsigned>> & yield) {
+		for (unsigned i=m; i<n; ++i) {
+			for (auto p : partitions(n-i,i)) {
+				vector<unsigned> out ({i});
+				for (auto j : p) out.push_back(j);
+				yield (out);
+			}
+		}
+		if (n>=m) yield ({n});
+	});
+}
+
+template <typename T> int size (Stream<T> S) { int out=0; for (auto & i : S) { (void)i; out++; } return out; }
+
 int main (int argc, char ** argv) {
-	H.init ("Streams and permutations", argc, argv, "k=2,n=4");
-	// for (auto x : by_cycles ({2,2,2})) { cout << x << endl; }
-	int n=0;
-	for (auto m : maps ({2,2,3,3}, {2,2,2,2,2}, {3,3,4})) { cout << m; ++n; }
-	cout << "Total: " << n << " hypermap(s)" << endl;
+	H.init ("Streams and permutations", argc, argv, "n=10");
+	for (unsigned k=1; k<=unsigned(H['n']); ++k) {
+		cout << "n: "             	<< setw(2) << k;
+		cout << "\tpermutations: "	<< setw(7) << size(permutations(k));
+		cout << "\tpartitions: "  	<< setw(2) << size(partitions(k)) << endl;
+	}
 }
