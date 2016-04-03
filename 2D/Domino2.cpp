685e04561a67cd3c2db8bd71199cf4d715e3dbde
Vincent Beffara <vbeffara@gmail.com>
2016-04-03 13:48:24 +0200

    Two choices of domino weights

--- 2D/Domino2.cpp
+++ 2D/Domino2.cpp
@@ -4,71 +4,59 @@
 using namespace vb; using namespace std;
 
 vector<vector<vector<double>>> W, hues;
-
-double weight (coo z, int d);
+double minw, maxw;
 
 class Domino { public:
-    Domino (coo z_ = {0,0}, int d_ = 0) : z(z_), d(d_) { if (d>=2) { z += dz[d]; d -= 2; } }
-    Domino (int) {}
+	Domino (coo z_ = {0,0}, int d_ = 0, bool active_ = false) : z(z_), d(d_), active(active_) { if (d>=2) { z += dz[d]; d -= 2; } }
+	Domino (int) {}
 
-    double weight () const { return W [z.x % W.size()] [z.y % W[0].size()] [d]; }
+	double weight () const { return W [z.x % W.size()] [z.y % W[0].size()] [d]; }
 
-    operator Color () {
-        if (!active) return BLACK;
-        static int l1 = W.size(), l2 = W[0].size();
-        static double minw = 1, maxw = -1;
-        if (maxw == -1) {
-            for (int i=0; i<l1; ++i) for (int j=0; j<l2; ++j) for (int k=0; k<2; ++k) {
-                minw = std::min(minw,W[i][j][k]); maxw = std::max(maxw,W[i][j][k]);
-            }
-            if (maxw == minw) maxw = minw + 1;
-        }
-        double hh = hues [z.x%l1] [z.y%l2] [d];
-        return HSV (hh,1,1-.5*(::weight(z,d)-minw)/(maxw-minw));
-    }
+	operator Color () const {
+		if (!active) return BLACK;
+		double hh = hues [z.x % W.size()] [z.y % W[0].size()] [d];
+		return HSV (hh,1,.2+.8*(weight()-minw)/(maxw-minw));
+	}
 
-    coo z;
-    int d;
-    bool active = false;
+	coo z; int d; bool active;
 };
-
-double weight (coo z, int d) { return Domino{z,d}.weight(); }
-
-Color col (coo z, int d) { return Domino{z,d}; }
 
 class Tiling : public Bitmap<Domino> { public:
-    Tiling (int n) : Bitmap<Domino> (2*n,2*n) {
-        for (int x=0; x<w(); ++x) for (int y=0; y<h(); ++y) at(coo(x,y)).z = coo(x,y);
-        for (int i=0; i<n; ++i) for (int j=n-1-i; j<n+i; j+=2) putd (coo(i,j),1);
-        for (int i=n; i<2*n; ++i) for (int j=i-n; j<3*n-i-1; j+=2) putd (coo(i,j),1);
-    };
+	Tiling (int n) : Bitmap<Domino> (2*n,2*n) {
+		for (int i=0; i<n; ++i)  	for (int j=n-1-i; j<n+i; j+=2)  	putd ( {coo(i,j),1} );
+		for (int i=n; i<2*n; ++i)	for (int j=i-n; j<3*n-i-1; j+=2)	putd ( {coo(i,j),1} );
+	};
 
-    void putd (coo z, int d) {
-        Domino D {z,d}; D.active = true; atp(D.z) = D; atp(D.z + dz[D.d]) = D;
-        // atp(z).d = d; atp(z).active = true; atp(z+dz[d]).d = (d+2)%4; atp(z+dz[d]).active = true;
-    }
+	void putd (Domino d) { d.active=true; putp(d.z,d); putp(d.z + dz[d.d],d); }
 
-    void flip (coo z) {
-        Domino d1 = atp(z); if (!d1.active) return; z = d1.z;
-        int d = d1.d; coo zz = z + dz[(d+1)%4];
-        Domino d2 = atp(zz); if ((!d2.active) || (d2.z != zz) || (d2.d != d)) return;
-        double w1 = weight (z,d) * weight (zz,d);
-        double w2 = weight (z,(d+1)%4) * weight (z + dz[d], (d+1)%4);
-        if ((w2>=w1) || prng.bernoulli(w2/w1)) { putd (z, (d+1)%4); putd (z + dz[d], (d+1)%4); }
-    }
-    void run () {
-        while (visible()) { flip(rand()); step(); }
-    };
+	void flip (coo z) {
+		Domino d1 = atp(z); if (!d1.active) return; z = d1.z;
+		int d = d1.d; coo zz = z + dz[(d+1)%4];
+		Domino d2 = atp(zz); if ((!d2.active) || (d2.z != zz) || (d2.d != d)) return;
+		double w1 = d1.weight() * d2.weight();
+		d1 = {z,(d+1)%4}; d2 = {z+dz[d],(d+1)%4};
+		double w2 = d1.weight() * d2.weight();
+		if ((w2>=w1) || prng.bernoulli(w2/w1)) { putd (d1); putd (d2); }
+	}
 
-    static vector<vector<vector<double>>> W;
+	void run () { while (visible()) { flip(rand()); step(); } };
+
+	static vector<vector<vector<double>>> W;
 };
 
+void two_by_two (double a) {
+	W = vector<vector<vector<double>>> (2, vector<vector<double>> (2, vector<double> (2,1)));
+	W[0][0][0]=a; W[0][0][1]=a; W[0][1][0]=a; W[1][0][1]=a; minw = min(a,1.0); maxw = max(a,1.0); if (maxw==minw) minw -= 1;
+}
+
+void three_by_two (double a) {
+	W = vector<vector<vector<double>>> (6, vector<vector<double>> (2, vector<double> (2,1)));
+	W[0][0][1]=a; W[3][1][1]=a; minw = min(a,1.0); maxw = max(a,1.0); if (maxw==minw) minw -= 1;
+}
+
 int main (int argc, char ** argv) {
-    H.init ("Domino tiling (v2)", argc,argv, "n=100,a=.5");
-    W = vector<vector<vector<double>>> (6, vector<vector<double>> (2, vector<double> (2,1)));
-    hues = vector<vector<vector<double>>> (6, vector<vector<double>> (2, vector<double> (2,0)));
-    double a = H['a']; W[0][0][1]=a; W[3][1][1]=a;
-    Tiling D(H['n']);
-    Console C; for (auto & x : hues) for (auto & y : x) for (auto & z : y) { z = prng.uniform_real(); C.manage (z,0.0,1.0); }
-    C.show(); D.show(); D.run(); D.pause();
+	H.init ("Domino tiling (v2)", argc,argv, "n=100,a=.5");
+	two_by_two(H['a']); hues = W;
+	Console C; for (auto & x : hues) for (auto & y : x) for (auto & z : y) { z = prng.uniform_real(); C.manage (z,0.0,1.0); }
+	Tiling D(H['n']); C.show(); D.show(); D.run(); D.pause();
 }
