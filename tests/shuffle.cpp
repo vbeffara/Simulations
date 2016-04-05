9200c6ff310920f59aa98824cb94694919cb1558
Vincent Beffara <vbeffara@gmail.com>
2016-04-05 01:16:06 +0100

    Refactor domino shuffle, gain factor 3 in time

--- tests/shuffle.cpp
+++ tests/shuffle.cpp
@@ -12,163 +12,106 @@ auto threeperiodic (int m, double a, double b, double c) {
     return A;
 }
 
-auto d3p (vector<vector<double>> x1) {
-    int n = x1.size();
+auto d3p (const vector<vector<double>> & x1) {
     vector<vector<vector<vector<double>>>> A;
-    vector<vector<vector<double>>> B;
+    int n = x1.size(); A.resize (n/2);
 
-    for (int i=0; i<n; ++i) {
-        vector<vector<double>> row;
-        for (int j=0; j<n; ++j) {
-            if (x1[i][j] == 0) row.push_back ({1,1});
-            else row.push_back ({x1[i][j],0});
-        }
-        B.push_back(row);
-    }
-    A.push_back(B);
+    A[0].resize (n, vector<vector<double>> (n));
+    for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) if (x1[i][j] == 0) A[0][i][j] = {1,1}; else A[0][i][j] = {x1[i][j],0};
 
     for (int k=0; k<n/2-1; ++k) {
-        vector<vector<vector<double>>> C;
+        A[k+1].resize(n-2*k-2, vector<vector<double>> (n-2*k-2));
         for (int i=0; i<n-2*k-2; ++i) {
-            vector<vector<double>> row;
             for (int j=0; j<n-2*k-2; ++j) {
-                vector<double> a1 = A[k][i+2*(i%2)][j+2*(j%2)], a2;
+                const vector<double> & a1 = A[k][i+2*(i%2)][j+2*(j%2)];
+                double a20, a21;
                 if (A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1] == A[k][i+2*(i%2)][j+1][1] + A[k][i+1][j+2*(j%2)][1]) {
-                    a2 = { A[k][i+2*(i%2)][j+2*(j%2)][0] * A[k][i+1][j+1][0] + A[k][i+2*(i%2)][j+1][0] * A[k][i+1][j+2*(j%2)][0],
-                        A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1] };
+                    a20 = A[k][i+2*(i%2)][j+2*(j%2)][0] * A[k][i+1][j+1][0] + A[k][i+2*(i%2)][j+1][0] * A[k][i+1][j+2*(j%2)][0];
+                    a21 = A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1];
                 } else if (A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1] < A[k][i+2*(i%2)][j+1][1] + A[k][i+1][j+2*(j%2)][1]) {
-                    a2 = { A[k][i+2*(i%2)][j+2*(j%2)][0] * A[k][i+1][j+1][0], A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1] };
+                    a20 = A[k][i+2*(i%2)][j+2*(j%2)][0] * A[k][i+1][j+1][0];
+                    a21 = A[k][i+2*(i%2)][j+2*(j%2)][1] + A[k][i+1][j+1][1];
                 } else {
-                    a2 = { A[k][i+2*(i%2)][j+1][0] * A[k][i+1][j+2*(j%2)][0], A[k][i+2*(i%2)][j+1][1] + A[k][i+1][j+2*(j%2)][1] };
+                    a20 = A[k][i+2*(i%2)][j+1][0] * A[k][i+1][j+2*(j%2)][0];
+                    a21 = A[k][i+2*(i%2)][j+1][1] + A[k][i+1][j+2*(j%2)][1];
                 }
-                row.push_back ( { a1[0]/a2[0], a1[1]-a2[1] } );
+                A[k+1][i][j] = { a1[0]/a20, a1[1]-a21 };
             }
-            C.push_back(row);
         }
-        A.push_back(C);
     }
     return A;
 }
 
-auto probs (vector<vector<double>> x1) {
+auto probs (const vector<vector<double>> & x1) {
     auto a0 = d3p (x1);
     int n = a0.size();
-    vector<vector<vector<double>>> A;
+    vector<vector<vector<double>>> A (n);
 
     for (int k=0; k<n; ++k) {
-        vector<vector<double>> C;
+        A[k].resize (k+1, vector<double> (k+1));
         for (int i=0; i<k+1; ++i) {
-            vector<double> row;
             for (int j=0; j<k+1; ++j) {
                 if (a0[n-k-1][2*i][2*j][1] + a0[n-k-1][2*i+1][2*j+1][1] > a0[n-k-1][2*i+1][2*j][1] + a0[n-k-1][2*i][2*j+1][1])
-                    row.push_back (0);
+                    A[k][i][j] = 0;
                 else if (a0[n-k-1][2*i][2*j][1] + a0[n-k-1][2*i+1][2*j+1][1] < a0[n-k-1][2*i+1][2*j][1] + a0[n-k-1][2*i][2*j+1][1])
-                    row.push_back (1);
+                    A[k][i][j] = 1;
                 else
-                    row.push_back (a0[n-k-1][2*i+1][2*j+1][0] * a0[n-k-1][2*i][2*j][0] /
+                    A[k][i][j] = (a0[n-k-1][2*i+1][2*j+1][0] * a0[n-k-1][2*i][2*j][0] /
                         (a0[n-k-1][2*i+1][2*j+1][0] * a0[n-k-1][2*i][2*j][0] + a0[n-k-1][2*i+1][2*j][0]*a0[n-k-1][2*i][2*j+1][0]));
             }
-            C.push_back(row);
         }
-        A.push_back(C);
     }
     return A;
 }
 
-auto delslide (vector<vector<int>> x1) {
+auto delslide (const vector<vector<int>> & x1) {
     int n = x1.size();
-    vector<vector<int>> a0;
+    vector<vector<int>> a0 (n+2, vector<int> (n+2));
     for (int i=0; i<n+2; ++i) {
-        vector<int> row;
-        for (int j=0; j<n+2; ++j) {
-            if ((i==0) || (i==n+1) || (j==0) || (j==n+1))
-                row.push_back (0);
-            else
-                row.push_back (x1[i-1][j-1]);
-        }
-        a0.push_back (row);
+        for (int j=0; j<n+2; ++j) if ((i==0) || (i==n+1) || (j==0) || (j==n+1)) a0[i][j] = 0; else a0[i][j] = x1[i-1][j-1];
     }
     for (int i=0; i<n/2; ++i) {
         for (int j=0; j<n/2; ++j) {
-            if ((a0[2*i][2*j]==1) && (a0[2*i+1][2*j+1]==1)) {
-                a0[2*i][2*j]=0;
-                a0[2*i+1][2*j+1]=0;
-            }
-            else if ((a0[2*i][2*j+1]==1) && (a0[2*i+1][2*j]==1)) {
-                a0[2*i+1][2*j]=0;
-                a0[2*i][2*j+1]=0;
-            }
+            if ((a0[2*i][2*j]==1) && (a0[2*i+1][2*j+1]==1)) { a0[2*i][2*j]=0; a0[2*i+1][2*j+1]=0; }
+            else if ((a0[2*i][2*j+1]==1) && (a0[2*i+1][2*j]==1)) { a0[2*i+1][2*j]=0; a0[2*i][2*j+1]=0; }
         }
     }
     for (int i=0; i<n/2+1; ++i) {
         for (int j=0; j<n/2+1; ++j) {
-            if (a0[2*i+1][2*j+1]==1) {
-                a0[2*i][2*j]=1;
-                a0[2*i+1][2*j+1]=0;
-            } else if (a0[2*i][2*j]==1) {
-                a0[2*i][2*j]=0;
-                a0[2*i+1][2*j+1]=1;
-            } else if (a0[2*i+1][2*j]==1) {
-                a0[2*i][2*j+1]=1;
-                a0[2*i+1][2*j]=0;
-            } else if (a0[2*i][2*j+1]==1) {
-                a0[2*i+1][2*j]=1;
-                a0[2*i][2*j+1]=0;
-            }
+            if (a0[2*i+1][2*j+1]==1) { a0[2*i][2*j]=1; a0[2*i+1][2*j+1]=0; }
+            else if (a0[2*i][2*j]==1) { a0[2*i][2*j]=0; a0[2*i+1][2*j+1]=1; }
+            else if (a0[2*i+1][2*j]==1) { a0[2*i][2*j+1]=1; a0[2*i+1][2*j]=0; }
+            else if (a0[2*i][2*j+1]==1) { a0[2*i+1][2*j]=1; a0[2*i][2*j+1]=0; }
         }
     }
     return a0;
 }
 
-auto create (vector<vector<int>> x0, vector<vector<double>> p) {
+auto create (vector<vector<int>> & x0, const vector<vector<double>> & p) {
     int n = x0.size();
     for (int i=0; i<n/2; ++i) {
         for (int j=0; j<n/2; ++j) {
             if ((x0[2*i][2*j]==0) && (x0[2*i+1][2*j]==0) && (x0[2*i][2*j+1]==0) && (x0[2*i+1][2*j+1]==0)) {
                 bool a1,a2,a3,a4;
-                if (j>0)
-                    a1 = (x0[2*i][2*j-1]==0) && (x0[2*i+1][2*j-1]==0);
-                else
-                    a1 = true;
-                if (j<n/2-1)
-                    a2 = (x0[2*i][2*j+2]==0) && (x0[2*i+1][2*j+2]==0);
-                else
-                    a2 = true;
-                if (i>0)
-                    a3 = (x0[2*i-1][2*j]==0) && (x0[2*i-1][2*j+1]==0);
-                else
-                    a3 = true;
-                if (i<n/2-1)
-                    a4 = (x0[2*i+2][2*j]==0) && (x0[2*i+2][2*j+1]==0);
-                else
-                    a4 = true;
+                if (j>0) a1 = (x0[2*i][2*j-1]==0) && (x0[2*i+1][2*j-1]==0); else a1 = true;
+                if (j<n/2-1) a2 = (x0[2*i][2*j+2]==0) && (x0[2*i+1][2*j+2]==0); else a2 = true;
+                if (i>0) a3 = (x0[2*i-1][2*j]==0) && (x0[2*i-1][2*j+1]==0); else a3 = true;
+                if (i<n/2-1) a4 = (x0[2*i+2][2*j]==0) && (x0[2*i+2][2*j+1]==0); else a4 = true;
                 if (a1 && a2 && a3 && a4) {
-                    if (prng.bernoulli(p[i][j])) {
-                        x0[2*i][2*j]=1;
-                        x0[2*i+1][2*j+1]=1;
-                    } else {
-                        x0[2*i+1][2*j]=1;
-                        x0[2*i][2*j+1]=1;
-                    }
+                    if (prng.bernoulli(p[i][j])) { x0[2*i][2*j]=1; x0[2*i+1][2*j+1]=1; }
+                    else { x0[2*i+1][2*j]=1; x0[2*i][2*j+1]=1; }
                 }
             }
         }
     }
-    return x0;
 }
 
 auto aztecgen (vector<vector<double>> xr) {
     auto x0 = probs(xr);
     int n = x0.size();
     vector<vector<int>> a1;
-    if (prng.bernoulli(x0[0][0][0]))
-        a1 = {{1,0},{0,1}};
-    else
-        a1 = {{0,1},{1,0}};
-    for (int i=0; i<n-1; ++i) {
-        a1=delslide(a1);
-        a1=create(a1,x0[i+1]);
-    }
+    if (prng.bernoulli(x0[0][0][0])) a1 = {{1,0},{0,1}}; else a1 = {{0,1},{1,0}};
+    for (int i=0; i<n-1; ++i) { a1=delslide(a1); create(a1,x0[i+1]); }
     return a1;
 }
 
