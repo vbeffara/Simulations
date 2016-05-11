440e5606c31416028e51a6708e1fc709fe24915a
Vincent Beffara <vbeffara@gmail.com>
2016-05-11 11:35:59 +0200

    Customizable grey level for 2D/Perco

--- 2D/Perco.cpp
+++ 2D/Perco.cpp
@@ -1,20 +1,22 @@
 #include <vb/Image.h>
 
+
 using namespace vb;
 
 int main(int argc, char ** argv) {
-  H.init ("Percolation", argc, argv, "n=500,p=.5,d,s=0");
+  H.init ("Percolation", argc, argv, "n=500,p=.5,d,s=0,t=128");
   int    n = H['n']; n -= (n%4);
   double p = H['p'];
   bool   d = H['d'];
   int    s = H['s'];
+  int tint = H['t'];
 
   Image img (n,n);
 
   for (int x=0; x<n; ++x) {
     for (int y=0; y<n; ++y) {
-      if      ((x%2 == 0) && (y%2 == 0)) img.put (coo(x,y), 128);
-      else if ((x%2 == 0) || (y%2 == 0)) img.put (coo(x,y), prng.bernoulli(p) * 128);
+      if      ((x%2 == 0) && (y%2 == 0)) img.put (coo(x,y), tint);
+      else if ((x%2 == 0) || (y%2 == 0)) img.put (coo(x,y), prng.bernoulli(p) * tint);
     }
   }
 
@@ -30,11 +32,11 @@ int main(int argc, char ** argv) {
       if ((x+y)%2 == 0) continue;
       bool on_it = (img.at(coo(x,y)) == Color(255,0,0));
 
-      Color c = prng.bernoulli(p) * 128;
+      Color c = prng.bernoulli(p) * tint;
       if (img.at(coo(x,y)) == c) continue;
-      if (on_it && (c == Color(128))) continue;
+      if (on_it && (c == Color(tint))) continue;
 
-      img.fill (coo(n/2,n/2), 128);
+      img.fill (coo(n/2,n/2), tint);
       img.put (coo(x,y), c);
       img.fill (coo(n/2,n/2), Color(255,0,0));
 
