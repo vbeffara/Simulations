89468bc8cadb3ce7b6912925460e5132abc80629
Vincent Beffara <vbeffara@gmail.com>
2016-06-17 15:36:07 +0200

    Saner choice of when to redisplay the Cluster-based ORRW

--- tests/test_cluster.cpp
+++ tests/test_cluster.cpp
@@ -16,26 +16,26 @@ class Cluster2 : public Cluster { public:
 };
 
 int main (int argc, char ** argv) {
-    H.init ("Once-reinforced random walk, Cluster version", argc,argv, "l=200,a=10,v,s=0");
+    H.init ("Once-reinforced random walk, Cluster version", argc,argv, "l=0,a=10,v,s=0");
     int l = H['l'];
     double a = H['a']; a = 1 / (1+a);
     int s = H['s']; if (s) prng.seed(s);
     Image I (729,729); I.show();
     if (H['v']) I.snapshot_setup("ORRW_cluster",60);
 
-    Cluster2 W; coo z(0,0); W.insert(z); long supsup=0;
+    Cluster2 W; coo z(0,0); W.insert(z); long sup_=0, t_=0;
 
-    for (unsigned long t=0 ; supsup < l; t++) {
+    for (long t=0 ;; t++) {
 		int ir = W.inrad_go(z), d = 1 + ir/2;
         coo nz = z + dz[prng.uniform_int(4)] * d;
         bool there = (d>1) ? true : W.at(nz);
         if (there || prng.bernoulli(a)) {
 			z = nz; if (!there) W.insert(z);
-			supsup = max (supsup,sup(z));
-		}
-        if (!(t%1000000)) {
-			cerr << "\r" << t << " particles.";
-			W.paint (I);
+			long ns = sup(z); if ((ns >= sup_ + 10) || (t >= t_ + 1000000)) {
+				sup_ = max (sup_, ns); t_ = t; W.paint (I);
+				cerr << "\rtime = " << double(t) << " radius = " << sup_ << " particles = " << W.np << "      ";
+				if (l && (sup_ >= l)) break;
+			}
 		}
     }
 }
