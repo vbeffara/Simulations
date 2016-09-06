947d6faaac125c55f41eb4bf0489cc7970b7c49e
Vincent Beffara <vbeffara@gmail.com>
2016-09-06 15:24:59 +0200

    Finished optimizing Diploid, most of time now spent in I/O

--- 1D/Diploid.cpp
+++ 1D/Diploid.cpp
@@ -3,91 +3,92 @@
 
 using namespace vb; using namespace std;
 
-vector<double> BR (const vector<int> & nu) {
-	const double naa = nu[0], naA = nu[1], nAA = nu[2], naB = nu[3], nAB = nu[4], nBB = nu[5];
-	return {
-		naa * (naa + naA/2) / (naa + naA + nAA)
+class Diploid { public:
+	int K;
+	double f = 2, d = .5, delta = .5, GC = 1, eta = .003;
+
+	Diploid () : K(H['K']) {
+		double DaA = d, DAA = d, Daa = d + delta, DaB = d - delta, DAB = d - delta, DBB = d - delta;
+		DR = {Daa, DaA, DAA, DaB, DAB, DBB};
+		B = {0,0,0,0,0,0}; De = {0,0,0,0,0,0};
+
+		double c0 = GC/K, c1 = GC/K, c2 = GC/K, c3 = (GC - eta)/K;
+		Comp = {{c0,c1,c2,0,0,0},
+				{c1,c0,c1,c1,c2,c3},
+				{c2,c1,c0,c2,c1,c2},
+				{0,c1,c2,c0,c1,c2},
+				{0,c2,c1,c1,c0,c1},
+				{0,c3,c2,c2,c1,c0}};
+
+		nu = { K/5, K, 5*K, 0, 1, 0 };
+	}
+
+	void compute_B () {
+		const double naa = nu[0], naA = nu[1], nAA = nu[2], naB = nu[3], nAB = nu[4], nBB = nu[5];
+
+		B[0] = naa * (naa + naA/2) / (naa + naA + nAA)
 			+ (naA/2 + naB/2) * naB / (2 * (naA + nAA + naB + nAB + nBB))
-			+ naA * (naa + naA/2 + naB/2) / (2 * (naa + naA + nAA + naB + nAB + nBB)),
-	    .25 * (2 * naa * (naA + 2 * nAA) / (naa + naA + nAA)
+			+ naA * (naa + naA/2 + naB/2) / (2 * (naa + naA + nAA + naB + nAB + nBB));
+		B[1] = .25 * (2 * naa * (naA + 2 * nAA) / (naa + naA + nAA)
 			+ (2 * naB * (nAA + nAB) + naA * (naB + nAB)) / (naA + nAA + naB + nAB + nBB)
-			+ ((naA + 2 * nAA) * (2 * (naa + naA) + naB) + naA * nAB) / (naa + naA + nAA + naB + nAB + nBB)),
-		(naA/2 + nAA + nAB/2) * nAB / (2 * (naA + nAA + naB + nAB + nBB))
+			+ ((naA + 2 * nAA) * (2 * (naa + naA) + naB) + naA * nAB) / (naa + naA + nAA + naB + nAB + nBB));
+		B[2] = (naA/2 + nAA + nAB/2) * nAB / (2 * (naA + nAA + naB + nAB + nBB))
 			+ naA * (naA/2 + nAA + nAB/2) / (2 * (naa + naA + nAA + naB + nAB + nBB))
-			+ nAA * (naA/2 + nAA + nAB/2) / (naa + naA + nAA + naB + nAB + nBB),
-		(naA/2 + naB/2) * naB / (2 * (naA + nAA + naB + nAB + nBB))
+			+ nAA * (naA/2 + nAA + nAB/2) / (naa + naA + nAA + naB + nAB + nBB);
+		B[3] = (naA/2 + naB/2) * naB / (2 * (naA + nAA + naB + nAB + nBB))
 			+ (naA/2 + naB/2) * nAB / (2 * (naA + nAA + naB + nAB + nBB))
 			+ (naA/2 + naB/2) * nBB / (naA + nAA + naB + nAB + nBB)
 			+ (naB * (naB/2 + nAB/2 + nBB)) / (2 * (naA + nAA + naB + nAB + nBB))
-			+ (naA * (naB/2 + nAB/2 + nBB)) / (2 * (naa + naA + nAA + naB + nAB + nBB)),
-		naB * (naA/2 + nAA + nAB/2) / (2 * (naA + nAA + naB + nAB + nBB))
+			+ (naA * (naB/2 + nAB/2 + nBB)) / (2 * (naa + naA + nAA + naB + nAB + nBB));
+		B[4] = naB * (naA/2 + nAA + nAB/2) / (2 * (naA + nAA + naB + nAB + nBB))
 			+ (naA/2 + nAA + nAB/2) * nAB / (2 * (naA + nAA + naB + nAB + nBB))
 			+ (naA/2 + nAA + nAB/2) * nBB / (naA + nAA + naB + nAB + nBB)
 			+ nAB * (naB/2 + nAB/2 + nBB) / (2 * (naA + nAA + naB + nAB + nBB))
 			+ naA * (naB/2 + nAB/2 + nBB) / (2 * (naa + naA + nAA + naB + nAB + nBB))
-			+ nAA * (naB/2 + nAB/2 + nBB) / (naa + naA + nAA + naB + nAB + nBB),
-		(naB + nAB + 2 * nBB) * (naB + nAB + 2 * nBB) / (4 * (naA + nAA + naB + nAB + nBB))
-	};
-}
-
-double f = 2, d = .5, delta = .5, GC = 1, eta = .003;
-int K = 200;
-
-double DaA = d, DAA = d, Daa = d + delta, DaB = d - delta, DAB = d - delta, DBB = d - delta;
-vector<double> DR = {Daa, DaA, DAA, DaB, DAB, DBB};
-
-double c0 = GC/K, c1 = GC/K, c2 = GC/K, c3 = (GC - eta)/K, c4 = (GC - eta)/K;
-
-vector<vector<double>> Comp =
-	{{c0, c1, c2, 0, 0, 0},
-	{c1, c0, c1, c1, c2, c3},
-	{c2, c1, c0, c2, c1, c2},
-	{0, c1, c2, c0, c1, c2},
-	{0, c2, c1, c1, c0, c1},
-	{0, c3, c2, c2, c1, c0}};
-
-double NewNu (vector<int> & nu) {
-	vector<double> De = {0,0,0,0,0,0};
-
-	for (int i=0; i<6; ++i) {
-		De[i] = DR[i];
-		for (int j=0; j<6; ++j) De[i] += Comp[i][j] * nu[j];
-		De[i] *= nu[i];
+			+ nAA * (naB/2 + nAB/2 + nBB) / (naa + naA + nAA + naB + nAB + nBB);
+		B[5] = (naB + nAB + 2 * nBB) * (naB + nAB + 2 * nBB) / (4 * (naA + nAA + naB + nAB + nBB));
 	}
 
-	vector<double> B = BR(nu); for (auto & x : B) x *= f;
+	double NewNu () {
+		for (int i=0; i<6; ++i) {
+			De[i] = DR[i];
+			for (int j=0; j<6; ++j) De[i] += Comp[i][j] * nu[j];
+			De[i] *= nu[i];
+		}
 
-	double BB = 0; for (auto x : B) BB += x;
-	double DD = 0; for (auto x : De) DD += x;
-	double Tot = BB + DD;
+		compute_B();
 
-	if (prng.bernoulli (BB/Tot)) {
-		for (auto & x : B) x /= BB;
-		int x = prng.discrete(B);
-		nu[x] += 1;
-	} else {
-		for (auto & x : De) x /= DD;
-		int x = prng.discrete(De);
-	    nu[x] -= 1; if (nu[x]<.1) nu[x] = 0;
+		double BB = 0; for (const auto & x : B) BB += x;
+		double DD = 0; for (const auto & x : De) DD += x;
+
+		if (prng.bernoulli (BB*f/(BB*f+DD))) {
+			for (auto & x : B) x /= BB;
+			nu[prng.discrete(B)] += 1;
+		} else {
+			for (auto & x : De) x /= DD;
+		    nu[prng.discrete(De)] -= 1;
+		}
+
+		return prng.exponential(BB*f+DD);
 	}
 
-	return prng.exponential(Tot);
-}
+	vector<int> nu;
+	vector<double> DR, B, De;
+	vector<vector<double>> Comp;
+};
 
 int main (int argc, char ** argv) {
-	H.init ("Diploid model", argc, argv, "s=0,i=60000");
+	H.init ("Diploid model", argc, argv, "s=0,i=60000,K=200");
 
 	if (int seed = H['s']) prng.seed(seed);
-	int iterations = H['i'];
 
-	vector<int> nu = { K/5, K, 5*K, 0, 1, 0};
+	Diploid D;
 
-	double t = 0;
-
-	for (int k=1; k<iterations; ++k) {
-		t += NewNu (nu);
-		cout << t; for (const auto & x : nu) cout << " " << x; cout << endl;
+	double t = 0; int iterations = H['i'];
+	for (int k=0; k<iterations; ++k) {
+		t += D.NewNu();
+		cout << t; for (const auto & x : D.nu) cout << " " << x; cout << endl;
 	}
 
-	cerr << t; for (const auto & x : nu) cerr << " " << x; cerr << endl;
+	cerr << t; for (const auto & x : D.nu) cerr << " " << x; cerr << endl;
 }
