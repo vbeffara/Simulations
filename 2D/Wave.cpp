0675e653bd61e6da18bb9816e83742bee43cfd75
Vincent Beffara <vbeffara@gmail.com>
2016-10-10 22:37:58 +0200

    Saner defaults for 2D/Wave, and choices of cutoff

--- 2D/Wave.cpp
+++ 2D/Wave.cpp
@@ -5,15 +5,15 @@ using namespace vb; using namespace std;
 
 class Wave : public Coloring { public:
 	class Mode { public:
-		Mode (double a_, double t_, double l_) : a(a_), c(cos(t_)/l_), s(sin(t_)/l_), r(H['r']) {}
-		double operator() (const cpx & z) { double out = cos(c*real(z) + s*imag(z)); if (r) out += cos(c*real(z) - s*imag(z)); return a * out; }
-		double a,c,s;
-		bool r;
+		Mode (double a_, double t, double l, double p_) : a(a_), c(l*cos(t)), s(l*sin(t)), p(t) {}
+		double operator() (const cpx & z) { return a * cos(c*real(z) + s*imag(z) + p); }
+		double a,c,s,p;
 	};
 
-	Wave (int n, int k, double l_) : Coloring (cpx(-n,-n), cpx(n,n), n, [&](cpx z){return c(z);}) {
+	Wave (int n, int k, double l_, double e_, double u_) : Coloring (cpx(-n,-n), cpx(n,n), n, [&](cpx z){return c(z);}) {
 		for (int i=0; i<k; ++i) {
-			m.emplace_back (prng.gaussian(), prng.uniform_real(0,2.0*M_PI), l_);
+			double ll = (1 + e_ * prng.gaussian() + u_ * (2*prng.uniform_real()-1)) * l_;
+			m.emplace_back (prng.gaussian(), prng.uniform_real(0,2.0*M_PI), ll, prng.uniform_real(0,2.0*M_PI));
 		}
 	}
 
@@ -25,6 +25,6 @@ class Wave : public Coloring { public:
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Random planar waves", argc, argv, "n=600,k=100,l=5,r");
-	Wave W(H['n'],H['k'],H['l']); W.show(); W.output(); Fl::run();
+	H.init ("Random planar waves", argc, argv, "n=600,k=1000,l=.1,e=0,u=0");
+	Wave W(H['n'],H['k'],H['l'],H['e'],H['u']); W.show(); W.output(); Fl::run();
 }
