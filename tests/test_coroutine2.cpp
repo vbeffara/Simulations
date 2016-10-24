05c0865c282eddf6cb03730a90e9223dbff11748
Vincent Beffara <vbeffara@gmail.com>
2016-10-24 20:15:14 +0200

    OK, apparently coroutine2::push_type does a move, make a copy

--- tests/test_coroutine2.cpp
+++ tests/test_coroutine2.cpp
@@ -1,19 +1,30 @@
 #include <vb/Hub.h>
-// #include <vb/Permutation.h>
+#include <vb/Permutation.h>
 #include <boost/coroutine2/all.hpp>
 
 using namespace vb; using namespace std;
 
 template <typename T> using coro_t = boost::coroutines2::coroutine<T>;
-template <typename T> using Stream2 = typename coro_t<T>::pull_type;
 template <typename T> using Sink2 = typename coro_t<T>::push_type;
 
-// auto genperm2 (int n) {
-//     return Stream2 <Permutation> ([n](Sink2 <Permutation> & yield) {
-//         Permutation p(n);
-//         do { yield(p); } while ( next_permutation(p.begin(),p.end()) );
-//     });
-// }
+template <typename T> struct Stream2 : public coro_t<T>::pull_type {
+    using coro_t<T>::pull_type::pull_type;
+};
+
+template <typename T, typename F> auto filter (F && f, Stream2 <T> & S) {
+    return Stream2 <T> ([&](Sink2 <T> & yield) { for (auto x : S) if (f(x)) yield(x); });
+}
+
+auto genperm2 (int n) {
+    return Stream2 <Permutation> ([n](Sink2 <Permutation> & yield) {
+        Permutation p(n);
+        do { yield(Permutation(p)); } while ( next_permutation(p.begin(),p.end()) );
+    });
+}
+
+template <typename U, typename F> auto fmap (F && f, Stream2 <U> & S) {
+    return Stream2 <decltype(f(U()))> ([&](Sink2 <decltype(f(U()))> & yield) { for (auto x : S) yield(f(x)); });
+}
 
 auto ints () {
     return Stream2 <int> ([](Sink2 <int> & yield) {
@@ -28,14 +39,13 @@ template <typename T> auto take (int n, Stream2 <T> & S) {
 	});
 }
 
-// bool good (const Permutation & p) { return p[0]==2; }
-// int sec (const Permutation & p) { return p[2]; }
+bool good (const Permutation & p) { return p[0]==2; }
+int sec (const Permutation & p) { return p[2]; }
 
 int main(int argc, char ** argv) {
-    H.init ("Testing coroutines", argc, argv, "n=10");
-    auto S = ints ();
-    auto SS = take <int> (H['n'], S);
-    for (auto p : SS) H.L->info (p);
-    // auto SS = filter (good,S);
-    // auto SSS = fmap<Permutation,int> (sec,SS);
+    H.init ("Testing coroutines", argc, argv, "n=5");
+    auto S = genperm2(H['n']);
+    auto SS = filter (good,S);
+    auto SSS = fmap (sec,SS);
+    for (const auto & p : SSS) H.L->info ("{}",p);
 }
