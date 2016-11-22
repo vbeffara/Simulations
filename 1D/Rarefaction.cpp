32c892f7ee38600683cdd45952ef8eabe7cc0264
Vincent Beffara <vbeffara@gmail.com>
2016-11-22 09:31:52 +0100

    Completely rewrite 1D/Rarefaction

--- 1D/Rarefaction.cpp
+++ 1D/Rarefaction.cpp
@@ -1,57 +1,19 @@
-/*
- * Rarefaction - TASEP Rarefaction fan
- */
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <unistd.h>
-
+#include <vb/Hub.h>
 #include <vb/PRNG.h>
 
-long n;
+using namespace vb; using namespace std;
 
 int main (int argc, char **argv) {
-  int *field;
-  int i,t,running;
-  char ch;
+	H.init ("Rarefaction fan", argc, argv, "n=20");
+	int n = H['n'];
 
-  n = 20;
+	vector<int> field (2*n); for (int i=0; i<2*n; i++) field[i]=n-i;
 
-  while ((ch = getopt(argc,argv,"hn:")) != -1) {
-    switch (ch) {
-    case 'n':
-      n = atoi (optarg);
-      break;
-    case 'h':
-      fprintf (stderr, "Syntax: %s [-n size]\n", argv[0]);
-      exit (1);
-      break;
-    }
-  }
+	while (true) {
+		int i = prng.uniform_int(2*n-1);
+		if (field[i]>field[i+1]) swap (field[i],field[i+1]);
+		if ((field[0]<0) || (field[2*n-1]>0)) break;
+	}
 
-  field = (int*) calloc (2*n,sizeof(int));
-
-  for (i=0;i<2*n;i++) field[i]=n-i;
-
-  running=1;
-  while (running) {
-    i = vb::prng()%(2*n-1);
-    if (field[i]>field[i+1]) {
-      t = field[i];
-      field[i] = field[i+1];
-      field[i+1] = t;
-    }
-    if ((field[0]<0) || (field[2*n-1]>0))
-      running=0;
-  }
-
-  for (i=0;i<2*n;i++) {
-    printf ("%d %d\n", i, field[i]);
-  }
-
-  free(field);
-
-  return 0;
+	for (int i=0; i<2*n; i++) cout << i << " " << field[i] << endl;
 }
