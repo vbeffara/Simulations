60c2b4d9f1009782bd41de11e72a5834cfe203d4
Vincent Beffara <vbeffara@gmail.com>
2017-01-06 10:05:36 +0100

    More proper output for the hexagonal lattice

--- 2D/Ising-bis.cpp
+++ 2D/Ising-bis.cpp
@@ -1,33 +1,54 @@
 #include <vb/Bitmap.h>
+#include <vb/Figure.h>
 
 using namespace vb; using namespace std;
 
 struct Spin {
 	Spin (int s) : s(s) {}
-	operator Color() { return 255*s; }
+	operator Color() { return s ? Indexed((3+s)/2) : Color(128); }
 	int s;
 };
 
 class Ising : public Bitmap<Spin> { public:
-	Ising (int n, double beta, int con) : Bitmap<Spin>(n,n), beta(beta), con(con) {
-		for (auto & s : *this) s = prng.bernoulli(.5);
-		for (int c=0; c<=con; ++c) p.push_back (1 / (1 + exp(2*beta*(con-2*c))));
+	Ising (int n, double beta, int con) : Bitmap<Spin>(n,n), con(con), p(2*con+1) {
+		for (auto & s : *this) s = 2*prng.bernoulli(.5)-1;
+		if (con==6) {
+			int m=2*n/3;
+			for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) if ((j>m) || (i<j/2) || (i>m+j/2)) put(coo(i,j),0);
+		}
+		for (int c=-con; c<=con; ++c) p[c+con] = 1 / (1 + exp(-beta*c));
 	};
 
 	void run() {
-		while (true) {
-			coo z = rand(); int c = 0;
-			for (int d=0; d<con; ++d) c += atp(z+dz[d]).s;
-			put(z,prng.bernoulli(p[c]));
+		while (visible()) {
+			coo z = rand(); if (!at(z).s) continue;
+			int c = 0; for (int d=0; d<con; ++d) c += atp(z+dz[d]).s;
+			put(z,2*prng.bernoulli(p[c+con])-1);
 		}
 	}
 
-	double beta;
+	void output_pdf (const std::string &s = "") {
+		Figure F; cpx shift; vector<cpx> pattern;
+		if (con==6) {
+			shift = {-.5, sqrt(3.0)/2};
+			for (int k=0; k<6; ++k) pattern.push_back (exp(cpx(0,(2*k+1)*M_PI/6))/sqrt(3.0));
+		} else {
+			shift = I;
+			pattern = {{.5,.5}, {-.5,.5}, {-.5,-.5}, {.5,-.5}};
+		}
+		for (int i=0; i<w(); ++i) for (int j=0; j<h(); ++j) {
+			Color c = at(coo(i,j)); if (c==Spin(0)) continue;
+			vector<cpx> p; for (auto zz : pattern) p.push_back (cpx(i) + cpx(j)*shift + zz);
+			F.add (make_unique<Polygon> (p,Pen(0,1,c,true)));
+		}
+		F.output_pdf(s);
+	}
+
 	int con;
 	vector<double> p;
 };
 
 int main (int argc, char ** argv) {
 	H.init ("2D Ising model", argc, argv, "n=500,b=.7,c=4");
-	Ising I (H['n'],H['b'],H['c']); I.show(); I.run();
+	Ising I (H['n'],H['b'],H['c']); I.show(); I.run(); I.output_pdf();
 }
