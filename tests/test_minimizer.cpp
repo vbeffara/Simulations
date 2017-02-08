ed30133ce35a07f36e56cfbadbfc4d18b0c00972
Vincent Beffara <vbeffara@gmail.com>
2017-02-09 00:14:17 +0100

    Use explicit gradient, trace progress

--- tests/test_minimizer.cpp
+++ tests/test_minimizer.cpp
@@ -13,24 +13,33 @@ FitFunc f = [](const double *x, const int N) {
   return o;
 };
 
+GradFunc df = [](const double *x, const int N) {
+  dVec grad(N);
+  for (int i=0; i<N; i++) { double xi = x[i]-i; grad(i) = xi * (2 * sin (xi*xi) + .02); }
+  return grad;
+};
+
+ProgressFunc <CMAParameters<>,CMASolutions> pf = [](const CMAParameters<> &cmaparams, const CMASolutions &cmasols) {
+	H.L->trace ("Current best : {}", cmasols.get_best_seen_candidate().get_fvalue());
+	return 0;
+};
+
 int main (int argc, char ** argv) {
 	H.init ("Tests of minimization strategies", argc, argv, "d=10,s=1,l=-1");
-	int dim = H['d'];
+	int dim = H['d'], lambda = H['l'];
 	double sigma = H['s'];
-	int lambda = H['l'];
 
 	vector<double> x0 (dim,0);
 	CMAParameters<> cmaparams(x0,sigma,lambda);
-	CMASolutions cmasols = cmaes<>(f,cmaparams);
+	cmaparams.set_algo (aCMAES);
 
+	spdlog::set_level(spdlog::level::trace);
+	auto cmasols = cmaes<>(f,cmaparams,pf,df);
+
+	H.L->info ("Final value : {}", cmasols.get_best_seen_candidate().get_fvalue());
 	std::cout << cmasols << std::endl;
-
-	auto C = cmasols.get_best_seen_candidate();
-	H.L->info ("Final value : {}", C.get_fvalue());
 }
 
 #else
-
 int main () { cerr << "Nothing to see here, move along.\n"; }
-
 #endif
