e9afa7393d142f7b70245dff738027a97460d2cb
Vincent Beffara <vbeffara@gmail.com>
2017-02-09 18:01:47 +0100

    Install eigen3 in the docker image, tweak libcmaes test

--- tests/test_minimizer.cpp
+++ tests/test_minimizer.cpp
@@ -8,32 +8,33 @@ using namespace std; using namespace vb;
 using namespace libcmaes;
 
 FitFunc f = [](const double *x, const int N) {
-  double o = 0;
-  for (int i=0; i<N; ++i) { double xi = x[i]-i; o += 1 - cos(xi*xi) + .01*xi*xi; }
-  return o;
+	double o = 0;
+	for (int i=0; i<N; ++i) { double xi = x[i]-i; o += 1 - cos(xi*xi) + .01*xi*xi; }
+	return o;
 };
 
 GradFunc df = [](const double *x, const int N) {
-  dVec grad(N);
-  for (int i=0; i<N; i++) { double xi = x[i]-i; grad(i) = xi * (2 * sin (xi*xi) + .02); }
-  return grad;
+	dVec grad(N);
+	for (int i=0; i<N; i++) { double xi = x[i]-i; grad(i) = xi * (2 * sin (xi*xi) + .02); }
+	return grad;
 };
 
-ProgressFunc <CMAParameters<>,CMASolutions> pf = [](const CMAParameters<> &cmaparams, const CMASolutions &cmasols) {
+ProgressFunc <CMAParameters<>,CMASolutions> pf = [](const CMAParameters<> &, const CMASolutions &cmasols) {
 	H.L->trace ("Current best : {}", cmasols.get_best_seen_candidate().get_fvalue());
 	return 0;
 };
 
 int main (int argc, char ** argv) {
-	H.init ("Tests of minimization strategies", argc, argv, "d=10,s=1,l=-1");
+	H.init ("Tests of minimization strategies", argc, argv, "d=10,s=1,l=-1,t,a=acmaes,m");
 	int dim = H['d'], lambda = H['l'];
 	double sigma = H['s'];
 
 	vector<double> x0 (dim,0);
 	CMAParameters<> cmaparams(x0,sigma,lambda);
-	cmaparams.set_algo (aCMAES);
+	cmaparams.set_str_algo (H['a']);
+	cmaparams.set_mt_feval (H['m']);
 
-	spdlog::set_level(spdlog::level::trace);
+	if (H['t']) spdlog::set_level(spdlog::level::trace);
 	auto cmasols = cmaes<>(f,cmaparams,pf,df);
 
 	H.L->info ("Final value : {}", cmasols.get_best_seen_candidate().get_fvalue());
