cca63a1cea1185d41ef1d88ff6ad25fd62bcf9a5
Vincent Beffara <vbeffara@gmail.com>
2017-02-10 14:17:03 +0100

    Use fullPivLu rather than colPivHouseholderQr for better compatibility

--- libvb/vb/impl/LinearAlgebra.hxx
+++ libvb/vb/impl/LinearAlgebra.hxx
@@ -3,6 +3,6 @@
 
 namespace vb {
 	template <typename T> Vector<T> solve (const Matrix<T> & A, const Vector<T> & y) {
-		return A.colPivHouseholderQr().solve(y);
+		return A.fullPivLu().solve(y);
 	}
 }
