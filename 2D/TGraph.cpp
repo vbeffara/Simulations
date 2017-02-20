2469fbefa811832fa18da9e172ca9bdf6ff0f541
Vincent Beffara <vbeffara@gmail.com>
2017-02-20 13:53:37 +0100

    Refactor vb::Console (start), and use in 2D/TGraph

--- 2D/TGraph.cpp
+++ 2D/TGraph.cpp
@@ -1,4 +1,5 @@
 #include <vb/Array.h>
+#include <vb/Console.h>
 #include <vb/Figure.h>
 
 using namespace vb; using namespace std;
@@ -8,10 +9,6 @@ struct loc { cpx z=0; };
 class TGraph : public Array<loc> {
 public:
 	TGraph (int n, cpx AA, cpx l) : Array<loc> (n,n), A(AA), lambda(l) {
-		a=abs(B-A); b=abs(C-B); c=abs(A-C);
-		alpha=(B-A)/a; beta=(C-B)/b; gamma=(A-C)/c;
-		pa=arg((C-A)/(B-A))/M_PI, pb=arg((A-B)/(C-B))/M_PI, pc=arg((B-C)/(A-C))/M_PI;
-
 		compute(); plot(); F.show(); F.output();
 	};
 
@@ -21,6 +18,11 @@ public:
 	}
 
 	void compute () {
+		H.L->info ("A = {}, lambda = {}", A, lambda);
+		a=abs(B-A); b=abs(C-B); c=abs(A-C);
+		alpha=(B-A)/a; beta=(C-B)/b; gamma=(A-C)/c;
+		pa=arg((C-A)/(B-A))/M_PI, pb=arg((A-B)/(C-B))/M_PI, pc=arg((B-C)/(A-C))/M_PI;
+
 		for (int i=0; i<ww; ++i) {
 			for (int j=0; j<hh; ++j) {
 				if (j>0) {
@@ -52,10 +54,24 @@ int main (int argc, char ** argv) {
 	H.init ("T-graph for the triangular lattice", argc, argv, "n=20,l");
 	int n=H['n'];
 	TGraph TG (n, cpx{.4,.6}, cpx{1,0});
+
 	if (H['l']) {
 		for (int i=0 ;; ++i) {
 			TG.lambda = exp(2.0*M_PI*I*cpx(i,0)/100.0); TG.compute(); TG.plot(); TG.F.update();
 			if (i==0) TG.F.output();
 		}
-	} else TG.F.pause();
+	} else {
+		Console C;
+		C.input (.5,0,1,[&TG](double x) {
+			TG.lambda = exp(2.0*M_PI*I*x); TG.compute(); TG.plot(); TG.F.update();
+		});
+		C.input (real(TG.A),-1,2,[&TG](double x) {
+			TG.A = {x,imag(TG.A)}; TG.compute(); TG.plot(); TG.F.update();
+		});
+		C.input (imag(TG.A),0,2,[&TG](double x) {
+			TG.A = {real(TG.A),x}; TG.compute(); TG.plot(); TG.F.update();
+		});
+		C.show();
+		C.pause();
+	}
 }
--- libvb/vb/Console.h
+++ libvb/vb/Console.h
@@ -6,48 +6,58 @@
 #include <FL/Fl_Window.H>
 
 namespace vb {
-	class Slot : public Fl_Output {
-	public:
-		Slot (const char *n, int w, int h) : Fl_Output (0,h,w-100,30,n) { align(FL_ALIGN_RIGHT); }
-		template <typename T> void draw_value (T t) {
-			value(boost::lexical_cast<std::string> (t).c_str()); Fl_Output::draw();
-		}
-	};
-
-	template <typename T> class Watcher : public Slot {
-	public:
-		Watcher (T &tt, const char *nn, int w, int h) : Slot (nn,w,h), t(tt) {}
-		void draw () override { draw_value(t); }
-		T &t;
-	};
-
-	template <typename T> class Lambda : public Slot {
-	public:
-		Lambda (std::function <T()> ff, const char *nn, int w, int h) : Slot (nn,w,h), f(std::move(ff)) {}
-		void draw () override { draw_value(f()); }
-		std::function <T()> f;
-	};
-
-	template <typename T> void apply (Fl_Widget *W);
-
-	template <typename T> class Manager : public Fl_Hor_Nice_Slider {
-	public:
-		Manager (T &tt, T t1, T t2, int w, int h) : Fl_Hor_Nice_Slider (0,h,w,30), t(tt) {
-			bounds (t1,t2); value (tt); callback (apply<T>);
-		}
-		T &t;
-	};
-
-	template <typename T> void apply (Fl_Widget *W) { auto M = static_cast <Manager<T>*> (W); M->t = M->value(); }
+	template <typename T> void runoutput (Fl_Widget *W);
+	template <typename T> void runinput (Fl_Widget *W);
 
 	class Console : public AutoWindow <Fl_Window> {
 	public:
+		template <typename T> struct Input : public Fl_Hor_Nice_Slider {
+			Input (T t, T t1, T t2, std::function <void(T)> cb, int w, int h) : Fl_Hor_Nice_Slider (0,h,w,30), t(t), f(move(cb)) {
+				bounds (t1,t2); value(t); callback (runinput<T>);
+			}
+			T t;
+			std::function <void(T)> f;
+		};
+
+		template <typename T> class Manager : public Fl_Hor_Nice_Slider {
+		public:
+			Manager (T &tt, T t1, T t2, int w, int h) : Fl_Hor_Nice_Slider (0,h,w,30), t(tt) {
+				bounds (t1,t2); value (tt); callback (runoutput<T>);
+			}
+			T &t;
+		};
+
+		template <typename T> struct Output : public Fl_Output {
+			Output (std::function <T()> ff, const char *nn, int w, int h) : Fl_Output (0,h,w-100,30,nn), f(std::move(ff)) {
+				align(FL_ALIGN_RIGHT);
+			}
+			void draw_value (T t) {
+				value(boost::lexical_cast<std::string> (t).c_str()); Fl_Output::draw();
+			}
+			void draw () override { draw_value(f()); }
+			std::function <T()> f;
+		};
+
 		Console () : AutoWindow (400,0) { label("Console"); }
 
 		void add (Fl_Widget *S) { size (w(),h()+S->h()); AutoWindow::add (S); }
 
-		template <typename T> void watch (T &t,                  	const char *n)	{ add (new Watcher<T> (t,n,w(),h())); }
-		template <typename T> void lambda (std::function <T()> f,	const char *n)	{ add (new Lambda<T> (f,n,w(),h())); }
-		template <typename T> void manage (T &t, T t1, T t2)     	              	{ add (new Manager<T> (t,t1,t2,w(),h())); }
+		template <typename T> void lambda (std::function <T()> f, const char *n) { add (new Output<T> (f,n,w(),h())); }
+
+		template <typename T> void watch (T &t, const char *n) { lambda<T> ([&t]{ return t; }, n); }
+
+		template <typename T> void manage (T &t, T t1, T t2) { add (new Manager<T> (t,t1,t2,w(),h())); }
+
+		void input (double t, double t1, double t2, std::function<void(double)> f) {
+			add (new Input<double> (t,t1,t2,move(f),w(),h()));
+		}
 	};
+
+	template <typename T> void runinput (Fl_Widget *W) {
+		auto M = static_cast <Console::Input<T>*> (W); M->t = M->value(); M->f(M->t);
+	}
+
+	template <typename T> void runoutput (Fl_Widget *W) {
+		auto M = static_cast <Console::Manager<T>*> (W); M->t = M->value();
+	}
 }
