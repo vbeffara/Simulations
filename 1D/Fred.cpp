d050e34a2dc4efe6ac27b3de1d3af402344939d7
Vincent Beffara <vbeffara@gmail.com>
2017-06-08 16:16:37 +0200

    More customization options in Boltzmann

--- 1D/Fred.cpp
+++ 1D/Fred.cpp
@@ -1,22 +1,26 @@
 #include <vb/Hub.h>
 #include <vb/PRNG.h>
+#include <vb/ProgressBar.h>
+#include <fstream>
 
 using namespace vb; using namespace std;
 
-struct state { double v,x; };
+struct state {
+    double v,x;
+    void step () { x = 2*x + .394875; while (x>=1) x -= 1; }
+};
 
 class Model : public vector<state> { public:
     Model (int n) : vector<state> (n,{0,0}) {
         for (unsigned k=0; k<size(); ++k) at(k).x = prng.uniform_real();
     }
 
-    void step (int k) {
-        double a=at(k).x*2*M_PI, c=cos(a), s=sin(a);
-        double z=at(k).v;
-        int l = (k+1)%size();
-        at(k).v = c*z + s*at(l).v; at(l).v = c*at(l).v - s*z;
-        at(k).x = 2*at(k).x + .394875; while (at(k).x>=1) at(k).x -= 1;
+    void rotate (int k, double a) {
+        double c=cos(a), s=sin(a), &x=at(k).v, &y=at((k+1)%size()).v;
+        tie(x,y) = make_tuple (c*x-s*y, s*x+c*y);
     }
+
+    void step (int k) { rotate (k,at(k).x*2*M_PI); at(k).step(); }
 
     void swipe () {
         for (unsigned k=0; k<size(); k+=2) step(k);
@@ -25,13 +29,24 @@ class Model : public vector<state> { public:
 };
 
 int main (int argc, char ** argv) {
-    H.init ("Deterministic Fourier law", argc, argv, "n=500");
-    int n=H['n']; vector<double> data (n,0);
-    for (int i=0; i<1000; ++i) {
-        Model M(n); M[n/2].v = 1;
-        for (int t=0; t<100; ++t) M.swipe();
-        for (int k=0; k<n; ++k) data[k] += M[k].v*M[k].v;
+    H.init ("Deterministic Fourier law", argc, argv, "n=500,m=1000,t=100");
+    int n=H['n']; vector<double> profile(n,0), boltzmann;
+    {
+        ProgressBar PB (H['m']);
+        for (int i=0; i<int(H['m']); ++i) {
+            PB.set(i);
+            Model M(n); M[n/2].v = 1;
+            for (int t=0; t<int(H['t']); ++t) M.swipe();
+            for (int k=0; k<n; ++k) profile[k] += M[k].v*M[k].v;
+            boltzmann.push_back(M[n/2].v);
+        }
     }
 
-    for (auto u : data) cout << u << endl;
+    { ofstream of("out.profile"); for (auto u : profile) of << u << endl; }
+    {
+        int nclass = sqrt(double(H['m']));
+        double bmin=0, bmax=0; for (auto b : boltzmann) { bmin=min(bmin,b); bmax=max(bmax,b); }
+        vector<int> data(nclass); for (auto b : boltzmann) data[(nclass-.01)*(b-bmin)/(bmax-bmin)]++;
+        ofstream of("out.boltzmann"); for (int i=0; i<nclass; ++i) of << bmin+i*(bmax-bmin)/nclass << " " << data[i] << endl;
+    }
 }
