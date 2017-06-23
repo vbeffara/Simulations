01ec2e8a95ced2474616d5dde9ddf1aa2b58e01e
Vincent Beffara <vbeffara@gmail.com>
2017-06-23 18:54:26 +0200

    Almost total rewrite of Perco.cpp

--- 2D/Perco.cpp
+++ 2D/Perco.cpp
@@ -1,48 +1,40 @@
 #include <vb/Image.h>
 
+using namespace vb; using namespace std;
 
-using namespace vb;
+const Color IN {128,128,128};
 
-int main(int argc, char ** argv) {
-  H.init ("Percolation", argc, argv, "n=500,p=.5,d,s=0,t=128");
-  int    n = H['n']; n -= (n%4);
-  double p = H['p'];
-  bool   d = H['d'];
-  int    s = H['s'];
-  int tint = H['t'];
+class Perco : public Image { public:
+	Perco (int n, double p) : Image(n,n), mid(n/2,n/2), p(p) {
+		for (int x=0; x<n; ++x) {
+			for (int y=0; y<n; ++y) {
+				if ((x%2 == 0) && (y%2 == 0)) put (coo(x,y), IN); else
+                if ((x%2 == 0) || (y%2 == 0)) put (coo(x,y), prng.bernoulli(p) ? IN : BLACK);
+			}
+		}
+        fill (mid,RED); show();
+	}
 
-  Image img (n,n);
+	void run () {
+        int s = H['s'], t = 0; bool c = H['c'];
 
-  for (int x=0; x<n; ++x) {
-    for (int y=0; y<n; ++y) {
-      if      ((x%2 == 0) && (y%2 == 0)) img.put (coo(x,y), tint);
-      else if ((x%2 == 0) || (y%2 == 0)) img.put (coo(x,y), prng.bernoulli(p) * tint);
-    }
-  }
+        while (true) {
+            if ((s > 0) && ((t++ % s) == 0)) snapshot ();
+            auto z = rand (); if ((z.x+z.y) % 2 == 0) continue;
+            fill (mid, IN);
+    		if (c) swap (at(z), atp(z + dz[4+prng.uniform_int(4)]));
+    		else at(z) = prng.bernoulli (p) ? IN : BLACK;
+            fill (mid, RED);
+            step ();
+		}
+	}
 
-  img.fill (coo(n/2,n/2), Color(255,0,0));
-  img.show();
+    coo mid;
+    double p;
+};
 
-  if (d) {
-    int t=0;
-    while (true) {
-      if ((s>0) && ((t++%s)==0)) img.snapshot();
-      int x = prng.uniform_int(n);
-      int y = prng.uniform_int(n);
-      if ((x+y)%2 == 0) continue;
-      bool on_it = (img.at(coo(x,y)) == Color(255,0,0));
-
-      Color c = prng.bernoulli(p) * tint;
-      if (img.at(coo(x,y)) == c) continue;
-      if (on_it && (c == Color(tint))) continue;
-
-      img.fill (coo(n/2,n/2), tint);
-      img.at (coo(x,y)) = c;
-      img.fill (coo(n/2,n/2), Color(255,0,0));
-
-      img.step();
-    }
-  } else img.pause();
-
-  return 0;
+int main (int argc, char ** argv) {
+	H.init ("Percolation", argc, argv, "n=500,p=.5,c,d,s=0");
+	Perco P (H['n'], H['p']);
+    if (H['d']) P.run(); else P.pause();
 }
