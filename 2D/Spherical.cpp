bf75f95ebf8c37f7bcd6789e9ee7c510ef636306
Vincent Beffara <vbeffara@gmail.com>
2017-08-15 16:27:51 +0200

    Compute spherical harmonics without boost, works with higher degree

--- 2D/Spherical.cpp
+++ 2D/Spherical.cpp
@@ -3,9 +3,25 @@
 
 using namespace std; using namespace vb;
 
-#include <boost/math/special_functions/factorials.hpp>
+template <typename T> T check (T x, T y) {
+    static double merr = -1.0;
+    double err = abs(x-y); if (err>merr) {
+        merr = err;
+        H.L->info ("max error: {}", merr);
+    }
+    return x;
+}
 
 class Field : public Coloring { public:
+    Field (int n) : Coloring ({-1.0,-1.0},{1.0,1.0},800,[this](cpx z){return c(z);}), n(n){
+        for (int m=0; m<=n; ++m) {
+            a.emplace_back (prng.gaussian(),prng.gaussian());
+            double dfpm = sqrt((2*n+1) / (4*M_PI));
+            for (int i=1; i<=m; ++i) dfpm *= double(2*i-1) / sqrt(double(2*i-1+n-m)*double(2*i+n-m));
+            dfp.push_back (dfpm);
+        }
+    };
+
     double legendre_next (int l,        double x, double Pl, double Plm1) { return ((2*l+1)*x*Pl - l*Plm1) / (l+1); }
     double legendre_next (int l, int m, double x, double Pl, double Plm1) { return ((2*l+1)*x*Pl - (l+m)*Plm1) / (l+1-m); }
 
@@ -19,21 +35,15 @@ class Field : public Coloring { public:
     double legendre_p (int l, int m, double x) { // l>=0, 0<=m<=l, -1<=x<=1
         if (m==0) return legendre_p (l, x);
 
-        double p0 = df[m] * pow(1-x*x, m/2.0); if (m&1) p0 *= -1; if (m == l) return p0;
+        double p0 = pow(1-x*x, m/2.0); if (m&1) p0 *= -1; if (m == l) return p0;
         double p1 = x * (2*m+1) * p0;
         for (int n=m+1; n<l; ++n) { std::swap(p0, p1); p1 = legendre_next(n, m, x, p0, p1); }
         return p1;
     }
 
     cpx spherical_harmonic (int n, int m, double theta, double phi) { // 0<=m<=n, 0<=theta<=pi, 0<=phi<=2pi
-        return cpx { cos(m*phi), sin(m*phi) } * p[m] * legendre_p (n, m, cos(theta));
+        return cpx { cos(m*phi), sin(m*phi) } * dfp[m] * legendre_p (n, m, cos(theta));
     }
-
-    Field (int n) : Coloring ({-1.0,-1.0},{1.0,1.0},800,[this](cpx z){return c(z);}), n(n){
-        for (int m=0; m<=n; ++m)  a.emplace_back (prng.gaussian(),prng.gaussian());
-        for (int m=0; m<=n; ++m)  p.push_back (sqrt(boost::math::tgamma_delta_ratio(double(n-m+1),double(2*m)) * (2*n+1) / (4*M_PI)));
-        for (int m=0; m<=n; ++m) df.push_back (m==0 ? 1 : boost::math::double_factorial<double>(2*m-1));
-    };
 
     double v (double theta, double phi) {
         double harm = 0;
@@ -53,7 +63,7 @@ class Field : public Coloring { public:
 
     int n;
     vector<cpx> a;
-    vector<double> p, df;
+    vector<double> dfp;
 };
 
 int main (int argc, char ** argv) {
