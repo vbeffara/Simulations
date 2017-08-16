896bbff6b93eca6f65784834bd835b4ea437c7b0
Vincent Beffara <vbeffara@gmail.com>
2017-08-16 11:45:22 +0200

    Nicer lighting of the sphere

--- 2D/Spherical.cpp
+++ 2D/Spherical.cpp
@@ -63,7 +63,7 @@ class Field : public Coloring { public:
             cpx z = c_to_z(coo(i,j)); Color & c = at(coo(i,j));
             double f=1, x1=real(z), x2=imag(z), z2=x1*x1+x2*x2;
             if (z2<=1) {
-                double x3=sqrt(1-z2), y1=.3, y2=.2, y3=sqrt(1-y1*y1-y2*y2);
+                double x3=sqrt(1-z2), y1=.3, y2=-.2, y3=sqrt(1-y1*y1-y2*y2);
                 double xy = x1*y1 + x2*y2 + x3*y3; f = xy>0 ? xy : 0; f = .1 + .9*f*f;
             }
             f = f*c.a/255; c.r = f*c.r + 255-c.a; c.g = f*c.g + 255-c.a; c.b = f*c.b + 255-c.a; c.a=255; step();
