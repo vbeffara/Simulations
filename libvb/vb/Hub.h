ca28e8c037809abd27bfe8dab63d2f1ff33bb00b
Vincent Beffara <vbeffara@gmail.com>
2017-10-19 14:20:26 +0200

    Some build system upgrades

--- libvb/vb/Hub.h
+++ libvb/vb/Hub.h
@@ -1,9 +1,10 @@
 #pragma once /// \file
 #undef False
+#define SPDLOG_FMT_EXTERNAL
 #include <boost/chrono.hpp>
 #include <cstdlib>
 #include <map>
-#include <spdlog/fmt/ostr.h>
+#include <fmt/ostream.h>
 #include <spdlog/spdlog.h>
 #include <string>
 
