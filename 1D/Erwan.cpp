3d8bbe2f453bcdbd415d506fcf245f78bafa7deb
Vincent Beffara <vbeffara@gmail.com>
2017-11-13 23:57:29 +0100

    Look for small values

--- 1D/Erwan.cpp
+++ 1D/Erwan.cpp
@@ -1,41 +1,91 @@
 #include <vb/LinearAlgebra.h>
 #include <vb/PRNG.h>
 
-using namespace vb; using namespace std;
+using namespace vb; using namespace std; using Eigen::Matrix2d;
 
-vector<Matrix<double>> rho,Gamma;
+double lambda (const Matrix2d & A) { return abs (A(0,0) + A(1,1)); }
 
-double lambda (const Matrix<double> & A) {
-    return abs (A(0,0) + A(1,1));
+vector<double> random_p (int k) {
+    vector<double> out; double s = 0.0;
+    for (int i=0; i<k; ++i) { double ss = prng.uniform_real(); s += ss; out.push_back(ss); }
+    for (auto & v : out) v /= s;
+    return out;
 }
 
-double n2 (int n) {
-    Matrix<double> A (2,2), B (2,2); A(0,0)=1; A(0,1)=0; A(1,0)=0; A(1,1)=1; B=A;
-    for (int i=0; i<n; ++i) {
-        auto d = prng.uniform_int(4);
-        A *= rho[d];
-        B *= Gamma[d];
+vector<vector<double>> random_markov (int k) {
+    vector<vector<double>> p;
+    for (int i=0; i<k; ++i) p.push_back (random_p(k));
+    return p;
+}
+
+class Erwan { public:
+    Erwan() {
+        Matrix2d A;
+
+        A << 1, +sqrt(3), 0, 1; rho.push_back(A);
+        A << 1, -sqrt(3), 0, 1; rho.push_back(A);
+        A << 1, 0, +sqrt(3), 1; rho.push_back(A);
+        A << 1, 0, -sqrt(3), 1; rho.push_back(A);
+
+        A << 1, +2, 0, 1; Gamma.push_back(A);
+        A << 1, -2, 0, 1; Gamma.push_back(A);
+        A << 1, 0, +2, 1; Gamma.push_back(A);
+        A << 1, 0, -2, 1; Gamma.push_back(A);
     }
-    return log(lambda(A)) / log(lambda(B));
-}
+
+    double markov (int n, vector<vector<double>> p) {
+        Matrix2d A, B; A << 1, 0, 0, 1; B=A;
+        int d=0;
+        for (int i=0; i<n; ++i) {
+            d = prng.discrete(p[d]);
+            A *= rho[d];
+            B *= Gamma[d];
+        }
+        return log(lambda(A)) / log(lambda(B));
+    }
+
+    double n2 (int n) {
+        return markov (n, {{0.25,0.25,0.25,0.25},{0.25,0.25,0.25,0.25},{0.25,0.25,0.25,0.25},{0.25,0.25,0.25,0.25}});
+    }
+
+    double avg (int n, int t, const vector<vector<double>> & p) {
+        double s = 0.0;
+        for (int i=0; i<t; ++i) s += markov(n,p);
+        return s/t;
+    }
+
+    vector<vector<double>> explore (int n, int t, int m) {
+        double rec = 1.0; vector<vector<double>> best;
+        vector<vector<double>> p = random_markov(4);
+        for (int i=0; i<m; ++i) {
+            p = random_markov(4);
+            double tmp = avg (n,t,p);
+            if (tmp < rec) {
+                H.L->info ("  Current best value: {}", tmp);
+                rec = tmp; best = p;
+            }
+        }
+        return best;
+    }
+
+    void run (int n, int t) {
+        auto p = explore (n,t/100,H['m']);
+
+        for (int i=0; i<4; ++i) {
+            H.L->info ("Transition matrix: {} {} {} {}", p[i][0], p[i][1], p[i][2], p[i][3]);
+        }
+
+        vector<double> n2s;
+        for (int i=0; i<int(t); ++i) n2s.push_back(markov(n,p));
+        sort(begin(n2s),end(n2s));
+        for (auto v : n2s) cout << v << endl;
+        double s=0.0; for (auto v : n2s) s += v; H.L->info ("Average value: {}", s/int(H['t']));
+    }
+
+    vector<Matrix2d> rho,Gamma;
+};
 
 int main (int argc, char ** argv) {
-    H.init ("Erwan's product of random matrices", argc,argv, "n=100,t=100");
-
-    Matrix<double> A (2,2);
-
-    A(0,0)=1; A(0,1)=sqrt(3.0); A(1,0)=0; A(1,1)=1; rho.push_back(A);
-    A(0,0)=1; A(0,1)=-sqrt(3.0); A(1,0)=0; A(1,1)=1; rho.push_back(A);
-    A(0,0)=1; A(0,1)=0; A(1,0)=sqrt(3.0); A(1,1)=1; rho.push_back(A);
-    A(0,0)=1; A(0,1)=0; A(1,0)=-sqrt(3.0); A(1,1)=1; rho.push_back(A);
-
-    A(0,0)=1; A(0,1)=2; A(1,0)=0; A(1,1)=1; Gamma.push_back(A);
-    A(0,0)=1; A(0,1)=-2; A(1,0)=0; A(1,1)=1; Gamma.push_back(A);
-    A(0,0)=1; A(0,1)=0; A(1,0)=2; A(1,1)=1; Gamma.push_back(A);
-    A(0,0)=1; A(0,1)=0; A(1,0)=-2; A(1,1)=1; Gamma.push_back(A);
-
-    vector<double> n2s;
-    for (int i=0; i<int(H['t']); ++i) n2s.push_back(n2(H['n']));
-    sort(begin(n2s),end(n2s));
-    for (auto v : n2s) cout << v << endl;
+    H.init ("Erwan's product of random matrices", argc,argv, "n=100,t=100,m=1000");
+    Erwan().run(H['n'],H['t']);
 }
