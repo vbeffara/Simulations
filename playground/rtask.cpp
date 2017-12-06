e2af08d7252fe67fdf434efa8c52ce0a1d25ecae
Vincent Beffara <vbeffara@gmail.com>
2017-12-06 21:34:27 +0100

    Dependency management works

--- playground/rtask.cpp
+++ playground/rtask.cpp
@@ -1,12 +1,25 @@
 #include <functional>
 #include <iostream>
+#include <optional>
 #include <vector>
 
 using namespace std;
 
-struct job;
-
 struct project {
+    using job = std::function<project(void)>;
+    project() = default;
+    template <typename T, typename... Ts> project(T t, Ts... ts) : project(ts...) { add(t); }
+
+    template <typename T> void add(T t) {
+        project p;
+        p.j   = make_unique<job>(std::move(t));
+        p.par = this;
+        deps.push_back(std::move(p));
+        ++ndep;
+    }
+
+    template <typename T> void then(T t) { j = make_unique<job>(t); }
+
     std::vector<project> deps;
     std::unique_ptr<job> j;
     project *            par  = nullptr;
@@ -15,22 +28,14 @@ struct project {
 
 vector<project *> fringe;
 
-struct job : public function<project()> {
-    template <typename T> job(T t) : function<project()>(t) {}
-};
-
-project single(job j) { return project{{}, make_unique<job>(std::move(j)), nullptr, 0}; }
-
 project fakefib(int n, int d) {
     project p;
-    if (n < 0) return p;
-    p.deps.push_back(single([n, d]() { return fakefib(n - 2, d + 2); }));
-    p.deps.push_back(single([n, d]() { return fakefib(n - 1, d + 2); }));
-    p.ndep = 2;
-    p.j    = make_unique<job>([n, d]() {
+    if (n > 0) p.add(std::bind(fakefib, n - 1, d + 2));
+    if (n > 1) p.add(std::bind(fakefib, n - 2, d + 2));
+    p.then([n, d]() -> project {
         for (int i = 0; i < d; ++i) std::cout << ' ';
         std::cout << n << endl;
-        return project{};
+        return {};
     });
     return p;
 }
@@ -58,7 +63,8 @@ void step() {
 }
 
 int main() {
-    project p = single([]() { return fakefib(4, 0); });
+    project p;
+    p.then([]() { return fakefib(5, 0); });
     fringe.push_back(&p);
     while (fringe.size() > 0) { step(); }
 }
