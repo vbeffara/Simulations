886521831be57b2fd0228afefe73c5f47e938feb
Vincent Beffara <vbeffara@gmail.com>
2017-12-06 23:23:23 +0100

    Fold expressions look better

--- playground/rtask.cpp
+++ playground/rtask.cpp
@@ -6,32 +6,30 @@
 using namespace std;
 
 struct project {
-    using job = std::function<project(void)>;
     project() = default;
-    template <typename T, typename... Ts> project(T t, Ts... ts) : project(ts...) { add(t); }
+    template <typename... Ts> explicit project(Ts... ts) { (add(ts), ...); }
 
     template <typename T> void add(T t) {
-        project p;
-        p.j   = make_unique<job>(std::move(t));
-        p.par = this;
-        deps.push_back(std::move(p));
         ++ndep;
+        deps.emplace_back();
+        deps.back().j   = std::move(t);
+        deps.back().par = this;
     }
 
-    template <typename T> void then(T t) { j = make_unique<job>(t); }
+    template <typename T> void then(T t) { j = std::move(t); }
 
-    std::vector<project> deps;
-    std::unique_ptr<job> j;
-    project *            par  = nullptr;
-    int                  ndep = 0;
+    vector<project>                   deps;
+    optional<function<project(void)>> j;
+    project *                         par  = nullptr;
+    int                               ndep = 0;
 };
 
 vector<project *> fringe;
 
 project fakefib(int n, int d) {
     project p;
-    if (n > 0) p.add(std::bind(fakefib, n - 1, d + 2));
-    if (n > 1) p.add(std::bind(fakefib, n - 2, d + 2));
+    if (n > 0) p.add([=] { return fakefib(n - 1, d + 2); });
+    if (n > 1) p.add([=] { return fakefib(n - 2, d + 2); });
     p.then([n, d]() -> project {
         for (int i = 0; i < d; ++i) std::cout << ' ';
         std::cout << n << endl;
@@ -44,14 +42,16 @@ void step() {
     project * p = fringe.back();
     fringe.pop_back();
 
-    project * par = p->par;
-    *p            = (*(p->j))();
-    p->par        = par;
-    for (auto & pp : p->deps) pp.par = p;
+    auto par = p->par;
+    *p       = (*(p->j))();
+    p->par   = par;
 
-    if (p->deps.size() > 0) {
-        for (auto & t : p->deps) fringe.push_back(&t);
-    } else if (p->j != nullptr) {
+    for (auto & t : p->deps) {
+        t.par = p;
+        fringe.push_back(&t);
+    }
+    if (!(p->deps.empty())) return;
+    if (p->j) {
         fringe.push_back(p);
     } else if (par != nullptr) {
         --(par->ndep);
@@ -66,5 +66,5 @@ int main() {
     project p;
     p.then([]() { return fakefib(5, 0); });
     fringe.push_back(&p);
-    while (fringe.size() > 0) { step(); }
+    while (!fringe.empty()) step();
 }
