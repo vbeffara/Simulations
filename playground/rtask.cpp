c492e1a25632ba4dec87bed73eb633556edd811e
Vincent Beffara <vbeffara@gmail.com>
2017-12-07 18:42:54 +0100

    Now we are getting somewhere

--- playground/rtask.cpp
+++ playground/rtask.cpp
@@ -3,32 +3,56 @@
 #include <optional>
 #include <vector>
 
-using namespace std;
-
 struct project {
-    project() = default;
-    template <typename... Ts> explicit project(Ts... ts) { (add(ts), ...); }
+    template <typename... Ts> project(Ts... ts) { (add(ts), ...); } // NOLINT
 
     template <typename T> project & add(T t) {
         ++ndep;
         deps.emplace_back();
-        deps.back().j   = std::move(t);
-        deps.back().par = this;
+        deps.back().next = std::move(t);
+        deps.back().par  = this;
         return *this;
     }
 
     template <typename T> project & then(T t) {
-        j = std::move(t);
+        next = std::move(t);
         return *this;
     }
 
-    vector<project>                   deps;
-    optional<function<project(void)>> j;
-    project *                         par  = nullptr;
-    int                               ndep = 0;
+    std::vector<project>                    deps;
+    std::optional<std::function<project()>> next;
+    project *                               par  = nullptr;
+    int                                     ndep = 0;
 };
 
-vector<project *> fringe;
+template <typename F> void run(F f) {
+    auto                   p      = project{}.then(std::move(f));
+    std::vector<project *> fringe = {&p};
+
+    while (!fringe.empty()) {
+        project * p = fringe.back();
+        fringe.pop_back();
+
+        auto par = p->par;
+        *p       = (*(p->next))();
+        p->par   = par;
+
+        for (auto & t : p->deps) {
+            t.par = p;
+            fringe.push_back(&t);
+        }
+        if (!(p->deps.empty())) continue;
+        if (p->next)
+            fringe.push_back(p);
+        else if (par != nullptr) {
+            --(par->ndep);
+            if (par->ndep == 0) {
+                par->deps.clear();
+                fringe.push_back(par);
+            }
+        }
+    }
+}
 
 project fakefib(int n, int d) {
     project p;
@@ -36,38 +60,12 @@ project fakefib(int n, int d) {
     if (n > 1) p.add([=] { return fakefib(n - 2, d + 2); });
     p.then([n, d]() -> project {
         for (int i = 0; i < d; ++i) std::cout << ' ';
-        std::cout << n << endl;
+        std::cout << n << '\n';
         return {};
     });
     return p;
 }
 
-void step() {
-    project * p = fringe.back();
-    fringe.pop_back();
-
-    auto par = p->par;
-    *p       = (*(p->j))();
-    p->par   = par;
-
-    for (auto & t : p->deps) {
-        t.par = p;
-        fringe.push_back(&t);
-    }
-    if (!(p->deps.empty())) return;
-    if (p->j) {
-        fringe.push_back(p);
-    } else if (par != nullptr) {
-        --(par->ndep);
-        if (par->ndep == 0) {
-            par->deps.clear();
-            fringe.push_back(par);
-        }
-    }
-}
-
 int main() {
-    auto p = project{}.then([]() { return fakefib(5, 0); });
-    fringe.push_back(&p);
-    while (!fringe.empty()) step();
+    run([] { return fakefib(5, 0); });
 }
