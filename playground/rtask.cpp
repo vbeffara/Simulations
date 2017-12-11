ac25424d1fc5d1fc2859ad2ed1c0640ad38019c3
Vincent Beffara <vbeffara@gmail.com>
2017-12-11 15:48:15 +0100

    Do only overhead in playground/rtask.cpp

--- playground/rtask.cpp
+++ playground/rtask.cpp
@@ -6,27 +6,12 @@
 using namespace vb;
 using namespace std;
 
-Project go(int n, int * t) {
-    if (n < 2) {
-        *t = 1;
-        return {};
-    }
-    int *   t1 = new int;
-    int *   t2 = new int;
-    Project p{[=] { return go(n - 1, t1); }, [=] { return go(n - 2, t2); }};
-    p.then([=] {
-        *t = (*t1) + (*t2);
-        delete t1;
-        delete t2;
-        return Project{};
-    });
-    return p;
+Project go(int n) {
+    if (n < 2) { return {}; }
+    return Project{[=] { return go(n - 1); }, [=] { return go(n - 2); }}.then([=] { return Project{}; });
 };
 
 int main(int argc, char ** argv) {
     H.init("Playground for ThreadPool", argc, argv, "n=5");
-    int * s = new int;
-    execute_par([=] { return go(H['n'], s); });
-    H.L->info("Fib({}) = {}", int(H['n']), *s);
-    delete s;
+    execute_par([=] { return go(H['n']); });
 }
