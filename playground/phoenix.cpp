b2e29214e666ac6909a98a221223f60d4b970e8f
Vincent Beffara <vbeffara@gmail.com>
2018-02-21 17:35:21 +0100

    Saner configuration for vscode

--- playground/phoenix.cpp
+++ playground/phoenix.cpp
@@ -6,7 +6,7 @@ using namespace boost::phoenix::placeholders;
 
 auto l1 = [] (auto i) { return i; };
 
-int main(int argc, char ** argv) {
+int main(int /*argc*/, char ** /*argv*/) {
     std::cout << val(3)() << std::endl;
     std::cout << (_1)(3) << std::endl;
     std::cout << (l1)(3) << std::endl;
