4e687bd06280d06766a515b1e39709e49729a594
Vincent Beffara <vbeffara@gmail.com>
2018-03-21 09:58:07 +0100

    Remove a few more warnings

--- libvb/CoarseImage.cpp
+++ libvb/CoarseImage.cpp
@@ -6,21 +6,22 @@ namespace vb {
     bool CoarseImage::at(coo z) const {
         z += z0;
         const CoarseCell & d = Bitmap<CoarseCell>::at(coo(z.x / L, z.y / L));
-        if (d.fill == 0) return 0;
-        if (d.fill == LL) return 1;
-        return d.sub[(z.x % L) + L * (z.y % L)] > 0 ? true : false;
+        if (d.fill == 0) return false;
+        if (d.fill == LL) return true;
+        return d.sub[(z.x % L) + L * (z.y % L)];
     }
 
     void CoarseImage::put(coo z, bool c) {
         step();
         z += z0;
-        CoarseCell & d = Bitmap<CoarseCell>::at(coo(z.x / L, z.y / L));
-        if (d.fill == c * LL) return;
-        if (d.fill == (1 - c) * LL) d.sub.resize(LL, 1 - c);
+        int          cc = c ? 1 : 0;
+        CoarseCell & d  = Bitmap<CoarseCell>::at(coo(z.x / L, z.y / L));
+        if (d.fill == cc * LL) return;
+        if (d.fill == (1 - cc) * LL) d.sub.resize(LL, !c);
         int sub_xy = (z.x % L) + L * (z.y % L);
         if (d.sub[sub_xy] != c) {
             d.sub[sub_xy] = c;
-            d.fill += 2 * c - 1;
+            d.fill += 2 * cc - 1;
         }
         if ((d.fill == 0) || (d.fill == LL)) d.sub.shrink_to_fit();
     }
@@ -39,7 +40,7 @@ namespace vb {
 
         row m_row(true_width);
         for (int j = 0; j < true_height; ++j) {
-            for (int i = 0; i < true_width; ++i) m_row[i] = !at(coo(i, j) - z0);
+            for (int i = 0; i < true_width; ++i) m_row[i] = at(coo(i, j) - z0) ? 0 : 1;
             wr.write_row(png::row_traits<row>::get_data(m_row));
         }
 
