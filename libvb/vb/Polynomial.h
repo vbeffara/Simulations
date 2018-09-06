b51d8a89cf13983e4c68ede1189966d3d1f04047
Vincent Beffara <vbeffara@gmail.com>
2018-09-06 23:09:50 +0200

    Small tweak to make it build with g++

--- libvb/vb/Polynomial.h
+++ libvb/vb/Polynomial.h
@@ -7,7 +7,7 @@
 namespace vb {
     template <typename T> using Polynomial = boost::math::tools::polynomial<T>;
 
-    template <typename T> void add_root(Polynomial<T> & P, const T & x) { P *= Polynomial<T>{{-x, T{1}}}; }
+    template <typename T> void add_root(Polynomial<T> & P, const T & x) { P *= Polynomial<T>{-x, 1}; }
 
     template <typename T, typename V> V eval(const Polynomial<T> & P, const V & x) {
         if (P.size() == 0) return 0;
