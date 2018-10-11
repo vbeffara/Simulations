24428e6f67e806d8b505f1181fda84c5c07f708e
Vincent Beffara <vbeffara@gmail.com>
2018-10-11 19:40:52 +0200

    Calling of STP less awkward

--- playground/task2.cpp
+++ playground/task2.cpp
@@ -3,59 +3,45 @@
 #include <functional>
 #include <vector>
 
-using namespace std;
-using task = std::function<void()>;
-
-struct will : public task {
-    using task::task;
-    ~will() { (*this)(); }
+struct Will : public std::function<void()> {
+    using std::function<void()>::function;
+    ~Will() { (*this)(); }
 };
 
-boost::lockfree::stack<task> queue;
+struct STP : public boost::lockfree::stack<std::function<void()>> {
+    std::vector<std::thread> runners;
+    bool                     done = false;
+    template <typename F> STP(F &&f) {
+        f(*this, std::make_shared<Will>([=] { done = true; }));
+        for (int i = 0; i < std::max(std::thread::hardware_concurrency(), 1u); ++i)
+            runners.emplace_back([=] {
+                while (!done)
+                    if (value_type op; pop(op)) op();
+            });
+        for (auto &th : runners) th.join();
+    }
+};
 
-int fibo(int n) { return n < 2 ? n : fibo(n - 1) + fibo(n - 2); }
+constexpr int fib(int n) { return n < 2 ? n : fib(n - 1) + fib(n - 2); }
 
-void fib1(shared_ptr<will> parent, int n, const shared_ptr<int> & t) {
+void fib(STP &S, std::shared_ptr<Will> parent, int n, const std::shared_ptr<int> &t) {
     if (n < 25) {
-        *t = fibo(n);
+        *t = fib(n);
         return;
     }
-    auto t1 = make_shared<int>(0), t2 = make_shared<int>(0);
-    auto post = make_shared<will>([parent, t, t1, t2] { *t = *t1 + *t2; });
-    queue.push([=] { fib1(post, n - 1, t1); });
-    queue.push([=] { fib1(post, n - 2, t2); });
+    auto t1 = std::make_shared<int>(0), t2 = std::make_shared<int>(0);
+    auto post = std::make_shared<Will>([p = std::move(parent), t, t1, t2] { *t = *t1 + *t2; });
+    S.push([=, &S] { fib(S, post, n - 1, t1); });
+    S.push([=, &S] { fib(S, post, n - 2, t2); });
 }
 
-void runner(bool & done) {
-    while (!done)
-        if (task op; queue.pop(op)) op();
-}
-
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     vb::H.init("Trying a new task system", argc, argv, "n=41");
     int n = vb::H['n'];
 
-    vb::timing("Just the tasks", [=] {
-        bool done = false;
-        auto t    = make_shared<int>(0);
-        {
-            auto sentinel = make_shared<will>([&done] { done = true; });
-            queue.push([=] { fib1(sentinel, n, t); });
-        }
-        runner(done);
-        return *t;
-    });
-
-    vb::timing("Thread pool", [=] {
-        bool done = false;
-        auto t    = make_shared<int>(0);
-        {
-            auto sentinel = make_shared<will>([&done] { done = true; });
-            queue.push([=] { fib1(sentinel, n, t); });
-        }
-        std::vector<std::thread> runners;
-        for (int i = 0; i < std::max(std::thread::hardware_concurrency(), 1u); ++i) runners.emplace_back([&done] { runner(done); });
-        for (auto & th : runners) th.join();
+    vb::timing("Thread pool", [n] {
+        auto t = std::make_shared<int>(0);
+        STP([n, t](STP &S, std::shared_ptr<Will> post) { fib(S, post, n, t); });
         return *t;
     });
 }
