aedeed60612b8d6481d0fbbade804340eae4a95d
Vincent Beffara <vbeffara@gmail.com>
2018-12-20 22:25:57 +0100

    Self-contained source file

--- tests/bug.cpp
+++ tests/bug.cpp
@@ -1,10 +1,12 @@
-#include <vb/mp.h>
+#include <boost/multiprecision/gmp.hpp>
+#include <boost/multiprecision/mpfr.hpp>
 #include <iostream>
 
-using namespace vb;
+using boost::multiprecision::mpfr_float;
+using boost::multiprecision::mpz_int;
 
 int main() {
-    real_t::default_precision(100);
-    std::cerr << real_t{mpz_int{1} * real_t{1}}.precision() << "\n";
-    std::cerr << real_t{real_t{mpz_int{1}} * real_t{1}}.precision() << "\n";
+    mpfr_float::default_precision(100);
+    std::cerr << mpfr_float{mpz_int{1} * mpfr_float{1}}.precision() << "\n";
+    std::cerr << mpfr_float{mpfr_float{mpz_int{1}} * mpfr_float{1}}.precision() << "\n";
 }
