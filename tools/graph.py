9dabf832441a39d488d36b8c708a98e878c4588e
Vincent Beffara <vbeffara@gmail.com>
2019-02-06 00:28:08 +0100

    Simplify graph.py a lot

--- tools/graph.py
+++ tools/graph.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 
-from glob import glob
-from sys import stderr
+import os
 import re
+import glob
 
 nodes = {}
 clusters = {}
-
-
-def parent(path):
-    m = re.compile("^(?P<p>.*)/[^/]*$").match(path)
-    if m:
-        return m['p']
-    else:
-        return ""
 
 
 def traverse(path):
@@ -23,30 +15,28 @@ def traverse(path):
         print('    "%s" [label=\"%s\", style=filled, fillcolor=red]' %
               (f, nodes[f]["label"]))
     for c in clusters:
-        if parent(c) == path:
+        if os.path.dirname(c) == path:
             traverse(c)
     print("  }")
 
 
-for f in glob("include/vb/**/*.h", recursive=True):
-    m = re.compile("include/(?P<path>.*)/(?P<basename>[^/]*).h").match(f)
-    node = {"label": m['basename']+".h", "file": f, "links": [],
-            "name": m['path'] + "/" + m['basename'] + ".h"}
-    if not m['path'] in clusters:
-        clusters[m['path']] = []
-    clusters[m['path']] += [node['name']]
+for f in glob.glob("include/vb/**/*.h", recursive=True):
+    name, (path, filename) = f[8:], os.path.split(f[8:])
+    if not path in clusters:
+        clusters[path] = []
+    clusters[path] += [name]
 
+    node = {"label": filename, "links": []}
     for l in open(f, encoding="utf-8"):
-        mm = re.compile("\\#include \\<(?P<link>.*)\\>$").match(l)
+        mm = re.compile('#include \<(.*)\>$').match(l)
         if mm:
-            node["links"] += [mm['link']]
-
-    nodes[node['name']] = node
+            node["links"] += [mm[1]]
+    nodes[name] = node
 
 print("digraph {")
 print("  rankdir = LR")
 traverse("vb")
 for k, v in nodes.items():
     for l in v["links"]:
-        print('    "%s" -> "%s"' % (v["name"], l))
+        print('    "%s" -> "%s"' % (k, l))
 print("}")
