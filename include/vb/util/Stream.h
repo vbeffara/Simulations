aea57ae155a93e226c89bdd4edeec757edfed2bf
Vincent Beffara <vbeffara@gmail.com>
2021-08-10 17:20:25 +0200

    Remove Stream::filter and Stream::fmap

--- include/vb/util/Stream.h
+++ include/vb/util/Stream.h
@@ -8,13 +8,4 @@ namespace vb {
     Stream<std::vector<size_t>> partitions(size_t n, size_t m = 1); // Partitions of n with piece size at least m
     Stream<std::vector<size_t>> tuples(size_t k, size_t n);         // Ordered, distinct k-tuples in [0,n-1]
     Stream<std::vector<size_t>> cycles(size_t k, size_t n);         // k-tuples up to cyclic permutation
-
-    template <typename F, typename T> auto filter(F f, Stream<T> &&S) -> Stream<T> {
-        for (const auto &x : S)
-            if (f(x)) co_yield(x);
-    }
-
-    template <typename F, typename T> auto fmap(F f, Stream<T> &&S) -> Stream<decltype(f(T()))> {
-        for (const auto &x : S) co_yield(f(x));
-    }
 } // namespace vb
--- tests/test_coroutine.cpp
+++ tests/test_coroutine.cpp
@@ -11,8 +11,8 @@ auto sec(const Permutation &p) { return p[2]; }
 auto main(int argc, char **argv) -> int {
     Hub  H("Testing coroutines", argc, argv, "n=5");
     auto S    = permutations(H['n']);
-    auto SS   = filter(good, move(S));
-    auto SSS  = fmap(sec, move(SS));
+    auto SS   = S | ranges::views::filter(good);
+    auto SSS  = SS | ranges::views::transform(sec);
     auto SSSS = SSS | ranges::views::take(8);
     for (const auto &p : SSSS) spdlog::info("{}", p);
 }
