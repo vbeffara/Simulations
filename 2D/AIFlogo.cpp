b372b78a2ac74c7639c4866a178e12ac19140804
Vincent Beffara <vbeffara@gmail.com>
2021-11-10 22:41:51 +0100

    Store theta instead of eitheta

--- 2D/AIFlogo.cpp
+++ 2D/AIFlogo.cpp
@@ -7,38 +7,32 @@ using namespace vb;
 
 struct mobius {
     double a;
-    cpx    eitheta;
+    double theta;
 
-    mobius(double _a, double _theta) : mobius(_a, exp(I * _theta)) {}
-    mobius(double _a, cpx _eitheta) : a(_a), eitheta(_eitheta) {}
+    mobius(double _a, double _theta) : a(_a), theta(_theta) {}
     mobius(cpx zm, cpx zp) {
         if (abs(zp / zm + 1.0) < .001) {
-            a       = 0;
-            eitheta = sqrt(zp * zm);
+            a     = 0;
+            theta = arg(sqrt(zp * zm));
         } else {
-            cpx w   = sqrt(zp / zm);
-            a       = real((w - I) / (1.0 - w * I));
-            eitheta = zm * w;
+            cpx w = sqrt(zp / zm);
+            a     = real((w - I) / (1.0 - w * I));
+            theta = arg(zm * w);
         }
     }
 
-    cpx operator()(cpx z) { return eitheta * (a + z) / (1.0 + a * z); }
+    cpx    operator()(cpx z) { return exp(I * theta) * (a + z) / (1.0 + a * z); }
+    mobius operator()(mobius m) { return {(*this)(m(-I)), (*this)(m(I))}; }
 
-    mobius operator()(mobius m) {
-        cpx zm = m(-I), zp = m(I);
-        cpx fzm = (*this)(zm), fzp = (*this)(zp);
-        return {fzm, fzp};
-    }
-
-    std::string repr() const { return fmt::format("[a={}, eitheta={}]", a, eitheta); }
+    std::string repr() const { return fmt::format("[a={}, theta={}]", a, theta * 180 / M_PI); }
 
     std::string draw() {
         std::string r = repr();
         if (a > .001)
-            return fmt::format("{} circle ({}) % {}", eitheta * (1 + a * a) / (2 * a), (1 - a * a) / (2 * a), r);
+            return fmt::format("{} circle ({}) % {}", exp(I * theta) * (1 + a * a) / (2 * a), (1 - a * a) / (2 * a), r);
         else {
-            double theta = arg(eitheta) * 180 / M_PI;
-            return fmt::format("[rotate={}] (0,-1) rectangle (1,2) [rotate={}]", theta, -theta);
+            double th = theta * 180 / M_PI;
+            return fmt::format("[rotate={}] (0,-1) rectangle (1,2) [rotate={}] % {}", th, -th, r);
         }
     }
 };
