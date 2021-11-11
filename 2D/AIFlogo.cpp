b7f9f90819b8ce7629464de5571690bc20196e3e
Vincent Beffara <vbeffara@gmail.com>
2021-11-11 23:06:59 +0100

    Clean up AIFlogo

--- 2D/AIFlogo.cpp
+++ 2D/AIFlogo.cpp
@@ -9,62 +9,50 @@ struct mobius {
     cpx    a;
     double theta;
 
-    mobius(cpx _a, double _theta) : a(_a), theta(_theta) {}
-    mobius(cpx zm, cpx zp) {
-        if (abs(zp / zm + 1.0) < .001) {
-            a     = 0;
-            theta = arg(sqrt(zp * zm));
-        } else {
-            cpx w = sqrt(zp / zm);
-            a     = real((w - I) / (1.0 - w * I));
-            theta = arg(zm * w);
-        }
+    static auto from_endpoints(cpx zm, cpx zp) -> mobius {
+        if (abs(zp / zm + 1.0) < .001) return {0, arg(sqrt(zp * zm))};
+        cpx w = sqrt(zp / zm);
+        return {real((w - I) / (1.0 - w * I)), arg(zm * w)};
     }
 
-    cpx    operator()(cpx z) const { return exp(I * theta) * (a + z) / (1.0 + conj(a) * z); }
-    mobius operator()(mobius m) const { return {(*this)(m(-I)), (*this)(m(I))}; }
-    mobius inverse() const { return {-a * exp(I * theta), -theta}; }
-    cpx    sym(cpx z) const { return (*this)(-conj(inverse()(z))); }
-    mobius sym(mobius m) const { return {sym(m(I)), sym(m(-I))}; }
+    [[nodiscard]] auto operator()(cpx z) const -> cpx { return exp(I * theta) * (a + z) / (1.0 + conj(a) * z); }
+    [[nodiscard]] auto inverse() const -> mobius { return {-a * exp(I * theta), -theta}; }
+    [[nodiscard]] auto sym(cpx z) const -> cpx { return (*this)(-conj(inverse()(z))); }
+    [[nodiscard]] auto sym(mobius m) const -> mobius { return from_endpoints(sym(m(I)), sym(m(-I))); }
 
-    bool like(const mobius &m) const {
+    [[nodiscard]] auto like(const mobius &m) const -> bool {
         if ((abs(a) < .001) && (abs(m.a) < .001) && (abs(abs(theta - m.theta) - M_PI) < .001)) return true;
         if (abs(a - m.a) > .001) return false;
         if (abs(theta - m.theta) < .001) return true;
         return (abs(abs(theta - m.theta) - 2 * M_PI) < .001);
     }
 
-    std::string repr() const { return fmt::format("[a={}, theta={}]", a, theta * 180 / M_PI); }
+    [[nodiscard]] auto repr() const -> std::string { return fmt::format("[a={}, theta={}]", a, theta * 180 / M_PI); }
 
-    std::string draw() const {
-        std::string r = repr();
+    [[nodiscard]] auto draw() const -> std::string {
         if (abs(a) > .001) {
             cpx    center = exp(I * theta) * (1.0 + a * a) / (a + conj(a));
             double radius = real((1.0 - a * conj(a)) / (a + conj(a)));
-            return fmt::format("{} circle ({}) % {}", center, radius, r);
-        } else {
-            double th = theta * 180 / M_PI;
-            return fmt::format("[rotate={}] (0,-1) rectangle (1,2) [rotate={}] % {}", th, -th, r);
+            return fmt::format("{} circle ({}) % {}", center, radius, repr());
         }
+        double th = theta * 180 / M_PI;
+        return fmt::format("[rotate={}] (0,-1) rectangle (1,2) [rotate={}] % {}", th, -th, repr());
     }
 };
 
-int main() {
-    mobius              gen1(0, 0), gen2(0, -M_PI / 6), gen3(sqrt(sqrt(5.0) - 2), M_PI / 3);
-    std::vector<mobius> list{gen1, gen2, gen3};
+auto main() -> int {
+    std::vector<mobius> gens{{0, 0}, {0, -M_PI / 6}, {sqrt(sqrt(5.0) - 2), M_PI / 3}}, list{gens};
     bool                done = false;
     while (!done) {
         std::vector<mobius> candidates;
-        for (const auto &m : list) {
-            for (const auto &n : list) { candidates.push_back(m.sym(n)); }
-        }
+        for (const auto &m : gens)
+            for (const auto &n : list) candidates.push_back(m.sym(n));
         done = true;
         for (const auto &m : candidates) {
             if (abs(m.a) > .999) continue;
-            if (std::none_of(begin(list), end(list), [&](const mobius &n) { return m.like(n); })) {
-                list.push_back(m);
-                done = false;
-            }
+            if (std::any_of(begin(list), end(list), [&m](const mobius &n) { return m.like(n); })) continue;
+            list.push_back(m);
+            done = false;
         }
     }
     for (const auto &m : list) std::cout << m.draw() << std::endl;
