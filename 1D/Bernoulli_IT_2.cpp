387e8365008c0f5148f81b2a852bdcdf1582712c
Vincent Beffara <vbeffara@gmail.com>
2023-01-31 23:57:55 +0100

    Clean up

--- 1D/Bernoulli_IT_2.cpp
+++ 1D/Bernoulli_IT_2.cpp
@@ -1,5 +1,6 @@
 #include <boost/icl/interval_set.hpp>
 #include <spdlog/spdlog.h>
+#include <vb/util/Hub.h>
 #include <vb/util/PRNG.h>
 #include <vector>
 
@@ -11,40 +12,48 @@ struct piece {
     double a, b, d;
 };
 
-using function = std::vector<piece>;
-
-function split(piece p, function f) {
-    function g;
-    for (auto q : f) {
-        // The interval [aa,bb] is what is sent into q by x \mapsto x + d
-        double aa = q.a - p.d;
-        double bb = q.b - p.d;
-        // If [aa,bb] intersects p, this gives a piece of the output
-        double aaa = std::max(p.a, aa);
-        double bbb = std::min(p.b, bb);
-        if (aaa < bbb) { g.push_back({aaa, bbb, p.d + q.d}); }
-    };
-    return g;
-}
-
-function compose(function f1, function f2) {
-    function g;
-    for (const auto &p : f1) {
-        auto pf2 = split(p, f2);
-        for (const auto &q : pf2) g.push_back(q);
+class function : public std::vector<piece> {
+public:
+    void add(const piece &p) {
+        if ((!empty()) && (p.a == back().b) && (p.d == back().d))
+            back().b = p.b;
+        else
+            push_back(p);
     }
-    return g;
-}
 
-iset range(const function &f) {
-    iset out;
-    for (const auto &p : f) out.insert(intv::right_open(p.a + p.d, p.b + p.d));
-    return out;
-}
+    void print() const {
+        for (const auto &p : *this) { fmt::print("[{},{}] + {}\n", p.a, p.b, p.d); }
+    }
 
-void plot(const function &f) {
-    for (const auto &p : f) { fmt::print("{} {}\n{} {}\n\n", p.a, p.a + p.d, p.b, p.b + p.d); }
-}
+    function split(const piece &p) {
+        function g;
+        for (auto q : *this) {
+            double a = std::max(p.a, q.a - p.d), b = std::min(p.b, q.b - p.d);
+            if (a < b) g.add({a, b, p.d + q.d});
+        };
+        return g;
+    }
+
+    // Compose inside
+    function compose(function f1) {
+        function g;
+        for (const auto &p : f1) {
+            auto pf2 = split(p);
+            for (const auto &q : pf2) g.add(q);
+        }
+        return g;
+    }
+
+    iset range() const {
+        iset out;
+        for (const auto &p : *this) out.insert(intv::right_open(p.a + p.d, p.b + p.d));
+        return out;
+    }
+
+    void plot() const {
+        for (const auto &p : *this) { fmt::print("{} {}\n{} {}\n\n", p.a, p.a + p.d, p.b, p.b + p.d); }
+    }
+};
 
 double measure(const iset &I) {
     double out = 0;
@@ -72,17 +81,25 @@ double hole(const iset &I) {
     return std::max(hole_of_zero, biggest_hole);
 }
 
-int main() {
+int main(int argc, char **argv) {
+    vb::Hub H("Bernoullicity of [I,T]", argc, argv, "n=1000,i");
+
     double                theta = .61803398874989484820;
     function              f1{{{0.0, theta, 0.0}, {theta, 1.0, -theta}}};
     function              f2{{{0.0, theta, 1 - theta}, {theta, 1.0, 0.0}}};
     std::vector<function> ff{f1, f2};
 
-    function f{{0.0, 1.0, 0.0}};
-    for (int i = 0; i < 2000; ++i) {
-        f      = compose(f, ff[vb::prng.uniform_int(ff.size())]);
-        auto R = range(f);
-        fmt::print("{} {} {}\n", measure(R), 1 - hole(R), upper(R) - lower(R));
+    function f{{{0.0, 1.0, 0.0}}};
+    double   avg = 1;
+    for (int i = 0; i < int(H['n']); ++i) {
+        if (bool(H['i']))
+            f = f.compose(ff[vb::prng.uniform_int(ff.size())]);
+        else
+            f = ff[vb::prng.uniform_int(ff.size())].compose(f);
+        auto R = f.range();
+        avg    = .999 * avg + .001 * (1 - hole(R));
+        fmt::print("{} {} {}\n", measure(R), 1 - hole(R), avg);
     }
     // for (const auto &p : f) fmt::print("{} {}\n", p.a + p.d, p.b + p.d);
+    // f.print();
 }
