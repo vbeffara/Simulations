516a3a2229f49a72efee4c5eeead840598b8535e
Vincent Beffara <vbeffara@gmail.com>
2023-04-16 00:06:50 +0200

    Golf

--- tests/std_range.cpp
+++ tests/std_range.cpp
@@ -4,12 +4,11 @@
 
 // TODO: wait for <generator> header
 
-using namespace std::ranges;
+namespace rv = std::ranges::views;
 
 auto f(int i) -> double { return rand() % 1000 + i / 1000.0; }
 auto good(double i) -> bool { return (int(i) % 100) != 0; }
 
 auto main() -> int {
-    auto goods = views::iota(0) | views::transform(f) | views::take_while(good);
-    for_each(goods, [](double x) { std::cout << x << '\n'; });
+    for (const auto &x : rv::iota(0) | rv::transform(f) | rv::take_while(good)) { std::cout << x << '\n'; };
 }
