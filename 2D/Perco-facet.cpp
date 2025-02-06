8e7412bd504c25262181f7241a34c8ca60455dde
Vincent Beffara <vbeffara@gmail.com>
2025-02-06 22:20:04 +0100

    Proper (but inefficient) cluster exploration

--- 2D/Perco-facet.cpp
+++ 2D/Perco-facet.cpp
@@ -1,77 +1,137 @@
-#include <vb/util/CLP.h>
+#include <map>
+#include <vb/Figure.h>
 #include <vb/data/Array.h>
+#include <vb/util/CLP.h>
 #include <vb/util/PRNG.h>
 #include <vb/util/coo.h>
-#include <vb/Figure.h>
 
 using namespace vb;
 
 struct Site {
-  bool E{false}, N{false}, NW{false};
+  bool E{false}, N{false}, NW{false}, T1{false}, T2{false};
 };
 
-cpx pos (coo z) {
-  return {z.x + double(z.y)/2, z.y*sqrt(3)/2};
-}
+cpx pos(coo z) { return {z.x + double(z.y) / 2, z.y * sqrt(3) / 2}; }
 
-std::unique_ptr<Segment> seg_E (coo z) {
-  return std::make_unique<Segment>(pos(z), pos(z) + cpx(1,0));
-}
+std::unique_ptr<Segment> seg_E(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(1, 0)); }
 
-std::unique_ptr<Segment> seg_N (coo z) {
-  return std::make_unique<Segment>(pos(z), pos(z) + cpx(.5,sqrt(3)/2));
-}
+std::unique_ptr<Segment> seg_N(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(.5, sqrt(3) / 2)); }
 
-std::unique_ptr<Segment> seg_NW (coo z) {
-  return std::make_unique<Segment>(pos(z), pos(z) + cpx(-.5,sqrt(3)/2));
-}
+std::unique_ptr<Segment> seg_NW(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(-.5, sqrt(3) / 2)); }
 
-std::unique_ptr<Path> tri1 (coo z) {
-  Path P({pos(z), pos(z) + cpx(1,0), pos(z) + cpx(.5,sqrt(3)/2), pos(z)}, Pen(BLACK, 1.0, RED, true));
+std::unique_ptr<Path> tri1(coo z, long long c) {
+  Path P({pos(z), pos(z) + cpx(1, 0), pos(z) + cpx(.5, sqrt(3) / 2), pos(z)}, Pen(BLACK, 1.0, Indexed(c), true));
   return std::make_unique<Path>(P);
 }
 
-std::unique_ptr<Path> tri2 (coo z) {
-  Path P({pos(z) + cpx(1,0), pos(z) + cpx(1.5,sqrt(3)/2), pos(z) + cpx(.5,sqrt(3)/2), pos(z) + cpx(1,0)}, Pen(BLACK, 1.0, RED, true));
+std::unique_ptr<Path> tri2(coo z, long long c) {
+  Path P({pos(z) + cpx(1, 0), pos(z) + cpx(1.5, sqrt(3) / 2), pos(z) + cpx(.5, sqrt(3) / 2), pos(z) + cpx(1, 0)},
+         Pen(BLACK, 1.0, Indexed(c), true));
   return std::make_unique<Path>(P);
 }
 
 struct Perco {
-  size_t n;
-  Array <Site> sites;
+  size_t           n;
+  Array<Site>      sites;
+  Array<long long> c1, c2;
 
-  Perco (size_t n, double p) : n(n), sites({n,n}) {
-    for (auto z : coo_range<size_t>({n,n})) {
+  Perco(size_t n, double p) : n(n), sites({n, n}), c1({n, n}, 0), c2({n, n}, 0) {
+    // Open each edge with probability `p`
+    for (auto z : coo_range<size_t>({n, n})) {
       if (prng.bernoulli(p)) sites[z].E = true;
       if (prng.bernoulli(p)) sites[z].N = true;
       if (prng.bernoulli(p)) sites[z].NW = true;
+    }
+    // Open each facet if the corresponding edges are open
+    for (auto z : coo_range<long long>({int(n), int(n)})) {
+      if (sites.atp(z).E && sites.atp(z).N && sites.atp({z.x + 1, z.y}).NW) sites.atp(z).T1 = true;
+      if (sites.atp({z.x + 1, z.y}).NW && sites.atp({z.x + 1, z.y}).N && sites.atp({z.x, z.y + 1}).E) sites.atp(z).T2 = true;
     }
   }
 
   void show() {
     Figure F{"Percolation"};
-    for (auto z : coo_range<long long>({int(n),int(n)})) {
-      cpx pos {z.x + double(z.y)/2, z.y*sqrt(3)/2};
+    for (auto z : coo_range<long long>({int(n), int(n)})) {
+      cpx pos{z.x + double(z.y) / 2, z.y * sqrt(3) / 2};
       F.add(std::make_unique<Circle>(pos, .1));
-      if (sites.atp(z).E && sites.atp(z).N && sites.atp({z.x + 1, z.y}).NW)
-        F.add(tri1(z));
-      if (sites.atp({z.x + 1, z.y}).NW && sites.atp({z.x + 1, z.y}).N && sites.atp({z.x, z.y + 1}).E)
-        F.add(tri2(z));
+      if (sites.atp(z).T1) F.add(tri1(z, c1.atp(z)));
+      if (sites.atp(z).T2) F.add(tri2(z, c2.atp(z)));
       if (sites.atp(z).E) F.add(seg_E(z));
       if (sites.atp(z).N) F.add(seg_N(z));
       if (sites.atp(z).NW) F.add(seg_NW(z));
     }
     F.show();
     F.pause();
+    F.output_pdf("Facet percolation");
+  }
+
+  void explore() {
+    long long idx = 0;
+    for (auto z : coo_range<size_t>({n, n})) {
+      if (sites[z].T1) c1[z] = ++idx;
+      if (sites[z].T2) c2[z] = ++idx;
+    }
+    spdlog::info("{} open facets", idx);
+
+    bool done = false;
+    while (!done) {
+      done = true;
+      for (auto z : coo_range<long long>({int(n), int(n)})) {
+        if (sites.atp(z).T1) {
+          if (c2.atp(z) > c1.atp(z)) {
+            c1.atp(z) = c2.atp(z);
+            done      = false;
+          }
+          if (c2.atp({z.x - 1, z.y}) > c1.atp(z)) {
+            c1.atp(z) = c2.atp({z.x - 1, z.y});
+            done      = false;
+          }
+          if (c2.atp({z.x, z.y - 1}) > c1.atp(z)) {
+            c1.atp(z) = c2.atp({z.x, z.y - 1});
+            done      = false;
+          }
+        }
+        if (sites.atp(z).T2) {
+          if (c1.atp(z) > c2.atp(z)) {
+            c2.atp(z) = c1.atp(z);
+            done      = false;
+          }
+          if (c1.atp({z.x + 1, z.y}) > c2.atp(z)) {
+            c2.atp(z) = c1.atp({z.x + 1, z.y});
+            done      = false;
+          }
+          if (c1.atp({z.x, z.y + 1}) > c2.atp(z)) {
+            c2.atp(z) = c1.atp({z.x, z.y + 1});
+            done      = false;
+          }
+        }
+      }
+    }
+  }
+
+  // Count cluster sizes
+  size_t biggest() {
+    std::map<long long, size_t> count;
+    for (auto z : coo_range<size_t>({n, n})) {
+      if (sites[z].T1) ++count[c1[z]];
+      if (sites[z].T2) ++count[c2[z]];
+    }
+    size_t ans = 0;
+    for (auto [c, n] : count)
+      if (n > ans) { ans = n; };
+    return ans;
   }
 };
 
-int main (int argc, char ** argv) {
-    CLP  clp(argc, argv, "Facet percolation");
-    auto p = clp.param("p", 0.5, "Percolation parameter");
-    auto n = clp.param("n", size_t(50), "Domain size");
-    clp.finalize();
+int main(int argc, char **argv) {
+  CLP  clp(argc, argv, "Facet percolation");
+  auto p = clp.param("p", 0.5, "Percolation parameter");
+  auto n = clp.param("n", size_t(50), "Domain size");
+  auto v = clp.flag("v", "Show the picture");
+  clp.finalize();
 
-  Perco P (n, p);
-  P.show();
+  Perco P(n, p);
+  P.explore();
+  spdlog::info("Largest cluster size: {}", P.biggest());
+  if (v) P.show();
 }
