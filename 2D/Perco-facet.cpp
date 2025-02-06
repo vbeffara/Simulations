2c1aff23fa869e671692222e3227e3fb99f69fe9
Vincent Beffara <vbeffara@gmail.com>
2025-02-06 23:28:03 +0100

    Estimate p_c

--- 2D/Perco-facet.cpp
+++ 2D/Perco-facet.cpp
@@ -1,5 +1,7 @@
+#include <fmt/printf.h>
 #include <map>
 #include <vb/Figure.h>
+#include <vb/ProgressBar.h>
 #include <vb/data/Array.h>
 #include <vb/util/CLP.h>
 #include <vb/util/PRNG.h>
@@ -31,9 +33,10 @@ std::unique_ptr<Path> tri2(coo z, long long c) {
 }
 
 struct Perco {
-  size_t           n;
-  Array<Site>      sites;
-  Array<long long> c1, c2;
+  size_t              n;
+  Array<Site>         sites;
+  Array<long long>    c1, c2;
+  std::vector<size_t> sizes;
 
   Perco(size_t n, double p) : n(n), sites({n, n}), c1({n, n}, 0), c2({n, n}, 0) {
     // Open each edge with probability `p`
@@ -65,13 +68,24 @@ struct Perco {
     F.output_pdf("Facet percolation");
   }
 
+  // Count cluster sizes
+  void count() {
+    std::map<long long, size_t> count;
+    for (auto z : coo_range<size_t>({n, n})) {
+      if (sites[z].T1) ++count[c1[z]];
+      if (sites[z].T2) ++count[c2[z]];
+    }
+
+    for (auto [c, n] : count) sizes.push_back(n);
+    std::sort(sizes.begin(), sizes.end(), std::greater<size_t>());
+  }
+
   void explore() {
     long long idx = 0;
     for (auto z : coo_range<size_t>({n, n})) {
       if (sites[z].T1) c1[z] = ++idx;
       if (sites[z].T2) c2[z] = ++idx;
     }
-    spdlog::info("{} open facets", idx);
 
     bool done = false;
     while (!done) {
@@ -107,31 +121,36 @@ struct Perco {
         }
       }
     }
+
+    count();
   }
 
-  // Count cluster sizes
-  size_t biggest() {
-    std::map<long long, size_t> count;
-    for (auto z : coo_range<size_t>({n, n})) {
-      if (sites[z].T1) ++count[c1[z]];
-      if (sites[z].T2) ++count[c2[z]];
-    }
-    size_t ans = 0;
-    for (auto [c, n] : count)
-      if (n > ans) { ans = n; };
-    return ans;
-  }
+  size_t biggest() { return sizes.size() > 0 ? sizes[0] : 0; }
+
+  size_t second_biggest() { return sizes.size() > 1 ? sizes[1] : 0; }
 };
 
 int main(int argc, char **argv) {
   CLP  clp(argc, argv, "Facet percolation");
-  auto p = clp.param("p", 0.5, "Percolation parameter");
   auto n = clp.param("n", size_t(50), "Domain size");
+  auto p = clp.param("p", 0.5, "Percolation parameter (only used if t=1)");
+  auto t = clp.param("t", size_t(1), "Number of samples (for p_c estimation, ignores p)");
   auto v = clp.flag("v", "Show the picture");
   clp.finalize();
 
-  Perco P(n, p);
-  P.explore();
-  spdlog::info("Largest cluster size: {}", P.biggest());
-  if (v) P.show();
+  if (t == 1) {
+    Perco P(n, p);
+    P.explore();
+    spdlog::info("Largest cluster size: {}", P.biggest());
+    P.show();
+  } else {
+    ProgressBar PB(t);
+    for (size_t i = 0; i < t; ++i) {
+      PB.set(i);
+      double p = prng.uniform_real();
+      Perco  P(n, p);
+      P.explore();
+      fmt::print("{} {} {}\n", p, P.biggest(), P.second_biggest());
+    }
+  }
 }
--- libvb/ProgressBar.cpp
+++ libvb/ProgressBar.cpp
@@ -1,29 +1,31 @@
+#include <fmt/ostream.h>
+#include <iostream>
 #include <spdlog/spdlog.h>
 #include <vb/ProgressBar.h>
 
 namespace vb {
-    ProgressBar::ProgressBar(size_t length, double pow) : Auto(1), final(length), power(pow) {}
+  ProgressBar::ProgressBar(size_t length, double pow) : Auto(1), final(length), power(pow) {}
 
-    void ProgressBar::set(size_t pos) {
-        if (pos > final) pos = final;
-        current = pos;
-        step();
+  void ProgressBar::set(size_t pos) {
+    if (pos > final) pos = final;
+    current = pos;
+    step();
+  }
+
+  void ProgressBar::update() {
+    static const std::vector<std::string> symbols{" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"};
+    std::string                           bar, days, rest;
+
+    auto n = current * 400 / final;
+    for (size_t i = 0; i < n / 8; ++i) bar += symbols[8];
+    if (n < 400) bar += symbols[n % 8] + std::string(size_t(49 - n / 8), ' ');
+
+    if (current > 0) {
+      auto eta = int(Duration((now() - start) * (pow(double(final) / double(current), power) - 1)).count());
+      if (eta >= 3600 * 24) days = fmt::format("{}d ", eta / 3600 / 24);
+      rest = fmt::format("{}:{:02}:{:02}       ", (eta / 3600) % 24, (eta / 60) % 60, eta % 60);
     }
 
-    void ProgressBar::update() {
-        static const std::vector<std::string> symbols{" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"};
-        std::string                           bar, days, rest;
-
-        auto n = current * 400 / final;
-        for (size_t i = 0; i < n / 8; ++i) bar += symbols[8];
-        if (n < 400) bar += symbols[n % 8] + std::string(size_t(49 - n / 8), ' ');
-
-        if (current > 0) {
-            auto eta = int(Duration((now() - start) * (pow(double(final) / double(current), power) - 1)).count());
-            if (eta >= 3600 * 24) days = fmt::format("{}d ", eta / 3600 / 24);
-            rest = fmt::format("{}:{:02}:{:02}       ", (eta / 3600) % 24, (eta / 60) % 60, eta % 60);
-        }
-
-        spdlog::info("[{}] {}{}", bar, days, rest);
-    }
+    fmt::print(std::cerr, "\r[{}] {}{}", bar, days, rest);
+  }
 } // namespace vb
