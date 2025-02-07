31536af039e29ba2870cfd4e933330f89d17c963
Vincent Beffara <vbeffara@gmail.com>
2025-02-07 23:56:41 +0100

    No hardcoded graph structure anymore

--- 2D/Perco-facet.cpp
+++ 2D/Perco-facet.cpp
@@ -17,10 +17,27 @@ struct Pattern {
   std::vector<cpx>                             shifts;
   std::vector<std::tuple<size_t, size_t, coo>> adj_faces;
   std::vector<std::tuple<size_t, size_t, coo>> face_edges;
+  std::vector<std::tuple<size_t, size_t, coo>> face_sites;
+  std::vector<std::tuple<size_t, size_t, coo>> edge_sites;
 
   Pattern(size_t s, size_t b, size_t f) : n_sites(s), n_bonds(b), n_faces(f) {}
 
   cpx pos(coo z) { return {z.x + period.real() * z.y, period.imag() * z.y}; }
+
+  std::unique_ptr<Segment> draw_edge(coo z, size_t k) {
+    std::vector<cpx> pts;
+    for (auto [i, j, dz] : edge_sites)
+      if (i == k) pts.push_back(pos(z + dz) + shifts[j]);
+    return std::make_unique<Segment>(pts[0], pts[1], Pen(BLACK, 1.5));
+  }
+
+  std::unique_ptr<Path> draw_face(coo z, long long c, size_t k) {
+    Path ans({}, Pen(BLACK, 3.0, Indexed(c), true));
+    for (auto [i, j, dz] : face_sites)
+      if (i == k) ans.z.push_back(pos(z + dz) + shifts[j]);
+    ans.z.push_back(ans.z[0]);
+    return std::make_unique<Path>(ans);
+  }
 };
 
 Pattern Triangular() {
@@ -29,6 +46,8 @@ Pattern Triangular() {
   P.shifts     = {0};
   P.adj_faces  = {{0, 1, {0, 0}}, {0, 1, {0, -1}}, {0, 1, {-1, 0}}, {1, 0, {0, 0}}, {1, 0, {0, 1}}, {1, 0, {1, 0}}};
   P.face_edges = {{0, 0, {0, 0}}, {0, 1, {0, 0}}, {0, 2, {1, 0}}, {1, 0, {0, 1}}, {1, 1, {1, 0}}, {1, 2, {1, 0}}};
+  P.face_sites = {{0, 0, {0, 0}}, {0, 0, {1, 0}}, {0, 0, {0, 1}}, {1, 0, {1, 0}}, {1, 0, {1, 1}}, {1, 0, {0, 1}}};
+  P.edge_sites = {{0, 0, {0, 0}}, {0, 0, {1, 0}}, {1, 0, {0, 0}}, {1, 0, {0, 1}}, {2, 0, {0, 0}}, {2, 0, {-1, 1}}};
   return P;
 }
 
@@ -38,55 +57,33 @@ struct Site {
   std::vector<double> edge_labels, face_labels;
 };
 
-cpx pos(coo z) { return {z.x + double(z.y) / 2, z.y * sqrt(3) / 2}; }
-
-std::unique_ptr<Segment> seg_E(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(1, 0)); }
-
-std::unique_ptr<Segment> seg_N(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(.5, sqrt(3) / 2)); }
-
-std::unique_ptr<Segment> seg_NW(coo z) { return std::make_unique<Segment>(pos(z), pos(z) + cpx(-.5, sqrt(3) / 2)); }
-
-std::unique_ptr<Path> tri1(coo z, long long c) {
-  Path P({pos(z), pos(z) + cpx(1, 0), pos(z) + cpx(.5, sqrt(3) / 2), pos(z)}, Pen(BLACK, 1.0, Indexed(c), true));
-  return std::make_unique<Path>(P);
-}
-
-std::unique_ptr<Path> tri2(coo z, long long c) {
-  Path P({pos(z) + cpx(1, 0), pos(z) + cpx(1.5, sqrt(3) / 2), pos(z) + cpx(.5, sqrt(3) / 2), pos(z) + cpx(1, 0)},
-         Pen(BLACK, 1.0, Indexed(c), true));
-  return std::make_unique<Path>(P);
-}
-
 struct Perco {
   Pattern             P;
   size_t              n;
   double              p;
   Array<Site>         sites;
-  Array<long long>    c1, c2;
+  Array<long long>    c[2];
   std::vector<size_t> sizes;
 
   // Initialize edge labels as iid uniforms, and face labels as the max of incident edge labels.
-  Perco(Pattern P, size_t n, double p) : P(P), n(n), p(p), sites({n, n}), c1({n, n}, 0), c2({n, n}, 0) {
+  Perco(Pattern P, size_t n, double p) : P(P), n(n), p(p), sites({n, n}), c{{{n, n}, 0}, {{n, n}, 0}} {
     for (auto z : coo_range<long long>({int(n), int(n)})) {
       for (size_t i = 0; i < P.n_bonds; ++i) sites.atp(z).edge_labels.push_back(prng.uniform_real());
       for (size_t i = 0; i < P.n_faces; ++i) sites.atp(z).face_labels.push_back(0);
     }
-
     for (auto z : coo_range<long long>({int(n), int(n)}))
       for (auto [i, j, dz] : P.face_edges)
-        if (sites.atp(z + dz).edge_labels[j] > sites.atp(z).face_labels[i]) sites.atp(z).face_labels[i] = sites.atp(z + dz).edge_labels[j];
+        sites.atp(z).face_labels[i] = std::max(sites.atp(z).face_labels[i], sites.atp(z + dz).edge_labels[j]);
   }
 
   void show(double p) {
-    Figure F{"Percolation"};
+    Figure F{"Facet Percolation"};
     for (auto z : coo_range<long long>({int(n), int(n)})) {
-      cpx pos = P.pos(z);
-      F.add(std::make_unique<Circle>(pos, .1));
-      if (sites.atp(z).face_labels[0] < p) F.add(tri1(z, c1.atp(z)));
-      if (sites.atp(z).face_labels[1] < p) F.add(tri2(z, c2.atp(z)));
-      if (sites.atp(z).edge_labels[0] < p) F.add(seg_E(z));
-      if (sites.atp(z).edge_labels[1] < p) F.add(seg_N(z));
-      if (sites.atp(z).edge_labels[2] < p) F.add(seg_NW(z));
+      for (size_t i = 0; i < P.n_faces; ++i)
+        if (sites.atp(z).face_labels[i] < p) F.add(P.draw_face(z, c[i].atp(z), i));
+      for (size_t i = 0; i < P.n_bonds; ++i)
+        if (sites.atp(z).edge_labels[i] < p) F.add(P.draw_edge(z, i));
+      F.add(std::make_unique<Circle>(P.pos(z), .1, Pen(BLACK, 1, BLACK, true)));
     }
     F.show();
     F.pause();
@@ -97,8 +94,8 @@ struct Perco {
   void count() {
     std::map<long long, size_t> count;
     for (auto z : coo_range<long long>({int(n), int(n)})) {
-      if (sites.atp(z).face_labels[0] < p) ++count[c1.atp(z)];
-      if (sites.atp(z).face_labels[1] < p) ++count[c2.atp(z)];
+      if (sites.atp(z).face_labels[0] < p) ++count[c[0].atp(z)];
+      if (sites.atp(z).face_labels[1] < p) ++count[c[1].atp(z)];
     }
 
     for (auto [c, n] : count) sizes.push_back(n);
@@ -108,40 +105,18 @@ struct Perco {
   void explore() {
     long long idx = 0;
     for (auto z : coo_range<long long>({int(n), int(n)})) {
-      if (sites.atp(z).face_labels[0] < p) c1.atp(z) = ++idx;
-      if (sites.atp(z).face_labels[1] < p) c2.atp(z) = ++idx;
+      if (sites.atp(z).face_labels[0] < p) c[0].atp(z) = ++idx;
+      if (sites.atp(z).face_labels[1] < p) c[1].atp(z) = ++idx;
     }
 
     bool done = false;
     while (!done) {
       done = true;
       for (auto z : coo_range<long long>({int(n), int(n)})) {
-        if (sites.atp(z).face_labels[0] < p) {
-          if (c2.atp(z) > c1.atp(z)) {
-            c1.atp(z) = c2.atp(z);
-            done      = false;
-          }
-          if (c2.atp({z.x - 1, z.y}) > c1.atp(z)) {
-            c1.atp(z) = c2.atp({z.x - 1, z.y});
-            done      = false;
-          }
-          if (c2.atp({z.x, z.y - 1}) > c1.atp(z)) {
-            c1.atp(z) = c2.atp({z.x, z.y - 1});
-            done      = false;
-          }
-        }
-        if (sites.atp(z).face_labels[1] < p) {
-          if (c1.atp(z) > c2.atp(z)) {
-            c2.atp(z) = c1.atp(z);
-            done      = false;
-          }
-          if (c1.atp({z.x + 1, z.y}) > c2.atp(z)) {
-            c2.atp(z) = c1.atp({z.x + 1, z.y});
-            done      = false;
-          }
-          if (c1.atp({z.x, z.y + 1}) > c2.atp(z)) {
-            c2.atp(z) = c1.atp({z.x, z.y + 1});
-            done      = false;
+        for (auto [i, j, dz] : P.adj_faces) {
+          if (sites.atp(z).face_labels[i] < p && c[j].atp(z + dz) > c[i].atp(z)) {
+            c[i].atp(z) = c[j].atp(z + dz);
+            done        = false;
           }
         }
       }
