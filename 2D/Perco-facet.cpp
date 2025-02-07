86b923108d33c25547322097376a71580b2f847b
Vincent Beffara <vbeffara@gmail.com>
2025-02-07 11:03:49 +0100

    Store edge labels

--- 2D/Perco-facet.cpp
+++ 2D/Perco-facet.cpp
@@ -9,8 +9,17 @@
 
 using namespace vb;
 
+struct Pattern {
+  size_t n_sites, n_bonds, n_faces;
+  cpx    period;
+};
+
+Pattern TRI{1, 3, 2, cpx(1, sqrt(3) / 2)};
+
 struct Site {
-  bool E{false}, N{false}, NW{false}, T1{false}, T2{false};
+  std::vector<double> edge_labels, face_labels;
+  [[deprecated]] bool E{false}, N{false}, NW{false};
+  bool                T1{false}, T2{false};
 };
 
 cpx pos(coo z) { return {z.x + double(z.y) / 2, z.y * sqrt(3) / 2}; }
@@ -34,34 +43,35 @@ std::unique_ptr<Path> tri2(coo z, long long c) {
 
 struct Perco {
   size_t              n;
+  double              p;
   Array<Site>         sites;
   Array<long long>    c1, c2;
   std::vector<size_t> sizes;
 
-  Perco(size_t n, double p) : n(n), sites({n, n}), c1({n, n}, 0), c2({n, n}, 0) {
-    // Open each edge with probability `p`
+  Perco(Pattern P, size_t n, double p) : n(n), p(p), sites({n, n}), c1({n, n}, 0), c2({n, n}, 0) {
     for (auto z : coo_range<size_t>({n, n})) {
-      if (prng.bernoulli(p)) sites[z].E = true;
-      if (prng.bernoulli(p)) sites[z].N = true;
-      if (prng.bernoulli(p)) sites[z].NW = true;
+      for (size_t i = 0; i < P.n_bonds; ++i) sites[z].edge_labels.push_back(prng.uniform_real());
     }
     // Open each facet if the corresponding edges are open
     for (auto z : coo_range<long long>({int(n), int(n)})) {
-      if (sites.atp(z).E && sites.atp(z).N && sites.atp({z.x + 1, z.y}).NW) sites.atp(z).T1 = true;
-      if (sites.atp({z.x + 1, z.y}).NW && sites.atp({z.x + 1, z.y}).N && sites.atp({z.x, z.y + 1}).E) sites.atp(z).T2 = true;
+      if (sites.atp(z).edge_labels[0] < p && sites.atp(z).edge_labels[1] < p && sites.atp({z.x + 1, z.y}).edge_labels[2] < p)
+        sites.atp(z).T1 = true;
+      if (sites.atp({z.x + 1, z.y}).edge_labels[2] < p && sites.atp({z.x + 1, z.y}).edge_labels[1] < p &&
+          sites.atp({z.x, z.y + 1}).edge_labels[0] < p)
+        sites.atp(z).T2 = true;
     }
   }
 
-  void show() {
+  void show(double p) {
     Figure F{"Percolation"};
     for (auto z : coo_range<long long>({int(n), int(n)})) {
       cpx pos{z.x + double(z.y) / 2, z.y * sqrt(3) / 2};
       F.add(std::make_unique<Circle>(pos, .1));
       if (sites.atp(z).T1) F.add(tri1(z, c1.atp(z)));
       if (sites.atp(z).T2) F.add(tri2(z, c2.atp(z)));
-      if (sites.atp(z).E) F.add(seg_E(z));
-      if (sites.atp(z).N) F.add(seg_N(z));
-      if (sites.atp(z).NW) F.add(seg_NW(z));
+      if (sites.atp(z).edge_labels[0] < p) F.add(seg_E(z));
+      if (sites.atp(z).edge_labels[1] < p) F.add(seg_N(z));
+      if (sites.atp(z).edge_labels[2] < p) F.add(seg_NW(z));
     }
     F.show();
     F.pause();
@@ -135,20 +145,19 @@ int main(int argc, char **argv) {
   auto n = clp.param("n", size_t(50), "Domain size");
   auto p = clp.param("p", 0.5, "Percolation parameter (only used if t=1)");
   auto t = clp.param("t", size_t(1), "Number of samples (for p_c estimation, ignores p)");
-  auto v = clp.flag("v", "Show the picture");
   clp.finalize();
 
   if (t == 1) {
-    Perco P(n, p);
+    Perco P(TRI, n, p);
     P.explore();
     spdlog::info("Largest cluster size: {}", P.biggest());
-    P.show();
+    P.show(p);
   } else {
     ProgressBar PB(t);
     for (size_t i = 0; i < t; ++i) {
       PB.set(i);
       double p = prng.uniform_real();
-      Perco  P(n, p);
+      Perco  P(TRI, n, p);
       P.explore();
       fmt::print("{} {} {}\n", p, P.biggest(), P.second_biggest());
     }
